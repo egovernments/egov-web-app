9f39243fbb02ea2a00df8e2f2045d8b73f765a5f
KD <0xbeefdead@gmail.com>
2018-01-10 18:11:21 +0530

    moved measurebook spec to master folder

--- web/ui-app/src/components/framework/specs/works/master/measurementbook.js
+++ web/ui-app/src/components/framework/specs/works/master/measurementbook.js
@@ -1,67 +1,144 @@
 var dat = {
-    'works.search': {
-      numCols: 6,
-      useTimestamp: true,
-      objectName: 'RemarksRequest',
-      url: '/works-masters/v1/remarks/_search',
-      groups: [
-        {
-          name: 'remarkMasterSearch',
-          label: 'works.remarks.search.title',
-          fields: [
+  'works.search': {
+    numCols: 6,
+    useTimestamp: true,
+    objectName: 'RemarksRequest',
+    url: '/works-measurementbook/v1/measurementbooks/_search',
+    groups: [
+      {
+        name: 'search',
+        label: 'works.measurementbook.search.title',
+        fields: [
             {
-              name: 'typeOfDocument',
-              jsonPath: 'typeOfDocument',
-              label: 'works.remarks.label.typeOfDocument',
-              type: 'singleValueList',
-              isRequired: false,
-              isDisabled: false,
-              url: '/egov-mdms-service/v1/_get?&moduleName=Works&masterName=TypeOfDocument|$..code|$..name',
-              patternErrorMsg: 'works.remarks.error.message.typeOfDocument',
+                name: 'mbRefNumber',
+                jsonPath: 'measurementbooks[0].mbRefNo',
+                label: 'works.measurementbook.label.mbRefNumberLike',
+                type: 'autoCompelete',
+                isRequired: false,
+                isDisabled: false,
+                patternErrorMsg: 'works.measurementbook.error.message.mbRefNumberLike',
+                url: "/measurementbooks/_search?mbRefNumberLike=|$.measurementBooks.*.mbRefNo|$.measurementBooks.*.mbRefNo",
+                autoCompleteDependancy: {
+                    autoCompleteUrl: "/measurementbooks/_search?mbRefNumberLike=|$.measurementBooks.*.mbRefNo|$.measurementBooks.*.mbRefNo",
+                    autoFillFields: {
+                    },
+                },
             },
             {
-                name: 'remarksType',
-                jsonPath: 'remarksType',
-                label: 'works.remarks.label.remarksType',
+                name: 'loaNumberLike',
+                jsonPath: 'loaNumberLike',
+                label: 'works.measurementbook.label.loaNumberLike',
+                type: 'autoCompelete',
+                isRequired: false,
+                isDisabled: false,
+                patternErrorMsg: 'works.measurementbook.error.message.loaNumberLike',
+            },
+            {
+                name: 'fromDate',
+                jsonPath: 'fromDate',
+                label: 'works.measurementbook.label.fromDate',
+                type: 'datePicker',
+                isRequired: true,
+                isDisabled: false,
+                maxLength: 128,
+                minLength: 1,
+                patternErrorMsg: '',
+            },
+            {
+                name: 'toDate',
+                jsonPath: 'toDate',
+                label: 'works.measurementbook.label.toDate',
+                type: 'datePicker',
+                isRequired: false,
+                isDisabled: false,
+                maxLength: 128,
+                minLength: 1,
+                patternErrorMsg: '',
+            },
+            {
+                name: 'contractorNameLike',
+                jsonPath: 'contractorNameLike',
+                label: 'works.measurementbook.label.contractorNameLike',
+                type: 'autoCompelete',
+                isRequired: false,
+                isDisabled: false,
+                patternErrorMsg: 'works.measurementbook.error.message.contractorNameLike',
+                url: "swm-services/routes/_search?|$.routes.*.code|$.routes.*.name"
+            },
+            {
+                name: 'department',
+                jsonPath: 'department',
+                label: 'works.measurementbook.label.department',
+                type: 'multiValueList',
+                isRequired: false,
+                isDisabled: false,
+                patternErrorMsg: '',
+                url: "swm-services/routes/_search?|$.routes.*.code|$.routes.*.name"
+            },
+            {
+                name: 'detailedEstimateNumberLike',
+                jsonPath: 'detailedEstimateNumberLike',
+                label: 'works.measurementbook.label.detailedEstimateNumberLike',
+                type: 'autoCompelete',
+                isRequired: false,
+                isDisabled: false,
+                patternErrorMsg: 'works.measurementbook.error.message.detailedEstimateNumberLike',
+                url: "swm-services/routes/_search?|$.routes.*.code|$.routes.*.name"
+            },
+            {
+                name: 'createdBy',
+                jsonPath: 'createdBy',
+                label: 'works.measurementbook.label.createdBy',
                 type: 'singleValueList',
                 isRequired: false,
                 isDisabled: false,
-                url: '/egov-mdms-service/v1/_get?&moduleName=Works&masterName=RemarksType|$..code|$..name',
-                patternErrorMsg: 'works.remarks.error.message.remarksType',
+                patternErrorMsg: '',
+                url: "swm-services/routes/_search?|$.routes.*.code|$.routes.*.name"
             },
             {
-                name: 'remarksDescription',
-                jsonPath: 'remarksDescription',
-                label: 'works.remarks.label.remarksDescription',
-                type: 'text',
+                name: 'statuses',
+                jsonPath: 'statuses',
+                label: 'works.measurementbook.label.statuses',
+                type: 'singleValueList',
                 isRequired: false,
                 isDisabled: false,
-                patternErrorMsg: 'works.remarks.error.message.remarksDescription',
+                patternErrorMsg: '',
+                url: "swm-services/routes/_search?|$.routes.*.code|$.routes.*.name"
             },
-          ],
+            {
+                name: 'workOrderNumberLike',
+                jsonPath: 'workOrderNumberLike',
+                label: 'works.measurementbook.label.workOrderNumberLike',
+                type: 'autoCompelete',
+                isRequired: false,
+                isDisabled: false,
+                patternErrorMsg: '',
+                url: "swm-services/routes/_search?|$.routes.*.code|$.routes.*.name"
+            },
+        ],
+      },
+    ],
+    result: {
+      header: [
+        {
+          label: 'works.search.result.documenttype',
+        },
+        {
+          label: 'works.search.result.remarkstype',
+        },
+        {
+          label: 'works.search.result.remarksDescription',
+        },
+        {
+          label: 'works.search.result.editable',
         },
       ],
-      result: {
-        header: [
-          {
-            label: 'works.search.result.documenttype',
-          },
-          {
-            label: 'works.search.result.remarkstype',
-          },
-          {
-            label: 'works.search.result.remarksDescription',
-          },
-          {
-            label: 'works.search.result.editable',
-          },
-        ],
-        values: ['typeOfDocument', 'remarksType', 'remarksDetails[0].remarksDescription', 'remarksDetails[0].editable'],
-        resultIdKey: 'id',
-        resultPath: 'remarks',
-        rowClickUrlUpdate: '/update/works/remarks/{id}',
-        rowClickUrlView: '/view/works/remarks/{id}',
-      },
+      values: ['typeOfDocument', 'remarksType', 'remarksDetails[0].remarksDescription', 'remarksDetails[0].editable'],
+      resultIdKey: 'id',
+      resultPath: 'remarks',
+      rowClickUrlUpdate: '/update/works/remarks/{id}',
+      rowClickUrlView: '/view/works/remarks/{id}',
+    },
     },
     'works.create': {
       numCols: 12/2,
@@ -96,7 +173,6 @@ var dat = {
                 url: '/egov-mdms-service/v1/_get?&moduleName=Works&masterName=TypeOfDocument|$..code|$..name',
                 patternErrorMsg: 'works.remarks.error.message.typeOfDocument',
             },
-            
             {
                 name: 'remarksType',
                 jsonPath: 'remarks[0].remarksType',
