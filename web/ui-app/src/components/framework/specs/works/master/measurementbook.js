13d71c783ad73ed8c3b52cce7d9c5457163604cc
KD <0xbeefdead@gmail.com>
2018-01-11 19:23:13 +0530

    started with autocomplete for measurementbook

--- web/ui-app/src/components/framework/specs/works/master/measurementbook.js
+++ web/ui-app/src/components/framework/specs/works/master/measurementbook.js
@@ -1,3 +1,5 @@
+let criteria = '[' + encodeURIComponent('?') + '(@.moduleType' + encodeURIComponent('==\'MBHeader\'') + ')]';
+
 var dat = {
   'works.search': {
     numCols: 6,
@@ -10,7 +12,7 @@ var dat = {
         label: 'works.measurementbook.search.title',
         fields: [
             {
-                name: 'mbRefNo',
+                name: 'measurementbooks[0].mbRefNo',
                 jsonPath: 'measurementbooks[0].mbRefNo',
                 label: 'works.measurementbook.label.mbRefNumberLike',
                 type: 'autoCompelete',
@@ -23,26 +25,26 @@ var dat = {
                     },
                 },
             },
-            {
-                name: 'loaNumbers',
-                jsonPath: 'LetterOfAcceptance[0].loaNumbers',
-                label: 'works.measurementbook.label.loaNumbers',
-                type: 'autoCompelete',
-                isRequired: false,
-                isDisabled: false,
-                url: "/works-measurementbook/v1/letterofacceptances/_search?|$.measurementBooks.*.mbRefNo|$.measurementBooks.*.mbRefNo",
-                autoCompleteDependancy: {
-                    autoCompleteUrl: "/works-measurementbook/v1/letterofacceptances/_search?loaNumberLike={LetterOfAcceptance[0].loaNumbers}",
-                    autoFillFields: {
-                    },
-                },
-            },
+            // {
+            //     name: 'loaNumbers',
+            //     jsonPath: 'measurementbooks[0].loaNumbers',
+            //     label: 'works.measurementbook.label.loaNumbers',
+            //     type: 'autoCompelete',
+            //     isRequired: false,
+            //     isDisabled: false,
+            //     url: "/works-measurementbook/v1/letterofacceptances/_search|$.measurementBooks.*.mbRefNo|$.measurementBooks.*.mbRefNo",
+            //     autoCompleteDependancy: {
+            //         autoCompleteUrl: "/works-measurementbook/v1/letterofacceptances/_search?loaNumberLike={measurementbooks[0].loaNumbers}",
+            //         autoFillFields: {
+            //         },
+            //     },
+            // },
             {
                 name: 'fromDate',
                 jsonPath: 'fromDate',
                 label: 'works.measurementbook.label.fromDate',
                 type: 'datePicker',
-                isRequired: true,
+                isRequired: false,
                 isDisabled: false,
                 maxLength: 128,
                 minLength: 1,
@@ -59,66 +61,78 @@ var dat = {
                 minLength: 1,
                 patternErrorMsg: '',
             },
-            {
-                name: 'contractorNameLike',
-                jsonPath: 'contractorNameLike',
-                label: 'works.measurementbook.label.contractorNameLike',
-                type: 'autoCompelete',
-                isRequired: false,
-                isDisabled: false,
-                patternErrorMsg: 'works.measurementbook.error.message.contractorNameLike',
-                url: "swm-services/routes/_search?|$.routes.*.code|$.routes.*.name"
-            },
-            {
-                name: 'department',
-                jsonPath: 'department',
-                label: 'works.measurementbook.label.department',
-                type: 'multiValueList',
-                isRequired: false,
-                isDisabled: false,
-                patternErrorMsg: '',
-                url: "swm-services/routes/_search?|$.routes.*.code|$.routes.*.name"
-            },
-            {
-                name: 'detailedEstimateNumberLike',
-                jsonPath: 'detailedEstimateNumberLike',
-                label: 'works.measurementbook.label.detailedEstimateNumberLike',
-                type: 'autoCompelete',
-                isRequired: false,
-                isDisabled: false,
-                patternErrorMsg: 'works.measurementbook.error.message.detailedEstimateNumberLike',
-                url: "swm-services/routes/_search?|$.routes.*.code|$.routes.*.name"
-            },
-            {
-                name: 'createdBy',
-                jsonPath: 'createdBy',
-                label: 'works.measurementbook.label.createdBy',
-                type: 'singleValueList',
-                isRequired: false,
-                isDisabled: false,
-                patternErrorMsg: '',
-                url: "swm-services/routes/_search?|$.routes.*.code|$.routes.*.name"
-            },
+            // {
+            //     name: 'contractorNames',
+            //     jsonPath: 'measurementbooks[0].contractorNames[0]',
+            //     label: 'works.measurementbook.label.contractorNames',
+            //     type: 'autoCompelete',
+            //     isRequired: false,
+            //     isDisabled: false,
+            //     url: "/works-masters/v1/contractors/_search|$.measurementBooks.*.contractorNames|$.measurementBooks.*.contractorNames",
+            //     autoCompleteDependancy: {
+            //         autoCompleteUrl: "/works-masters/v1/contractors/_search?name={measurementbooks[0].contractorNames[0]}",
+            //         autoFillFields: {
+            //         },
+            //     },
+            // },
+            // {
+            //     name: 'department',
+            //     jsonPath: 'department',
+            //     label: 'works.measurementbook.label.department',
+            //     type: 'multiValueList',
+            //     isRequired: false,
+            //     isDisabled: false,
+            //     patternErrorMsg: '',
+            //     url: '/egov-mdms-service/v1/_get?&moduleName=Works&masterName=TypeOfDocument|$..code|$..name',
+            // },
+            // {
+            //     name: 'detailedEstimateNumberLike',
+            //     jsonPath: 'measurementbooks[0].detailedEstimateNumber[0]',
+            //     label: 'works.measurementbook.label.detailedEstimateNumberLike',
+            //     type: 'autoCompelete',
+            //     isRequired: false,
+            //     isDisabled: false,
+            //     url: "/works-estimate/v1/detailedestimates/_search|$.measurementBooks.*.contractorNames|$.measurementBooks.*.contractorNames",
+            //     autoCompleteDependancy: {
+            //         autoCompleteUrl: "/works-estimate/v1/detailedestimates/_search?name={measurementbooks[0].detailedEstimateNumber[0]}",
+            //         autoFillFields: {
+            //         },
+            //     },
+            // },
+            // {
+            //     name: 'createdBy',
+            //     jsonPath: 'createdBy',
+            //     label: 'works.measurementbook.label.createdBy',
+            //     type: 'singleValueList',
+            //     isRequired: false,
+            //     isDisabled: false,
+            //     patternErrorMsg: '',
+            //     url: "swm-services/routes/_search?|$.routes.*.code|$.routes.*.name"
+            // },
             {
                 name: 'statuses',
                 jsonPath: 'statuses',
                 label: 'works.measurementbook.label.statuses',
-                type: 'singleValueList',
+                type: 'multiValueList',
                 isRequired: false,
                 isDisabled: false,
                 patternErrorMsg: '',
-                url: "swm-services/routes/_search?|$.routes.*.code|$.routes.*.name"
+                url: `/egov-mdms-service/v1/_get?&moduleName=works&masterName=WorksStatus&filter=${criteria}|$..code|$..description`,
             },
-            {
-                name: 'workOrderNumberLike',
-                jsonPath: 'workOrderNumberLike',
-                label: 'works.measurementbook.label.workOrderNumberLike',
-                type: 'autoCompelete',
-                isRequired: false,
-                isDisabled: false,
-                patternErrorMsg: '',
-                url: "swm-services/routes/_search?|$.routes.*.code|$.routes.*.name"
-            },
+            // {
+            //     name: 'workOrderNumberLike',
+            //     jsonPath: 'workOrderNumberLike',
+            //     label: 'works.measurementbook.label.workOrderNumberLike',
+            //     type: 'autoCompelete',
+            //     isRequired: false,
+            //     isDisabled: false,
+            //     url: "/works-estimate/v1/detailedestimates/_search|$.measurementBooks.*.contractorNames|$.measurementBooks.*.contractorNames",
+            //     autoCompleteDependancy: {
+            //         autoCompleteUrl: "/works-estimate/v1/detailedestimates/_search?name={measurementbooks[0].detailedEstimateNumber[0]}",
+            //         autoFillFields: {
+            //         },
+            //     },
+            // },
         ],
       },
     ],
