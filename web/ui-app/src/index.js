8697297303c9fe0f6527a2f3abbe30ad333d9dfd
muralimgwl <murali.m@goodworklabs.com>
2018-01-12 15:20:54 +0530

    Removed commented line from index.js

--- web/ui-app/src/index.js
+++ web/ui-app/src/index.js
@@ -1,6 +1,3 @@
-// import 'materialize-css/dist/css/materialize.min.css';
-// import 'materialize-css/dist/js/materialize.min.js';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
@@ -9,12 +6,9 @@ import store from './store';
 import { grey300 } from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-// import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import App from './components/App';
-// import $ from "jquery";
-// import {firebaseDBRef} from './firebase/';
 
 import './styles/index.css';
 import './styles/application.css';
@@ -25,7 +19,6 @@ import './styles/buttons.dataTables.min.css';
 import './styles/bootstrap.datepicker.css';
 import './styles/react.datetimepicker.css';
 import './styles/react-big-calendar.css';
-// import router from "./router";
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
@@ -52,39 +45,7 @@ let themeObject = {
   },
 };
 
-// var hash = window.location.hash.split("/");
-//
-//
-// if(localStorage.getItem("token") && localStorage.getItem("userRequest"))
-// {
-//     getTheme(localStorage.getItem("tenantId")?localStorage.getItem("tenantId"):'default');
-// }
-// else {
-//     getTheme(hash[1]?hash[1]:"default");
-// }
-//
-//
-//
-// function getTheme(tenantId)
-// {
-//   $.ajax({
-//   url: "https://raw.githubusercontent.com/abhiegov/test/master/tenantTheme.json?timestamp="+new Date().getTime(),
-//   // dataType: 'application/javascript',
-//   success: function(results)
-//   {
-//       var content = JSON.parse(results);
-//       // console.log(content[(hash[1]?hash[1]:"default")]);
-//       renderUI(content["theme"][tenantId]);
-//   },
-//   error: function (results) {
-//     renderUI(themeObject);
-//   }})
-// }
-//
-//
-//
-// function renderUI(themeObject)
-// {
+
 const muiTheme = getMuiTheme(themeObject);
 
 ReactDOM.render(
@@ -98,11 +59,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 registerServiceWorker();
-// }
-
-// console.log(hash[1]?hash[1]:"default");
-// firebaseDBRef.child("theme/"+(hash[1]?hash[1]:"default")).once('value').then(function(snapshot) {
-//         console.log(snapshot);
-//         renderUI(snapshot.val());
-//
-//   });
