511b2efc6a412d1480c808ab4123db6c5811e898
Elzan Mathew <elzan.mathew@egovernments.org>
2018-01-16 15:09:37 +0530

    Check for custom attributes before paining it in immovable assets

--- web/ui-app/src/components/non-framework/asset/master/assetImmovableCreate.js
+++ web/ui-app/src/components/non-framework/asset/master/assetImmovableCreate.js
@@ -254,11 +254,13 @@ class assetImmovableCreate extends Component {
 
     Api.commonApiPost('asset-services-maha/assets/_search', { id: urlId }, {}, false, false, false, '', '', false).then(
       function(response) {
-        for (var i = 0; i < response.Assets[0].assetAttributes.length; i++) {
-          assetCheck[response.Assets[0].assetAttributes[i].key] = {
-            [response.Assets[0].assetAttributes[i].type]: response.Assets[0].assetAttributes[i].value,
-          };
-        }
+	if (response && response.Assets && response.Assets[0] && response.Assets[0].assetAttributes) {
+        	for (var i = 0; i < response.Assets[0].assetAttributes.length; i++) {
+         	 assetCheck[response.Assets[0].assetAttributes[i].key] = {
+            	[response.Assets[0].assetAttributes[i].type]: response.Assets[0].assetAttributes[i].value,
+          	};
+        	}
+	}
         if (response && response.Assets && response.Assets[0] && response.Assets[0].titleDocumentsAvailable) {
           response.Assets[0].titleDocumentsAvailable = response.Assets[0].titleDocumentsAvailable.join(',');
         }
