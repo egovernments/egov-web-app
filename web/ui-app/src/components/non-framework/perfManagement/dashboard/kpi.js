e54971eaf809b51f4db341a0b1a8514c89605f71
KD <0xbeefdead@gmail.com>
2018-01-22 13:47:18 +0530

    #368. Updated icons for dashboard

--- web/ui-app/src/components/non-framework/perfManagement/dashboard/kpi.js
+++ web/ui-app/src/components/non-framework/perfManagement/dashboard/kpi.js
@@ -82,28 +82,89 @@ export default class Dashboard extends Component {
 
   getDepartmentLogo = department => {
     if (department === 'ADMINISTRATION') {
-      return require('../../../../images/pms/Administration.png');
+      return require('../../../../images/pms/administration.png');
     }
     if (department === 'ACCOUNTS') {
       return require('../../../../images/pms/accounts.png');
     }
     if (department === 'ENGINEERING') {
-      return require('../../../../images/pms/Engineering.png');
+      return require('../../../../images/pms/engineering.png');
     }
-    if (department === 'TOWN PLANNING') {
-      return require('../../../../images/pms/TownPlanning.png');
+    if (department === 'TOWNPLANNING') {
+      return require('../../../../images/pms/townplanning.png');
     }
     if (department === 'REVENUE') {
-      return require('../../../../images/pms/Revenue.png');
+      return require('../../../../images/pms/revenue.png');
     }
-    if (department === 'PUBLIC HEALTH AND SANITATION') {
-      return require('../../../../images/pms/PublicHealthSanitation.png');
+    if (department === 'PUBLICHEALTHANDSANITATION') {
+      return require('../../../../images/pms/publichealthsanitation.png');
     }
-    if (department === 'URBAN POVERTY ALLEVIATION') {
-      return require('../../../../images/pms/UrbanPovertyAlleviation.png');
+    if (department === 'URBANPOVERTYALLEVIATION') {
+      return require('../../../../images/pms/urbanpovertyalleviation.png');
     }
     if (department === 'EDUCATION') {
-      return require('../../../../images/pms/Education.png');
+      return require('../../../../images/pms/education.png');
+    }
+
+    if (department === 'ASSESSMENTDEPARTMENT') {
+      return require('../../../../images/pms/assessmentdepartment.png');
+    }
+    if (department === 'ASSETDEPARTMENT') {
+      return require('../../../../images/pms/assetdepartment.png');
+    }
+    if (department === 'COMPUTERDEPARTMENT') {
+      return require('../../../../images/pms/computerdepartment.png');
+    }
+    if (department === 'CFC') {
+      return require('../../../../images/pms/CFC.png');
+    }
+    if (department === 'LEGALSERVICESDEPARTMENT') {
+      return require('../../../../images/pms/legalservicesdepartment.png');
+    }
+    if (department === 'ESTABLISHMENTDEPARTMENT') {
+      return require('../../../../images/pms/establishmentdepartment.png');
+    }
+    if (department === 'ELECTIONDEPARTMENT') {
+      return require('../../../../images/pms/electiondepartment.png');
+    }
+    if (department === 'FIREBRIGADEDEPARTMENT') {
+      return require('../../../../images/pms/firebrigadedepartment.png');
+    }
+    if (department === 'GENERALADMINISTRATIONDEPARTMENT') {
+      return require('../../../../images/pms/generaladministrationdepartment.png');
+    }
+    if (department === 'HEALTHDEPARTMENT') {
+      return require('../../../../images/pms/healthdepartment.png');
+    }
+    if (department === 'LICENSEDEPARTMENT') {
+      return require('../../../../images/pms/licensedepartment.png');
+    }
+    if (department === 'LIGHTDEPARTMENT') {
+      return require('../../../../images/pms/lightdepartment.png');
+    }
+    if (department === 'MARKETLICENSEDEPARTMENT') {
+      return require('../../../../images/pms/marketlicensedepartment.png');
+    }
+    if (department === 'MARRIAGEREGISTRATIONDEPARTMENT') {
+      return require('../../../../images/pms/marriageregistrationdepartment.png');
+    }
+    if (department === 'OFFICESUPRIDENTDEPARTMET') {
+      return require('../../../../images/pms/CFC.png');
+    }
+    if (department === 'RECORDDEPARTMENT') {
+      return require('../../../../images/pms/recorddepartment.png');
+    }
+    if (department === 'STOREDEPARTMENT') {
+      return require('../../../../images/pms/storedepartment.png');
+    }
+    if (department === 'SUVARNAJAYANTISHAHARIROJGARYOJANA') {
+      return require('../../../../images/pms/suvarnajayantishaharirojgaryojana.png');
+    }
+    if (department === 'INWARDOUTWARDDEPARTMENT') {
+      return require('../../../../images/pms/inwardoutwarddepartment.png');
+    }
+    if (department === 'WATERDEPARTMENT') {
+      return require('../../../../images/pms/waterdepartment.png');
     }
 
     return require('../../../../images/pms/kpi-default.png');
@@ -129,7 +190,7 @@ export default class Dashboard extends Component {
     let departments = parseDepartmentResponse(this.departments);
     if (departments.length > 0) {
       return departments.map((item, index) => (
-        <DashboardCard key={index} index={index} onClick={this.processOnClickOnCard} name={item.name} logo={this.getDepartmentLogo(item.name)} />
+        <DashboardCard key={index} index={index} onClick={this.processOnClickOnCard} name={item.name} logo={this.getDepartmentLogo(item.name.toUpperCase().replace(/ /g,''))} />
       ));
     }
   };
