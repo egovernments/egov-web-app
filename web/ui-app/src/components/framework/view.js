44ec5a5e4e868f5bde5e1809d7b01d5fe419694e
Varun Hegde <1hvarun@gmail.com>
2018-01-25 19:47:54 +0530

    print screen

--- web/ui-app/src/components/framework/view.js
+++ web/ui-app/src/components/framework/view.js
@@ -15,7 +15,6 @@ import UiBackButton from './components/UiBackButton';
 import UiEditButton from './components/UiEditButton';
 // import UiLogo from './components/UiLogo';
 
-
 var specifications = {};
 
 let reqRequired = [];
@@ -232,28 +231,26 @@ class Report extends Component {
     if (res) {
       let value = this.getVal(item.queryParameter);
       if (value) {
-        var filterdObject = _.filter(
-          res[`${item.responseArray}`],
-          function (o) {
-            // let jsonObject=Json.stringify()
-            var currentValue = _.get(o, item.primaryKey);
-            if (currentValue == value) { return o };
-          });
+        var filterdObject = _.filter(res[`${item.responseArray}`], function(o) {
+          // let jsonObject=Json.stringify()
+          var currentValue = _.get(o, item.primaryKey);
+          if (currentValue == value) {
+            return o;
+          }
+        });
         return filterdObject;
       }
       return null;
     }
 
     return null;
-
-  }
+  };
 
   setVal = (jsonPath, value) => {
     let formData = { ...this.props.formData };
     _.set(formData, jsonPath, value);
     this.props.setFormData(formData);
   };
-
 
   initData() {
     try {
@@ -263,9 +260,19 @@ class Report extends Component {
       } else {
         specifications = require(`./specs/${hash[2]}/master/${hash[3]}`).default;
       }
-    } catch (e) { }
+    } catch (e) {}
 
-    let { mockData, moduleName, actionName, setMetaData, setModuleName, setActionName, setMockData, setDropDownData, setDropDownOriginalData } = this.props;
+    let {
+      mockData,
+      moduleName,
+      actionName,
+      setMetaData,
+      setModuleName,
+      setActionName,
+      setMockData,
+      setDropDownData,
+      setDropDownOriginalData,
+    } = this.props;
     let hashLocation = window.location.hash;
     let self = this;
     let obj = specifications[`${hashLocation.split('/')[2]}.${hashLocation.split('/')[1]}`];
@@ -306,7 +313,7 @@ class Report extends Component {
     }
 
     var _body = {};
-    if (url.includes("/egov-mdms-service/v1/_search")) {
+    if (url.includes('/egov-mdms-service/v1/_search')) {
       var moduleDetails = [];
       var masterDetails = [];
       let data = { moduleName: '', masterDetails: [] };
@@ -315,7 +322,9 @@ class Report extends Component {
       data.moduleName = hashLocation.split('/')[2];
       // console.log(data, masterDetail)
       // console.log(url.split('?')[1].split('={')[0]);
-      var filterData = `[?(@.${specifications[`${hashLocation.split('/')[2]}.${hashLocation.split('/')[1]}`].url.split('?')[1].split('={')[0]}=='${hashLocation.split('/')[hashLocation.split('/').length - 1]}')]`;
+      var filterData = `[?(@.${specifications[`${hashLocation.split('/')[2]}.${hashLocation.split('/')[1]}`].url.split('?')[1].split('={')[0]}=='${
+        hashLocation.split('/')[hashLocation.split('/').length - 1]
+      }')]`;
       masterDetail.filter = filterData;
       masterDetail.name = specifications[`${hashLocation.split('/')[2]}.${hashLocation.split('/')[1]}`].objectName;
       data.masterDetails[0] = _.cloneDeep(masterDetail);
@@ -331,7 +340,7 @@ class Report extends Component {
     }
     self.props.setLoadingStatus('loading');
     if (obj && obj.preApiCalls) {
-      obj.preApiCalls.forEach(async (item) => {
+      obj.preApiCalls.forEach(async item => {
         let res = await callApi(item);
         let orgRes = Object.assign({}, res);
         if (item.type && item.type == 'text') {
@@ -340,7 +349,7 @@ class Report extends Component {
           if (jsonpaths && filteredresponse) {
             for (var i = 0; i < item.responsePaths.length; i++) {
               if (filteredresponse[0]) {
-                var value = _.get(filteredresponse[0], item.responsePaths[i])
+                var value = _.get(filteredresponse[0], item.responsePaths[i]);
                 if (value) {
                   self.setVal(jsonpaths[i], value);
                 }
@@ -353,16 +362,13 @@ class Report extends Component {
           setDropDownOriginalData(item.jsonPath, res);
           self.props.setLoadingStatus('hide');
         }
-      })
+      });
     }
     self.props.setLoadingStatus('hide');
 
     Api.commonApiPost(url, query, _body, false, specifications[`${hashLocation.split('/')[2]}.${hashLocation.split('/')[1]}`].useTimestamp).then(
-       (res)  => {
-        var spec =
-          specifications[
-          `${hashLocation.split("/")[2]}.${hashLocation.split("/")[1]}`
-          ];
+      res => {
+        var spec = specifications[`${hashLocation.split('/')[2]}.${hashLocation.split('/')[1]}`];
         const JP = jp;
         if (spec && spec.beforeSetForm) eval(spec.beforeSetForm);
         self.props.setFormData(res);
@@ -374,22 +380,18 @@ class Report extends Component {
           hashLocation.split('/')[1],
           specifications[`${hashLocation.split('/')[2]}.${hashLocation.split('/')[1]}`].objectName
         );
-        
-      
+
         if (spec && spec.afterSetForm) eval(spec.afterSetForm);
-
       },
-      function (err) { }
+      function(err) {}
     );
-
-
   }
 
   //To find last index in jsonPath for multiple cards
-  indexFinder = (jsonPath) => {
+  indexFinder = jsonPath => {
     let matches = jsonPath.match(/(\[\d+\])/g);
-    return matches.length ? parseInt(matches[matches.length - 1].replace(/[^\d]/g, "")) : -1;
-  }
+    return matches.length ? parseInt(matches[matches.length - 1].replace(/[^\d]/g, '')) : -1;
+  };
 
   componentDidMount() {
     this.initData();
@@ -421,17 +423,19 @@ class Report extends Component {
       : '';
   };
 
+  // problem with bootstrap media queries
   printer = () => {
     var mywindow = window.open('', 'PRINT', 'height=400,width=600');
 
     var cdn = `
-     <!-- Latest compiled and minified CSS -->
-     <link rel="stylesheet" media="all" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-     <!-- Optional theme -->
-     <link rel="stylesheet" media="all" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">  `;
-    mywindow.document.write('<html><head><title> </title>');
+    <link rel="stylesheet"  href="/static/css/main.8e498f23.css" crossorigin="anonymous"  />
+    `;
+    var extraStyles = `<style type="text/css">
+ @media print{.col-sm-1,.col-sm-10,.col-sm-11,.col-sm-12,.col-sm-2,.col-sm-3,.col-sm-4,.col-sm-5,.col-sm-6,.col-sm-7,.col-sm-8,.col-sm-9{float:left}.col-sm-12{width:100%}.col-sm-11{width:91.66666666666666%}.col-sm-10{width:83.33333333333334%}.col-sm-9{width:75%}.col-sm-8{width:66.66666666666666%}.col-sm-7{width:58.333333333333336%}.col-sm-6{width:50%}.col-sm-5{width:41.66666666666667%}.col-sm-4{width:33.33333333333333%}.col-sm-3{width:25%}.col-sm-2{width:16.666666666666664%}.col-sm-1{width:8.333333333333332%}}
+</style>`;
+    mywindow.document.write('<html><head>');
     mywindow.document.write(cdn);
+    mywindow.document.write(extraStyles);
     mywindow.document.write('</head><body>');
     mywindow.document.write(document.getElementById('printable').innerHTML);
     mywindow.document.write('</body></html>');
@@ -439,7 +443,7 @@ class Report extends Component {
     mywindow.document.close(); // necessary for IE >= 10
     mywindow.focus(); // necessary for IE >= 10*/
 
-    setTimeout(function () {
+    setTimeout(function() {
       mywindow.print();
       mywindow.close();
     }, 1000);
@@ -451,7 +455,7 @@ class Report extends Component {
     let { mockData, moduleName, actionName, formData, fieldErrors } = this.props;
     let { handleChange, getVal, addNewCard, removeCard, printer } = this;
 
-    const renderTable = function () {
+    const renderTable = function() {
       if (moduleName && actionName && formData && formData[objectName]) {
         var objectName = mockData[`${moduleName}.${actionName}`].objectName;
         let flag = 0;
@@ -485,10 +489,10 @@ class Report extends Component {
               i + 1,
               formData[objectName].documents[i].name || 'File',
               '<a href=/filestore/v1/files/id?tenantId=' +
-              localStorage.getItem('tenantId') +
-              '&fileStoreId=' +
-              formData[objectName].documents[i].fileStoreId +
-              '>Download</a>',
+                localStorage.getItem('tenantId') +
+                '&fileStoreId=' +
+                formData[objectName].documents[i].fileStoreId +
+                '>Download</a>',
             ]);
           }
         }
@@ -505,10 +509,10 @@ class Report extends Component {
           <Col xs={6} md={6}>
             <h3 style={{ paddingLeft: 15, marginBottom: '0' }}>
               {!_.isEmpty(mockData) &&
-                moduleName &&
-                actionName &&
-                mockData[`${moduleName}.${actionName}`] &&
-                mockData[`${moduleName}.${actionName}`].title
+              moduleName &&
+              actionName &&
+              mockData[`${moduleName}.${actionName}`] &&
+              mockData[`${moduleName}.${actionName}`].title
                 ? translate(mockData[`${moduleName}.${actionName}`].title)
                 : ''}
             </h3>
