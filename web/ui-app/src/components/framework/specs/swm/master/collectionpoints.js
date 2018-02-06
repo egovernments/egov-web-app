5564b79b786f917a20f42b3a91b672d92e15de97
venki <venki@egovernments.org>
2018-02-06 17:45:27 +0530

    Updated location as mandatory in collectionpoint update

--- web/ui-app/src/components/framework/specs/swm/master/collectionpoints.js
+++ web/ui-app/src/components/framework/specs/swm/master/collectionpoints.js
@@ -506,7 +506,7 @@ var dat = {
             "label": "",
             "hierarchyType": "REVENUE",
             "jsonPath": "collectionPoints[0].location.code",
-            "isRequired": false,
+            "isRequired": true,
             "patternErrorMsg": "",
             "multiple": true,
             "fullWidth": true,
