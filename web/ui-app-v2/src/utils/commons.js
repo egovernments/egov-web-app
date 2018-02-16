2d42517303cd971894c4da5c9e1898a477b94f5c
Varun Hegde <1hvarun@gmail.com>
2018-02-16 11:00:34 +0530

    removed retrieve specs folder

--- web/ui-app-v2/src/utils/commons.js
+++ web/ui-app-v2/src/utils/commons.js
@@ -2,7 +2,3 @@ export const prepareSearchUrl = (search, id) => {
   const { url: searchUrl, searchKey } = search;
   return `${searchUrl}?${searchKey}=${id}`;
 };
-
-export const retrieveSpecs = () => {
-  return window.localStorage.specs;
-};
