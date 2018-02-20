3fa7b519f33d03d570fc77af62c7825c704ed4ef
Varun Hegde <1hvarun@gmail.com>
2018-02-20 11:19:21 +0530

    renamed button story

--- web/ui-app-v2/.storybook/config.js
+++ web/ui-app-v2/.storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from "@storybook/react";
 
 function loadStories() {
-  require("../src/stories/index.js");
+  require("../src/stories/button.js");
 }
 
 configure(loadStories, module);
