a12bbcf717107aa74d011217f1aa0da055619f99
Varun Hegde <1hvarun@gmail.com>
2018-02-20 11:26:13 +0530

    moved theme config to config folder

---
+++ web/ui-app-v2/src/config/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+  fontFamily: "Lato, sans",
+  textColor: "rgba(0, 0, 0, 0.68)",
+  backgroundcolor: "#F7F7F7",
+  palette: {
+    primary1Color: "#009688",
+    primary2Color: "#f58720",
+    textColor: "#5f5c62",
+    canvasColor: "#F7F7F7",
+    borderColor: "#0288d1"
+  },
+  raisedButton: {
+    primaryColor: "#607D8B"
+  },
+  floatingActionButton: {
+    color: "#f58720"
+  }
+};
+
+export default theme;
--- web/ui-app-v2/src/stories/button.js
+++ web/ui-app-v2/src/stories/button.js
@@ -3,25 +3,7 @@ import { storiesOf, addDecorator } from "@storybook/react";
 import { muiTheme } from "storybook-addon-material-ui";
 import { action } from "@storybook/addon-actions";
 import { Button } from "../components";
-
-let theme = {
-  fontFamily: "Lato, sans",
-  textColor: "rgba(0, 0, 0, 0.68)",
-  backgroundcolor: "#F7F7F7",
-  palette: {
-    primary1Color: "#009688",
-    primary2Color: "#f58720",
-    textColor: "#5f5c62",
-    canvasColor: "#F7F7F7",
-    borderColor: "#0288d1"
-  },
-  raisedButton: {
-    primaryColor: "#607D8B"
-  },
-  floatingActionButton: {
-    color: "#f58720"
-  }
-};
+import theme from "../config/theme";
 
 storiesOf("Button", module)
   .addDecorator(muiTheme([theme]))
@@ -35,4 +17,5 @@ storiesOf("Button", module)
       label="Button with primary"
       onClick={action("clicked")}
     />
-  ));
+  ))
+  .add("with disabled", () => <Button disabled={true} label="I am disabled" />);
