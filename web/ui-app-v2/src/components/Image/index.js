0419b28e90ca45645b8306b2310649b7c9298572
Varun Hegde <1hvarun@gmail.com>
2018-02-23 13:07:19 +0530

    added circular images

--- web/ui-app-v2/src/components/Image/index.js
+++ web/ui-app-v2/src/components/Image/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./style.css";
 
-const Image = ({ source, height, width }) => {
+const Image = ({ className = "", style, source, height, width }) => {
+  const classNames = className.trim().length
+    ? `img-responsive ${className}`
+    : `img-responsive`;
   return (
     <img
-      className="img-responsive"
+      className={classNames}
+      style={style}
       src={source}
       height={height}
       width={width}
@@ -15,6 +19,8 @@ const Image = ({ source, height, width }) => {
 
 Image.propTypes = {
   source: PropTypes.string,
+  className: PropTypes.string,
+  style: PropTypes.object,
   width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   height: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
 };
