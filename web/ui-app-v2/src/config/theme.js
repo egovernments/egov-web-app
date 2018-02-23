9d5c73aa9e2a1ce59cbfcb6ef1667d1137dcbd1f
muralimgwl <murali.m@goodworklabs.com>
2018-02-23 14:36:08 +0530

    Updated config and theme

--- web/ui-app-v2/src/config/theme.js
+++ web/ui-app-v2/src/config/theme.js
@@ -5,11 +5,11 @@ const theme = {
   textColor: 'rgba(0, 0, 0, 0.68)',
   backgroundcolor: '#F7F7F7',
   palette: {
-    primary1Color: '#009688',
-    primary2Color: '#f58720',
-    textColor: '#5f5c62',
-    canvasColor: '#F7F7F7',
-    borderColor: grey300,
+    primary1Color: "#2f80ed",
+    primary2Color: "#f58720",
+    textColor: "#5f5c62",
+    canvasColor: "#F7F7F7",
+    borderColor: grey300
   },
   raisedButton: {
     primaryColor: '#607D8B',
