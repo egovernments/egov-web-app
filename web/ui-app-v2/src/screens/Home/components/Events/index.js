90a8e9d23ae71ffd157602de8848af20ff484b1b
Gyan4497 <gyanprakash92@gmail.com>
2018-02-26 15:54:14 +0530

    events block added

--- web/ui-app-v2/src/screens/Home/components/Events/index.js
+++ web/ui-app-v2/src/screens/Home/components/Events/index.js
@@ -1,8 +1,47 @@
 import React, { Component } from "react";
+import {Card} from "../../../../components";
+import ActionHome from "material-ui/svg-icons/action/home";
+import Location from "material-ui/svg-icons/maps/place";
+import Label from "../../../../components/Label";
+import LocationIcon from "material-ui/svg-icons/maps/place";
+
 
 class Events extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dateRange: "16 - 27",
+      month: "March",
+      eventName:"Amrita Shergill Retrospective",
+      location:"City Museum",
+      timing:"9 AM to 5 AM"
+    }
+  }
   render() {
-    return <div>Events</div>;
+    return (
+      <div>
+        <Card
+          card={{
+            style:{
+              backgroundColor:"#ffffff",
+            }
+          }}
+          textChildren={
+            <div className = "row">
+              <div className = "col-xs-2" style = {{background : "#8e44ad"}}>
+                <Label label={this.state.dateRange} style={{color: "#ffffff"}}/>
+                <Label label={this.state.month} style={{color: "#ffffff", backgroundColor:"#9b59b6"}}/>
+              </div>
+              <div className = "col-xs-10" style = {{}}>
+                <Label label={this.state.eventName} style={{}}/>
+                <Label label={this.state.location} labelPosition="after" icon={<LocationIcon/>}/>
+                <Label label={this.state.timing}  />
+              </div>
+            </div>
+          }
+        />
+      </div>
+    )
   }
 }
 
