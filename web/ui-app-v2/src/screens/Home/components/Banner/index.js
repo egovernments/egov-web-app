7f36b1fe666be89a6004c323846c2262b0e02920
Varun Hegde <1hvarun@gmail.com>
2018-02-26 14:03:13 +0530

    banner

--- web/ui-app-v2/src/screens/Home/components/Banner/index.js
+++ web/ui-app-v2/src/screens/Home/components/Banner/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import { Image } from "../../../../components";
 
+const styles = {
+  logo: {
+    display: "block",
+    margin: "0 auto",
+  },
+};
+
 class Banner extends Component {
   render() {
     return (
-      <div>
+      <div className="row">
         <Image source="https://placeimg.com/600/300/arch" />
       </div>
     );
