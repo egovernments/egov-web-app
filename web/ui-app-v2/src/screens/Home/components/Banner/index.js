e2e99a4ccd7fbb2e19d0662cef9bc6bbc9ad5c06
Varun Hegde <1hvarun@gmail.com>
2018-02-26 15:15:40 +0530

    added overlay

--- web/ui-app-v2/src/screens/Home/components/Banner/index.js
+++ web/ui-app-v2/src/screens/Home/components/Banner/index.js
@@ -7,6 +7,14 @@ const styles = {
     height: `200px`,
     backgroundImage: `url(https://amedia.britannica.com/700x450/53/176353-004-58581F75.jpg)`,
   },
+  overlay: {
+    position: "absolute",
+    left: "0px",
+    top: "0px",
+    backgroundColor: "rgba(0,0,0,0.4)",
+    width: "100%",
+    height: "200px",
+  },
   logo: {
     position: "absolute",
     left: "40%",
@@ -18,7 +26,7 @@ class Banner extends Component {
   render() {
     return (
       <div style={styles.container} className="row">
-        <div />
+        <div style={styles.overlay} />
         <Image style={styles.logo} circular={true} source="https://placeimg.com/100/100/tech" />
       </div>
     );
