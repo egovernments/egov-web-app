32332c4935723e739a643283a0e9224660970f14
BabikaB <babika.b@goodworklabs.com>
2018-02-27 18:19:52 +0530

    added status completed in complaint details

--- web/ui-app-v2/src/screens/ComplaintDetails/components/Status/index.js
+++ web/ui-app-v2/src/screens/ComplaintDetails/components/Status/index.js
@@ -1,51 +1,61 @@
 import React, { Component } from "react";
-import {Card} from "../../../../components";
+import { Card } from "../../../../components";
 import Info from "material-ui/svg-icons/action/info";
+import Check from "material-ui/svg-icons/navigation/check";
 import "./index.css";
 
 const iconStyle = {
-  margin:"10%",
+  margin: "10%",
   // marginRight: 24,
   height: "50px",
   width: "50px",
+
   // textAlign:"center"
 };
+const checkStyle = {
+  margin: "10%",
+  height: "50px",
+  width: "50px",
+  borderRadius: "50%",
+  padding: "12px",
+  background: "#ffffff",
+};
 
-const StatusIcon=({status})=> {
+const StatusIcon = ({ status }) => {
   switch (status) {
     case "PROCESS":
-      return <Info style={iconStyle} color={"#ffffff"} />
+      return <Info style={iconStyle} color={"#ffffff"} />;
+    case "COMPLETED":
+      return <Check style={checkStyle} color={"#7ed321"} />;
     default:
-      return <Info style={iconStyle} color={"#ffffff"} />
-
+      return <Info style={iconStyle} color={"#ffffff"} />;
   }
-}
-
+};
 
 class Status extends Component {
-
   render() {
-    let {status,bgColor,message}=this.props;
+    let { status, bgColor, message } = this.props;
     return (
       <div className="status">
-      <Card
-        card={{
-          style:{
-            backgroundColor:"#ffffff"
+        <Card
+          card={{
+            style: {
+              backgroundColor: "#ffffff",
+            },
+          }}
+          textChildren={
+            <div className="wrapper">
+              <div className="left" style={{ backgroundColor: bgColor }}>
+                <StatusIcon status={status} />
+              </div>
+              <div className="right" style={{ textAlign: "center" }}>
+                {message}
+              </div>
+            </div>
           }
-        }}
-        textChildren={
-          <div className="wrapper">
-            <div className="left" style={{backgroundColor:bgColor}}>
-                  <StatusIcon status={status}/>
-            </div>
-            <div className="right" style={{textAlign:"center"}}>
-                  {message}
-            </div>
-          </div>
-        }
-      />
-    </div>);
+        />
+      </div>
+    );
   }
 }
 
--- web/ui-app-v2/src/screens/ComplaintDetails/index.js
+++ web/ui-app-v2/src/screens/ComplaintDetails/index.js
@@ -6,24 +6,29 @@ import Comments from "./components/Comments";
 
 import "./index.css";
 
-
-
 class ComplaintDetails extends Component {
-  state= {
-    status:{
-      status:"PROCESS",
-      message:"Your application has been assigned to JR.INSPECTOR - J KUMAR & is under process",
-      bgColor:"#f5a623"
-    }
-  }
+  state = {
+    status: {
+      // status:"PROCESS",
+      // message:"Your application has been assigned to JR.INSPECTOR - J KUMAR & is under process",
+      // bgColor:"#f5a623"
+      status: "COMPLETED",
+      message: (
+        <div>
+          <span>THANK YOU </span> <br /> <span>for your Application!</span>
+        </div>
+      ),
+      bgColor: "#7ed321",
+    },
+  };
   render() {
-    let { status,details,timeLine,comments} = this.state;
+    let { status, details, timeLine, comments } = this.state;
     return (
       <div className="col-lg-offset-2 col-md-offset-2 col-md-8 col-lg-8 zeroPadding">
-        <Status {...status}/>
-        <Details {...details}/>
-        <ComplaintTimeLine timeLine={timeLine}/>
-        <Comments comments={comments}/>
+        <Status {...status} />
+        <Details {...details} />
+        <ComplaintTimeLine timeLine={timeLine} />
+        <Comments comments={comments} />
       </div>
     );
   }
