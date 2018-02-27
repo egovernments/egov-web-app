c401fe124883d8c7ab8bc89b27a3ecf7f2a413df
Shreya <shreya.b@egovernments.org>
2018-02-27 12:22:32 +0530

    Enhanced Loading Indicator

--- web/ui-app-v2/src/components/LoadingIndicator/index.js
+++ web/ui-app-v2/src/components/LoadingIndicator/index.js
@@ -1,35 +1,17 @@
 import React from "react";
 import RefreshIndicator from "material-ui/RefreshIndicator";
 
-const style = {
-  container: {
-    height: "100%",
-    width: "100%",
-    position: "fixed",
-    backgroundColor: "rgba(189,189,189,0.5)",
-    zIndex: 9998,
-    left: 0,
-    top: 0,
-  },
-  containerHide: {
-    display: "none",
-    position: "relative",
-  },
-  refresh: {
-    display: "block",
-    position: "relative",
-    zIndex: 9999,
-    marginLeft: "48%",
-    marginTop: "23%",
-    color: "#FF9800",
-  },
-};
 
-const LoadingIndicator = ({ status }) => {
-  console.log("Hi...." + status);
+const LoadingIndicator = ({ status , loadingColor, size , left, top , style }) => {
   return (
     <div style={status === "hide" ? style.containerHide : style.container}>
-      <RefreshIndicator size={40} left={10} top={0} status="loading" style={style.refresh} />
+      <RefreshIndicator 
+      size={40} 
+      left={50} 
+      top={0} 
+      status="loading"
+      loadingColor={loadingColor}
+      style={style} />
     </div>
   );
 };
