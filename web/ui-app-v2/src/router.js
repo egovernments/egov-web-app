b80d0a0c6c9e297debb3dc7893a9a568d3f815e2
Varun Hegde <1hvarun@gmail.com>
2018-02-28 14:13:06 +0530

    added url base

--- web/ui-app-v2/src/router.js
+++ web/ui-app-v2/src/router.js
@@ -8,16 +8,19 @@ import Login from "./screens/User/Login";
 import OTP from "./screens/User/OTP";
 import MapDemo from "./screens/common/MapDemo";
 
+const urlParts = window.location.pathname.split("/");
+const base = urlParts.slice(0, urlParts.length - 1).join("/");
+
 const Main = () => {
   return (
     <main>
       <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/otp" component={OTP} />
-        <PrivateRoute exact path="/" component={Home} />
-        <PrivateRoute exact path="/my-complaints" component={MyComplaints} />
-        <PrivateRoute exact path="/complaint-details" component={ComplaintDetails} />
-        <Route exact path="/map" component={MapDemo} />
+        <Route exact path={`${base}/login`} component={Login} />
+        <Route exact path={`${base}/otp`} component={OTP} />
+        <PrivateRoute exact path={`${base}/`} component={Home} />
+        <PrivateRoute exact path={`${base}/my-complaints`} component={MyComplaints} />
+        <PrivateRoute exact path={`${base}/complaint-details`} component={ComplaintDetails} />
+        <Route exact path={`${base}/map`} component={MapDemo} />
       </Switch>
     </main>
   );
