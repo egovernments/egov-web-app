727798355532adf946e782a8191f760c0354a60a
Varun Hegde <1hvarun@gmail.com>
2018-02-28 12:08:22 +0530

    moved assets locally

--- web/ui-app-v2/src/screens/User/Login.js
+++ web/ui-app-v2/src/screens/User/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
 import { Button, Image, DropDown, TextField, Card } from "../../components";
+import logoMuncipal from "../../assets/images/logo-muncipal.png";
+import bannerMuncipal from "../../assets/images/banner-muncipal.png";
 
 const styles = {
   logo: {
@@ -11,7 +13,7 @@ const styles = {
   },
   imageContainer: {
     position: "relative",
-    backgroundImage: `url(https://i.imgur.com/7M1osdL.png)`,
+    backgroundImage: `url(${bannerMuncipal})`,
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center",
     height: "300px",
@@ -42,11 +44,7 @@ class Login extends Component {
         <Card
           textChildren={
             <div>
-              <Image
-                style={styles.logo}
-                circular={true}
-                source="https://lh3.googleusercontent.com/wD-6OqDafrx6DAgJDGarnGF4B6KrG7EGnHBAiWr18drml_IntNgyibMv17OxqifPYb88Y7WQyTCpEmTF7L1PQ3uePpsNuXILtPDl-GKcXqEh3BpSENQEfKT9xxqWTQulUnFD-VpXI9NJtxZYzSnRR9qCX2YPTGdXk0mlMfK8qjBY03-prrF10NzjN3zoAy6XMf8ph40KmWkEAEvzzbn6vtmPiy-st2JYtI6lRi0Tfd4xj74hMn17BNOXF6-evRGjyW8nSdpy21kQ3Oe1Nnt4Z80nt0-2-CAWN5FoMw_IsWkq5ydIA0J0qaYEgHOT_Y9DeOyIogQ7F0yuFwo8JjZWgcw82SOel-oKcDkjzFQwdj2JiJmuiDx64HGTPt6i_VvwWwzTDaDV0jL3rJdAH59e49U5Ffr3xVNF1CvI_77jE0R4S6ck_6ZAlv7bbNjQRUS9N1B-SG7d10cBMcytAhZVyQDPvBXUqxcRl1heJXuwhKufTMb_UbVBoogTIE7HgvR5Zv7zQ0PDqz_MgwemZJ_6Kp5xyGxgwA4V-r6MsaeqzccWoevaTf_lHwt6fA=w1280-h703"
-              />
+              <Image style={styles.logo} circular={true} source={`${logoMuncipal}`} />
               <form>
                 <TextField className="textfield" name="name" id="name" fullWidth={true} placeholder="Name" />
                 <DropDown value={1} name="cities" dropDownData={dropDownData} fullWidth={true} />
--- web/ui-app-v2/src/screens/User/OTP.js
+++ web/ui-app-v2/src/screens/User/OTP.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
 import { Button, Image, Label, TextField, Card } from "../../components";
+import logoMuncipal from "../../assets/images/logo-muncipal.png";
+import bannerMuncipal from "../../assets/images/banner-muncipal.png";
 
 const styles = {
   logo: {
@@ -14,7 +16,7 @@ const styles = {
   },
   imageContainer: {
     position: "relative",
-    backgroundImage: `url(https://i.imgur.com/7M1osdL.png)`,
+    backgroundImage: `url(${bannerMuncipal})`,
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center",
     height: "300px",
@@ -38,11 +40,7 @@ class OTP extends Component {
         <Card
           textChildren={
             <div>
-              <Image
-                style={styles.logo}
-                circular={true}
-                source="https://lh3.googleusercontent.com/wD-6OqDafrx6DAgJDGarnGF4B6KrG7EGnHBAiWr18drml_IntNgyibMv17OxqifPYb88Y7WQyTCpEmTF7L1PQ3uePpsNuXILtPDl-GKcXqEh3BpSENQEfKT9xxqWTQulUnFD-VpXI9NJtxZYzSnRR9qCX2YPTGdXk0mlMfK8qjBY03-prrF10NzjN3zoAy6XMf8ph40KmWkEAEvzzbn6vtmPiy-st2JYtI6lRi0Tfd4xj74hMn17BNOXF6-evRGjyW8nSdpy21kQ3Oe1Nnt4Z80nt0-2-CAWN5FoMw_IsWkq5ydIA0J0qaYEgHOT_Y9DeOyIogQ7F0yuFwo8JjZWgcw82SOel-oKcDkjzFQwdj2JiJmuiDx64HGTPt6i_VvwWwzTDaDV0jL3rJdAH59e49U5Ffr3xVNF1CvI_77jE0R4S6ck_6ZAlv7bbNjQRUS9N1B-SG7d10cBMcytAhZVyQDPvBXUqxcRl1heJXuwhKufTMb_UbVBoogTIE7HgvR5Zv7zQ0PDqz_MgwemZJ_6Kp5xyGxgwA4V-r6MsaeqzccWoevaTf_lHwt6fA=w1280-h703"
-              />
+              <Image style={styles.logo} circular={true} source={`${logoMuncipal}`} />
               <span className="otp-text">
                 We have sent a 4 digit OTP number to your registered mobile number. Enter the OTP to create your account.
               </span>
