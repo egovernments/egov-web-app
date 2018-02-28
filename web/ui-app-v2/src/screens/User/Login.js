34fd4c27c49573a0b9b6cfe7af42d12074efb91f
Varun Hegde <1hvarun@gmail.com>
2018-02-28 08:32:23 +0530

    login, otp few style fixes

--- web/ui-app-v2/src/screens/User/Login.js
+++ web/ui-app-v2/src/screens/User/Login.js
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
-import RaisedButton from "material-ui/RaisedButton";
-import Paper from "material-ui/Paper";
-import DropDown from "../../components/DropDown";
-import Image from "../../components/Image";
-import TextField from "../../components/TextField";
-import Button from "../../components/Button";
+import { Button, Image, DropDown, TextField, Card } from "../../components";
 
 const styles = {
   logo: {
@@ -20,9 +15,6 @@ const styles = {
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center",
     height: "300px",
-  },
-  formContainer: {
-    padding: "10px",
   },
 };
 
@@ -45,21 +37,25 @@ class Login extends Component {
   render() {
     const { login, dropDownData } = this;
     return (
-      <div className="col-xs-12 col-lg-4 col-sm-6 col-md-4 col-lg-offset-4 col-sm-offset-3 col-md-offset-4">
+      <div className="col-xs-12 col-lg-6 col-sm-6 col-md-6 col-lg-offset-3 col-sm-offset-3 col-md-offset-3">
         <div style={styles.imageContainer} />
-        <Paper style={styles.formContainer} zDepth={1}>
-          <Image
-            style={styles.logo}
-            circular={true}
-            source="https://lh3.googleusercontent.com/wD-6OqDafrx6DAgJDGarnGF4B6KrG7EGnHBAiWr18drml_IntNgyibMv17OxqifPYb88Y7WQyTCpEmTF7L1PQ3uePpsNuXILtPDl-GKcXqEh3BpSENQEfKT9xxqWTQulUnFD-VpXI9NJtxZYzSnRR9qCX2YPTGdXk0mlMfK8qjBY03-prrF10NzjN3zoAy6XMf8ph40KmWkEAEvzzbn6vtmPiy-st2JYtI6lRi0Tfd4xj74hMn17BNOXF6-evRGjyW8nSdpy21kQ3Oe1Nnt4Z80nt0-2-CAWN5FoMw_IsWkq5ydIA0J0qaYEgHOT_Y9DeOyIogQ7F0yuFwo8JjZWgcw82SOel-oKcDkjzFQwdj2JiJmuiDx64HGTPt6i_VvwWwzTDaDV0jL3rJdAH59e49U5Ffr3xVNF1CvI_77jE0R4S6ck_6ZAlv7bbNjQRUS9N1B-SG7d10cBMcytAhZVyQDPvBXUqxcRl1heJXuwhKufTMb_UbVBoogTIE7HgvR5Zv7zQ0PDqz_MgwemZJ_6Kp5xyGxgwA4V-r6MsaeqzccWoevaTf_lHwt6fA=w1280-h703"
-          />
-          <form>
-            <TextField className="textfield" id="name" fullWidth={true} placeholder="Name" />
-            <DropDown value={1} dropDownData={dropDownData} fullWidth={true} />
-            <TextField className="textfield" id="phone-number" underlineShow={false} fullWidth={true} placeholder="Phone Number" />
-            <Button onClick={login} primary={true} label="Submit" fullWidth={true} />
-          </form>
-        </Paper>
+        <Card
+          textChildren={
+            <div style={{ padding: "10px" }}>
+              <Image
+                style={styles.logo}
+                circular={true}
+                source="https://lh3.googleusercontent.com/wD-6OqDafrx6DAgJDGarnGF4B6KrG7EGnHBAiWr18drml_IntNgyibMv17OxqifPYb88Y7WQyTCpEmTF7L1PQ3uePpsNuXILtPDl-GKcXqEh3BpSENQEfKT9xxqWTQulUnFD-VpXI9NJtxZYzSnRR9qCX2YPTGdXk0mlMfK8qjBY03-prrF10NzjN3zoAy6XMf8ph40KmWkEAEvzzbn6vtmPiy-st2JYtI6lRi0Tfd4xj74hMn17BNOXF6-evRGjyW8nSdpy21kQ3Oe1Nnt4Z80nt0-2-CAWN5FoMw_IsWkq5ydIA0J0qaYEgHOT_Y9DeOyIogQ7F0yuFwo8JjZWgcw82SOel-oKcDkjzFQwdj2JiJmuiDx64HGTPt6i_VvwWwzTDaDV0jL3rJdAH59e49U5Ffr3xVNF1CvI_77jE0R4S6ck_6ZAlv7bbNjQRUS9N1B-SG7d10cBMcytAhZVyQDPvBXUqxcRl1heJXuwhKufTMb_UbVBoogTIE7HgvR5Zv7zQ0PDqz_MgwemZJ_6Kp5xyGxgwA4V-r6MsaeqzccWoevaTf_lHwt6fA=w1280-h703"
+              />
+              <form>
+                <TextField className="textfield" id="name" fullWidth={true} placeholder="Name" />
+                <DropDown value={1} dropDownData={dropDownData} fullWidth={true} />
+                <TextField className="textfield" id="phone-number" underlineShow={false} fullWidth={true} placeholder="Phone Number" />
+                <Button onClick={login} primary={true} label="Submit" fullWidth={true} />
+              </form>
+            </div>
+          }
+        />
       </div>
     );
   }
--- web/ui-app-v2/src/screens/User/OTP.js
+++ web/ui-app-v2/src/screens/User/OTP.js
@@ -1,25 +1,23 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
-import Paper from "material-ui/Paper";
-import TextField from "../../components/TextField";
-import Button from "../../components/Button";
-import Image from "../../components/Image";
-import Label from "../../components/Label";
+import { Button, Image, Label, TextField, Card } from "../../components";
 
 const styles = {
   logo: {
     margin: "0 auto",
     display: "block",
+    width: "100px",
+    height: "100px",
   },
   resendOTP: {
     float: "right",
   },
-  formContainer: {
-    padding: "10px",
-    margin: "0px 2px",
-    zIndex: 30,
-    // why 65? ~= imageHeight/2 + paddding + bottomMargin
-    marginTop: "-65px",
+  imageContainer: {
+    position: "relative",
+    backgroundImage: `url(https://i.imgur.com/7M1osdL.png)`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    height: "300px",
   },
 };
 
@@ -35,22 +33,31 @@ class OTP extends Component {
   render() {
     const { onOtpSubmit } = this;
     return (
-      <div className="col-xs-12 col-lg-4 col-sm-4 col-md-4 col-lg-offset-4 col-sm-offset-4 col-md-offset-4">
-        <Image source="https://i.imgur.com/7M1osdL.png" />
-        <Paper style={styles.formContainer} zDepth={1}>
-          <Image style={styles.logo} circular={true} source="https://placeimg.com/100/100/tech" />
+      <div className="col-xs-12 col-lg-6 col-sm-6 col-md-6 col-lg-offset-3 col-sm-offset-3 col-md-offset-3">
+        <div style={styles.imageContainer} />
+        <Card
+          textChildren={
+            <div style={{ padding: "10px" }}>
+              <Image
+                style={styles.logo}
+                circular={true}
+                source="https://lh3.googleusercontent.com/wD-6OqDafrx6DAgJDGarnGF4B6KrG7EGnHBAiWr18drml_IntNgyibMv17OxqifPYb88Y7WQyTCpEmTF7L1PQ3uePpsNuXILtPDl-GKcXqEh3BpSENQEfKT9xxqWTQulUnFD-VpXI9NJtxZYzSnRR9qCX2YPTGdXk0mlMfK8qjBY03-prrF10NzjN3zoAy6XMf8ph40KmWkEAEvzzbn6vtmPiy-st2JYtI6lRi0Tfd4xj74hMn17BNOXF6-evRGjyW8nSdpy21kQ3Oe1Nnt4Z80nt0-2-CAWN5FoMw_IsWkq5ydIA0J0qaYEgHOT_Y9DeOyIogQ7F0yuFwo8JjZWgcw82SOel-oKcDkjzFQwdj2JiJmuiDx64HGTPt6i_VvwWwzTDaDV0jL3rJdAH59e49U5Ffr3xVNF1CvI_77jE0R4S6ck_6ZAlv7bbNjQRUS9N1B-SG7d10cBMcytAhZVyQDPvBXUqxcRl1heJXuwhKufTMb_UbVBoogTIE7HgvR5Zv7zQ0PDqz_MgwemZJ_6Kp5xyGxgwA4V-r6MsaeqzccWoevaTf_lHwt6fA=w1280-h703"
+              />
+              <span className="otp-text">
+                We have sent a 4 digit OTP number to your registered mobile number. Enter the OTP to create your account.
+              </span>
 
-          <span className="otp-text">We have sent a 4 digit OTP number to your registered mobile number. Enter the OTP to create your account.</span>
-
-          <form>
-            <TextField className="textfield" id="otp" fullWidth={true} placeholder="Enter OTP" />
-            <div style={styles.resendOTP}>
-              <Label label="Didn't recieve OTP?" />
-              <Label primary={true} label="resend" />
+              <form>
+                <TextField className="textfield" id="otp" fullWidth={true} placeholder="Enter OTP" />
+                <div style={styles.resendOTP}>
+                  <Label label="Didn't recieve OTP?" />
+                  <Label primary={true} label="resend" />
+                </div>
+                <Button onClick={onOtpSubmit} primary={true} label="Start" fullWidth={true} />
+              </form>
             </div>
-            <Button onClick={onOtpSubmit} primary={true} label="Start" fullWidth={true} />
-          </form>
-        </Paper>
+          }
+        />
       </div>
     );
   }
