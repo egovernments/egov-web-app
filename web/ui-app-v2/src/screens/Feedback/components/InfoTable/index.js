4b547c3dc76dc03c9d9b68dbbf26a914ba56aee9
sushant gwl <sushant.bhaskar@goodworklabs.com>
2018-03-06 14:59:53 +0530

    Feedback Screen Changes and InfoTable Component

--- web/ui-app-v2/src/screens/Feedback/components/InfoTable/index.js
+++ web/ui-app-v2/src/screens/Feedback/components/InfoTable/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import { InfoTable } from "../../../../components";
 import "./index.css";
 
-const labelParentClass = "feedback-info-label-container";
-const valueParentClass = "feedback-info-value-container";
-const parentClass = "feedback-info-item-container";
+const labelContainer = "feedback-info-label-container";
+const valueContainer = "feedback-info-value-container";
+const itemContainer = "feedback-info-item-container";
 const InfoTableComponent = ({ items }) => {
-  return <InfoTable items={items} labelParentClass={labelParentClass} valueParentClass={valueParentClass} parentClass={parentClass} />;
+  return <InfoTable items={items} labelContainer={labelContainer} valueContainer={valueContainer} itemContainer={itemContainer} />;
 };
 
 export default InfoTableComponent;
