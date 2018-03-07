cef6b604813a47c0943e1e509c8ad58a10b81653
Shreya <shreya.b@egovernments.org>
2018-03-07 17:52:53 +0530

    Modified Feedback Screen styles

--- web/ui-app-v2/src/screens/Feedback/components/ButtonToggle/index.js
+++ web/ui-app-v2/src/screens/Feedback/components/ButtonToggle/index.js
@@ -4,24 +4,31 @@ import { ButtonGroup } from "../../../../components";
 
 const defaultBGColor = "transparent";
 const BGColor = "#f5a623";
+
 const labelStyle = {
   textTransform: "none",
-  fontWeight: "900",
+  fontWeight: "500",
   color: "#ffffff",
+  fontSize: "14.5px",
+  letterSpacing: "0.7px",
+  padding: "0 14px",
 };
 
 const defaultStyle = {
   border: "1px solid #f5a623",
   borderRadius: "3px",
-  marginRight: "5px",
+  marginRight: "4.44%",
   height: "auto",
   lineHeight: "30px",
 };
 
 const defaultLabelStyle = {
   textTransform: "none",
-  fontWeight: "900",
+  fontWeight: "500",
   color: "#484848",
+  fontSize: "14.5px",
+  letterSpacing: "0.7px",
+  padding: "0 14px",
 };
 
 const ButtonToggleComponent = ({ items, onClick, selected }) => {
