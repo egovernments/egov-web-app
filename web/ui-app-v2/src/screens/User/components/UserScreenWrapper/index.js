fc96b706d98be5fe1c82416a91bf9aba148719b2
Varun Hegde <1hvarun@gmail.com>
2018-03-07 18:16:32 +0530

    removed the warnings

--- web/ui-app-v2/src/screens/User/components/UserScreenWrapper/index.js
+++ web/ui-app-v2/src/screens/User/components/UserScreenWrapper/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 
 const UserScreensWrapper = ({ children, className = "" }) => {
   return (
-    <div className={`${className} user-screens-wrapper col-xs-12 col-lg-6 col-sm-6 col-md-6 col-lg-offset-3 col-sm-offset-3 col-md-offset-3`} a>
+    <div className={`${className} user-screens-wrapper col-xs-12 col-lg-6 col-sm-6 col-md-6 col-lg-offset-3 col-sm-offset-3 col-md-offset-3`}>
       <div className="imageContainer" />
       <div className="cardBackground" />
       <div className="logo-container">
