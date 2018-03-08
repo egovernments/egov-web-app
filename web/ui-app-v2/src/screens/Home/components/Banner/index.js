314dbf9a5a317d949a70902f60b363bf59aff4c0
Varun Hegde <1hvarun@gmail.com>
2018-03-08 10:30:40 +0530

    changed homepage banner

--- web/ui-app-v2/src/screens/Home/components/Banner/index.js
+++ web/ui-app-v2/src/screens/Home/components/Banner/index.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router";
 import { Image } from "../../../../components";
-import logoMuncipal from "../../../../assets/images/logo-muncipal.png";
-
+import logoMuncipal from "../../../../assets/images/PB logo.png";
+import logoMseva from "../../../../assets/images/Mseva logo.png";
 import "./index.css";
 
-class Banner extends Component {
-  render() {
-    return (
-      <div className="banner">
-        <Image className="logo" circular={true} source={logoMuncipal} />
+const Banner = ({ children, className = "" }) => {
+  return (
+    <div className={`${className} home-screen-wrapper user-screens-wrapper col-xs-12`}>
+      <div className="row">
+      <div className="imageContainer" />
+      <div className="cardBackground" />
+      <div className="logo-container">
+        <Image className="logo" circular={true} source={`${logoMuncipal}`} />
       </div>
-    );
-  }
-}
+       {children}
+      </div>
+    </div>
+  );
+};
 
 export default Banner;
