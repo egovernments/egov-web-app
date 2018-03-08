57915329afba7095dfd260e8a2ccda7bc38f13ce
Gyan4497 <gyanprakash92@gmail.com>
2018-03-08 14:22:54 +0530

    CHanges for Map track location page

--- web/ui-app-v2/src/router.js
+++ web/ui-app-v2/src/router.js
@@ -5,7 +5,9 @@ import Home from "./screens/Home";
 import MyComplaints from "./screens/MyComplaints";
 import ComplaintDetails from "./screens/ComplaintDetails";
 import Profile from "./screens/profile";
-import MapDemo from "./screens/common/MapDemo";
+import Login from "./screens/User/Login";
+import OTP from "./screens/User/OTP";
+import TrackLocation from "./screens/common/TrackLocation";
 import Feedback from "./screens/Feedback";
 import LanguageSelection from "./screens/LanguageSelection";
 import ReOpenComplaint from "./screens/ReOpenComplaint";
@@ -26,7 +28,7 @@ const Main = () => {
         <PrivateRoute exact path="/" component={Home} />
         <PrivateRoute exact path="/my-complaints" component={MyComplaints} />
         <PrivateRoute exact path="/complaint-details" component={ComplaintDetails} />
-        <PrivateRoute exact path="/map" component={MapDemo} />
+        <PrivateRoute exact path="/map" component={TrackLocation} />
         <PrivateRoute exact path="/profile" component={Profile} />
         <PrivateRoute exact path="/feedback" component={Feedback} />
         <PrivateRoute exact path="/reopen-complaint" component={ReOpenComplaint} />
