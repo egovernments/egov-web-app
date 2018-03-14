8dc3f2d7fd71aed761b1d161f12b2c18191e0065
sushant gwl <sushant.bhaskar@goodworklabs.com>
2018-03-14 14:01:49 +0530

    commit message

--- web/ui-app-v2/src/components/Button/index.js
+++ web/ui-app-v2/src/components/Button/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import RaisedButton from "material-ui/RaisedButton";
-//
+
 const Button = (props) => {
   let { label, icon = {}, className, onClick, backgroundColor, labelColor, fullWidth, disabled, primary = false, style = {}, id } = props;
   return (
