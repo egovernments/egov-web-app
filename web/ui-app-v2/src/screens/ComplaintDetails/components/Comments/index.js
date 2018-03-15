ff06c2499cf603f2d134e9b6fdceed01ca3bb5de
muralimgwl <murali.m@goodworklabs.com>
2018-03-15 13:23:02 +0530

    Partial complete of complaint details

--- web/ui-app-v2/src/screens/ComplaintDetails/components/Comments/index.js
+++ web/ui-app-v2/src/screens/ComplaintDetails/components/Comments/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, List, Label, Icon} from "../../../../components";
+import { Card, List, Label, Icon } from "../../../../components";
 import WriteComment from "../WriteComment";
 import Avatar from "material-ui/Avatar";
 import faceOne from "../../../../assets/images/faceOne.jpg";
@@ -8,14 +8,34 @@ import "./index.css";
 
 const itemsOne = [
   {
-    leftAvatar:<div> <Avatar size={33} src={faceOne} /></div>,
-    primaryText: <div className="complaint-details-comments-section"><Label containerStyle={{marginBottom:"8px"}} labelStyle={{color:"#464646"}} label="please sterilize the dogs in the area."/> <Label labelClassName="rainmaker-small-font" label="11-MAR-18"/> </div>
-  }
+    leftAvatar: (
+      <div>
+        {" "}
+        <Avatar size={33} src={faceOne} />
+      </div>
+    ),
+    primaryText: (
+      <div className="complaint-details-comments-section">
+        <Label containerStyle={{ marginBottom: "8px" }} labelStyle={{ color: "#464646" }} label="please sterilize the dogs in the area." />{" "}
+        <Label labelClassName="rainmaker-small-font" label="11-MAR-18" />{" "}
+      </div>
+    ),
+  },
 ];
 
 const itemsTwo = [
   {
-    primaryText:<div className="complaint-details-comments-section"> <Label containerStyle={{marginBottom:"8px"}} labelStyle={{color:"#464646"}} label="Sterilization is scheduled in March. We are doing our best to resolve your issue at this time." /><Label labelClassName="rainmaker-small-font" label="11-MAR-18" /></div>,
+    primaryText: (
+      <div className="complaint-details-comments-section">
+        {" "}
+        <Label
+          containerStyle={{ marginBottom: "8px" }}
+          labelStyle={{ color: "#464646" }}
+          label="Sterilization is scheduled in March. We are doing our best to resolve your issue at this time."
+        />
+        <Label labelClassName="rainmaker-small-font" label="11-MAR-18" />
+      </div>
+    ),
     rightAvatar: <Avatar size={33} src={faceTwo} />,
   },
 ];
@@ -25,13 +45,14 @@ class Comments extends Component {
     return (
       <div>
         <Card
-        style={{
-            paddingBottom: "0px"
-        }}
+          style={{
+            paddingBottom: "0px",
+          }}
           textChildren={
             <div>
               <div className="rainmaker-displayInline">
-                <Icon action="communication" name="forum" color="#969696" />   <Label label="Comments" containerStyle={{marginLeft:"13px"}} labelClassName="dark-heading"/>
+                <Icon action="communication" name="forum" color="#969696" />{" "}
+                <Label label="Comments" containerStyle={{ marginLeft: "13px" }} labelClassName="dark-heading" />
               </div>
               <List listItemStyle={{ marginBottom: "-8.5px" }} items={itemsOne} />
               <List listItemStyle={{ marginBottom: "-8.5px" }} items={itemsTwo} />
--- web/ui-app-v2/src/screens/ComplaintDetails/components/ComplaintTimeLine/index.js
+++ web/ui-app-v2/src/screens/ComplaintDetails/components/ComplaintTimeLine/index.js
@@ -1,30 +1,29 @@
 import React, { Component } from "react";
-import { Card, TimeLine, List, Label,Icon,Image } from "../../../../components";
+import { Card, TimeLine, List, Label, Icon, Image } from "../../../../components";
 import garbageOne from "../../../../assets/images/Garbage_3.jpg";
 import "./index.css";
 
-const timelineIconCommonStyle ={
+const timelineIconCommonStyle = {
   height: "38px",
   width: "38px",
   borderRadius: "50%",
   padding: "7px",
-  marginLeft:"-7px",
-}
+  marginLeft: "-7px",
+};
 
 const statusCommonIconStyle = {
   ...timelineIconCommonStyle,
   backgroundColor: "#ffffff",
   boxShadow: "0 2px 2px 0 rgba(0, 0, 0, 0.24), 0 0 2px 0 rgba(0, 0, 0, 0.12)",
-  border: "solid 1px #f89a3f"
+  border: "solid 1px #f89a3f",
 };
 
 const statusResolvedIconStyle = {
   ...timelineIconCommonStyle,
   backgroundColor: "#22b25f",
   boxShadow: "0 2px 4px 0 rgba(0, 0, 0, 0.22)",
-  border: "solid 1px #22b25f"
+  border: "solid 1px #22b25f",
 };
-
 
 const callIconStyle = {
   marginRight: "7px",
@@ -33,113 +32,125 @@ const callIconStyle = {
   borderRadius: "50%",
 };
 
-
-
-const StatusIcon =({status})=>{
+const StatusIcon = ({ status }) => {
   switch (status) {
     case "SUBMITTED":
-        return <Icon action="custom" name="file-send" style={statusCommonIconStyle} color={"#f5a623"} />;
+      return <Icon action="custom" name="file-send" style={statusCommonIconStyle} color={"#f5a623"} />;
     case "ASSIGNED":
-        return <Icon action="custom" name="file-plus" style={statusCommonIconStyle} color={"#f5a623"} />;
+      return <Icon action="custom" name="file-plus" style={statusCommonIconStyle} color={"#f5a623"} />;
     case "REJECTED":
-        return <Icon action="content" name="clear" style={statusCommonIconStyle} color={"#f5a623"} />;
+      return <Icon action="content" name="clear" style={statusCommonIconStyle} color={"#f5a623"} />;
     default:
-        return <Icon action="action" name="done"style={statusResolvedIconStyle} color={"#FFFFFF"} />;
+      return <Icon action="action" name="done" style={statusResolvedIconStyle} color={"#FFFFFF"} />;
   }
-}
+};
 
-const StatusContent =({status,currentStatus,content}) =>{
+const StatusContent = ({ status, currentStatus, content }) => {
   switch (status) {
     case "SUBMITTED":
-        var {date,resolveImage,resolveFeedback}=content;
-        return (
-          <div className="complaint-timeline-content-section">
-              <Label labelClassName="rainmaker-small-font" label={date || "11-Mar-18"}/>
-              <Label labelClassName="dark-color" label="Complaint Filed"/>
-              {
-                currentStatus=="Submitted" && <div
-                  className="complaint-details-timline-button"
-                  onClick={(e) => {
-                    console.log("clicked");
-                  }}
-                >
-                  <Icon action="communication" name="call" style={callIconStyle} color={"#ffffff"} />
-                  CALL
-                </div>
-              }
-          </div>
-        );
+      var { date, resolveImage, resolveFeedback } = content;
+      return (
+        <div className="complaint-timeline-content-section">
+          <Label labelClassName="rainmaker-small-font" label={date || "11-Mar-18"} />
+          <Label labelClassName="dark-color" label="Complaint Filed" />
+          {currentStatus == "Submitted" && (
+            <div
+              className="complaint-details-timline-button"
+              onClick={(e) => {
+                console.log("clicked");
+              }}
+            >
+              <Icon action="communication" name="call" style={callIconStyle} color={"#ffffff"} />
+              CALL
+            </div>
+          )}
+        </div>
+      );
     case "ASSIGNED":
-        var {status,name,designation,department}=content;
-        return (
-          <div className="complaint-timeline-content-section">
-              <Label labelClassName="rainmaker-small-font" label={date || "12-Mar-18"}/>
-              <Label labelClassName="dark-color" label={`Assigned to ${name ||"Satpal Singh"}`}/>
-              <Label labelClassName="rainmaker-small-font" containerStyle={{width:"192px"}} label={`${designation || "Jr.Inspector"} - ${department || "Health & Sanitation"}`}/>
-          </div>
-        );
+      var { status, name, designation, department } = content;
+      return (
+        <div className="complaint-timeline-content-section">
+          <Label labelClassName="rainmaker-small-font" label={date || "12-Mar-18"} />
+          <Label labelClassName="dark-color" label={`Assigned to ${name || "Satpal Singh"}`} />
+          <Label
+            labelClassName="rainmaker-small-font"
+            containerStyle={{ width: "192px" }}
+            label={`${designation || "Jr.Inspector"} - ${department || "Health & Sanitation"}`}
+          />
+        </div>
+      );
 
     case "REASSIGNED":
-            var {name,designation,department}=content;
-            return (
-              <div className="complaint-timeline-content-section">
-                  <Label labelClassName="rainmaker-small-font" label={date || "15-Mar-18"}/>
-                  <Label labelClassName="dark-color" label={`Re-Assigned to ${name ||"Satpal Singh"}`}/>
-                  <Label labelClassName="rainmaker-small-font" containerStyle={{width:"192px"}} label={`${designation || "Jr.Inspector"} - ${department || "Health & Sanitation"}`}/>
-              </div>
-            );
+      var { name, designation, department } = content;
+      return (
+        <div className="complaint-timeline-content-section">
+          <Label labelClassName="rainmaker-small-font" label={date || "15-Mar-18"} />
+          <Label labelClassName="dark-color" label={`Re-Assigned to ${name || "Satpal Singh"}`} />
+          <Label
+            labelClassName="rainmaker-small-font"
+            containerStyle={{ width: "192px" }}
+            label={`${designation || "Jr.Inspector"} - ${department || "Health & Sanitation"}`}
+          />
+        </div>
+      );
     case "REJECTED":
-        var {name,department}=content;
-        return (
-          <div className="complaint-timeline-content-section">
-              <Label labelClassName="rainmaker-small-font" label={date || "12-Mar-18"}/>
-              <Label labelClassName="dark-color" label="Complaint Rejected"/>
-              <Label labelClassName="rainmaker-small-font" containerStyle={{width:"192px"}} label={department || "Amritsar Municipal Corporation"} />
-              <div
-                className="complaint-details-timline-button"
-                onClick={(e) => {
-                  console.log("clicked");
-                }}
-              >
-                RE-OPEN
-              </div>
+      var { name, department } = content;
+      return (
+        <div className="complaint-timeline-content-section">
+          <Label labelClassName="rainmaker-small-font" label={date || "12-Mar-18"} />
+          <Label labelClassName="dark-color" label="Complaint Rejected" />
+          <Label labelClassName="rainmaker-small-font" containerStyle={{ width: "192px" }} label={department || "Amritsar Municipal Corporation"} />
+          <div
+            className="complaint-details-timline-button"
+            onClick={(e) => {
+              console.log("clicked");
+            }}
+          >
+            RE-OPEN
           </div>
-        );
+        </div>
+      );
     default:
-        var {date,resolveImage,resolveFeedback}=content;
-        return (
-          <div className="complaint-timeline-content-section">
-              <Label labelClassName="rainmaker-small-font" label={date || "18-Mar-18"}/>
-              <Label labelClassName="dark-color" label="Complaint Resolved"/>
-              <Image
-                style={{
-                  width: "97px",
-                  height: "93px",
-                  margin:"8px 0"
-                }}
-                source={garbageOne || resolveImage}
-              />
-              <Label labelClassName="rainmaker-small-font" containerStyle={{width:"192px"}} label={resolveFeedback || "Sweepers will clean this area on mondays & thursdays"}/>
-              <div className="rainmaker-displayInline"><div
-                className="complaint-details-timline-button"
-                onClick={(e) => {
-                  console.log("clicked");
-                }}
-              >
-                RATE
-              </div>
-              <div
-                className="complaint-details-timline-button"
-                onClick={(e) => {
-                  console.log("clicked");
-                }}
-              >
-                RE-OPEN
-              </div></div>
+      var { date, resolveImage, resolveFeedback } = content;
+      return (
+        <div className="complaint-timeline-content-section">
+          <Label labelClassName="rainmaker-small-font" label={date || "18-Mar-18"} />
+          <Label labelClassName="dark-color" label="Complaint Resolved" />
+          <Image
+            style={{
+              width: "97px",
+              height: "93px",
+              margin: "8px 0",
+            }}
+            source={garbageOne || resolveImage}
+          />
+          <Label
+            labelClassName="rainmaker-small-font"
+            containerStyle={{ width: "192px" }}
+            label={resolveFeedback || "Sweepers will clean this area on mondays & thursdays"}
+          />
+          <div className="rainmaker-displayInline">
+            <div
+              className="complaint-details-timline-button"
+              onClick={(e) => {
+                console.log("clicked");
+              }}
+            >
+              RATE
+            </div>
+            <div
+              className="complaint-details-timline-button"
+              onClick={(e) => {
+                console.log("clicked");
+              }}
+            >
+              RE-OPEN
+            </div>
           </div>
-        );
+        </div>
+      );
   }
-}
+};
 
 class ComplaintTimeLine extends Component {
   render() {
@@ -150,71 +161,71 @@ class ComplaintTimeLine extends Component {
           active: true,
         },
         labelProps: {
-          icon: <StatusIcon status="RESOLVED"/>,
+          icon: <StatusIcon status="RESOLVED" />,
         },
         contentProps: {
           style: {
             marginTop: "-50px",
           },
         },
-        contentChildren: <StatusContent status="RESOLVED" content={{}}/>,
+        contentChildren: <StatusContent status="RESOLVED" content={{}} />,
       },
       {
         props: {
           active: true,
         },
         labelProps: {
-          icon: <StatusIcon status="ASSIGNED"/>,
+          icon: <StatusIcon status="ASSIGNED" />,
         },
         contentProps: {
           style: {
             marginTop: "-50px",
           },
         },
-        contentChildren: <StatusContent status="REASSIGNED" content={{}}/>,
+        contentChildren: <StatusContent status="REASSIGNED" content={{}} />,
       },
       {
         props: {
           active: true,
         },
         labelProps: {
-          icon: <StatusIcon status="ASSIGNED"/>,
+          icon: <StatusIcon status="ASSIGNED" />,
         },
         contentProps: {
           style: {
             marginTop: "-50px",
           },
         },
-        contentChildren: <StatusContent status="ASSIGNED" content={{}}/>,
+        contentChildren: <StatusContent status="ASSIGNED" content={{}} />,
       },
       {
         props: {
           active: true,
         },
         labelProps: {
-          icon: <StatusIcon status="REJECTED"/>,
+          icon: <StatusIcon status="REJECTED" />,
         },
         contentProps: {
           style: {
             marginTop: "-50px",
           },
         },
-        contentChildren: <StatusContent status="REJECTED" content={{}}/>,
+        contentChildren: <StatusContent status="REJECTED" content={{}} />,
       },
       {
         props: {
           active: true,
         },
         labelProps: {
-          icon: <StatusIcon status="SUBMITTED"/>,
+          icon: <StatusIcon status="SUBMITTED" />,
         },
         contentProps: {
           style: {
             marginTop: "-50px",
           },
         },
-        contentChildren:<StatusContent status="SUBMITTED" content={{}}/>,
-      }
+        contentChildren: <StatusContent status="SUBMITTED" content={{}} />,
+      },
     ];
     if (status === "Submitted") {
       steps = [
@@ -223,64 +234,64 @@ class ComplaintTimeLine extends Component {
             active: true,
           },
           labelProps: {
-            icon: <StatusIcon status="SUBMITTED"/>,
+            icon: <StatusIcon status="SUBMITTED" />,
           },
           contentProps: {
             style: {
               marginTop: "-50px",
             },
           },
-          contentChildren:<StatusContent currentStatus={status} status="SUBMITTED" content={{}}/>,
-        }
+          contentChildren: <StatusContent currentStatus={status} status="SUBMITTED" content={{}} />,
+        },
       ];
-    }
-    else if (status === "Rejected") {
-      steps=[
+    } else if (status === "Rejected") {
+      steps = [
         {
           props: {
             active: true,
           },
           labelProps: {
-            icon: <StatusIcon status="REJECTED"/>,
+            icon: <StatusIcon status="REJECTED" />,
           },
           contentProps: {
             style: {
               marginTop: "-50px",
             },
           },
-          contentChildren: <StatusContent status="REJECTED" content={{}}/>,
+          contentChildren: <StatusContent status="REJECTED" content={{}} />,
         },
         {
           props: {
             active: true,
           },
           labelProps: {
-            icon: <StatusIcon status="SUBMITTED"/>,
+            icon: <StatusIcon status="SUBMITTED" />,
           },
           contentProps: {
             style: {
               marginTop: "-50px",
             },
           },
-          contentChildren:<StatusContent status="SUBMITTED" content={{}}/>,
-        }
+          contentChildren: <StatusContent status="SUBMITTED" content={{}} />,
+        },
       ];
     }
     return (
       <div>
         <Card
           style={{
-              paddingBottom: "0px"
+            paddingBottom: "0px",
           }}
           textChildren={
             <div>
               <div className="rainmaker-displayInline">
-                <Icon action="action" name="timeline" color="#969696" />   <Label label="Complaint Timeline" containerStyle={{marginLeft:"13px"}} labelClassName="dark-heading"/>
+                <Icon action="action" name="timeline" color="#969696" />{" "}
+                <Label label="Complaint Timeline" containerStyle={{ marginLeft: "13px" }} labelClassName="dark-heading" />
               </div>
               <div className="complaintTimeLineContainer">
                 <TimeLine
                   stepperProps={{
-                    orientation: "vertical"
+                    orientation: "vertical",
                   }}
                   steps={steps}
                 />
