6bf03706444dcd74af8adcfb685b12eb97b01170
sushant gwl <sushant.bhaskar@goodworklabs.com>
2018-03-16 18:56:29 +0530

    Added Image upload functionality v1

--- web/ui-app-v2/src/screens/AddComplaint/index.js
+++ web/ui-app-v2/src/screens/AddComplaint/index.js
@@ -4,7 +4,8 @@ import ImageUpload from "./components/ImageUpload";
 import ComplaintTypeCard from "./components/ComplaintType";
 import LocationDetailsCard from "./components/LocationDetails";
 import AdditionalDetailsCard from "./components/AdditionalDetails";
-import { Button } from "../../components";
+import Potholes_1 from "../../assets/images/Potholes_1.png";
+import { Button, UploadDrawer } from "../../components";
 
 import "./index.css";
 
@@ -12,6 +13,8 @@ class AddComplaints extends Component {
   state = {
     landmark: "",
     locationDetails: "Sector 32, 1 main, Amritsar",
+    images: [{ imageSource: Potholes_1 }, { imageSource: Potholes_1 }],
+    openUploadSlide: false,
   };
 
   handleLandmarkChange = (e, value) => {
@@ -22,16 +25,42 @@ class AddComplaints extends Component {
     this.setState({ additionalDetails: value });
   };
 
+  removeImage = (e, index) => {
+    let { images } = this.state;
+    images.splice(index, 1);
+    this.setState({ images });
+    e.stopPropagation();
+  };
+
+  addImage = () => {
+    this.setState({ openUploadSlide: true });
+  };
+
+  onClickAddPic = (isOpen) => {
+    this.setState({ openUploadSlide: isOpen });
+  };
+
   render() {
     return (
       <Screen>
         <div className="add-complaint-main-cont">
-          <ImageUpload />
+          <ImageUpload images={this.state.images} removeImage={this.removeImage} addImage={this.addImage} />
           <ComplaintTypeCard />
           <LocationDetailsCard landmark={this.state.landmark} locationDetails={this.state.locationDetails} onChange={this.handleLandmarkChange} />
           <AdditionalDetailsCard additionalDetails={this.state.additionalDetails} onChange={this.handleDetailsChange} />
           <div className="add-complaint-button-cont">
             <Button className="add-complaint-submit-button" label="SUBMIT COMPLAINT" fullWidth={true} primary={true} />
+          </div>
+          <div>
+            {this.state.openUploadSlide && (
+              <UploadDrawer
+                openUploadSlide={this.state.openUploadSlide}
+                galleryIcon={true}
+                removeIcon={true}
+                setProfilePic={this.setProfilePic}
+                onClickAddPic={this.onClickAddPic}
+              />
+            )}
           </div>
         </div>
       </Screen>
