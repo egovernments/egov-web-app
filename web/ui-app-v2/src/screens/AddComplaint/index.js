3ca7923f131e3277046bb8de21646dacb1fb99c6
sushant gwl <sushant.bhaskar@goodworklabs.com>
2018-03-16 15:53:07 +0530

    Added Add Complaints Screen

---
+++ web/ui-app-v2/src/screens/AddComplaint/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Screen from "../common/Screen";
+import ImageUpload from "./components/ImageUpload";
+import ComplaintTypeCard from "./components/ComplaintType";
+import LocationDetailsCard from "./components/LocationDetails";
+import AdditionalDetailsCard from "./components/AdditionalDetails";
+import { Button } from "../../components";
+
+import "./index.css";
+
+class AddComplaints extends Component {
+  state = {
+    landmark: "",
+    locationDetails: "Sector 32, 1 main, Amritsar",
+  };
+
+  handleLandmarkChange = (e, value) => {
+    this.setState({ landmark: value });
+  };
+
+  handleDetailsChange = (e, value) => {
+    this.setState({ additionalDetails: value });
+  };
+
+  render() {
+    return (
+      <Screen>
+        <div className="add-complaint-main-cont">
+          <ImageUpload />
+          <ComplaintTypeCard />
+          <LocationDetailsCard landmark={this.state.landmark} locationDetails={this.state.locationDetails} onChange={this.handleLandmarkChange} />
+          <AdditionalDetailsCard additionalDetails={this.state.additionalDetails} onChange={this.handleDetailsChange} />
+          <div className="add-complaint-button-cont">
+            <Button className="add-complaint-submit-button" label="SUBMIT COMPLAINT" fullWidth={true} primary={true} />
+          </div>
+        </div>
+      </Screen>
+    );
+  }
+}
+
+export default AddComplaints;
