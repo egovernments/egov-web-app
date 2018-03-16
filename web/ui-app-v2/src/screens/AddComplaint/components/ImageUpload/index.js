2140d9dec32ab485303a967ad8fbd263620c545b
sushant gwl <sushant.bhaskar@goodworklabs.com>
2018-03-16 18:56:29 +0530

    Added Image upload functionality v1

--- web/ui-app-v2/src/screens/AddComplaint/components/ImageUpload/index.js
+++ web/ui-app-v2/src/screens/AddComplaint/components/ImageUpload/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { UploadDrawer, Icon, Label } from "../../../../components";
+import { UploadDrawer, Icon, Label, Image } from "../../../../components";
+import FloatingActionButton from "material-ui/FloatingActionButton";
 import "./index.css";
 
 const iconStyle = {
@@ -12,14 +13,47 @@ const labelStyle = {
   letterSpacing: "0.6px",
   lineHeight: 1,
 };
-const ImageUpload = () => {
+
+const ImageUpload = ({ images, removeImage, addImage }) => {
   return (
-    <div className="upload-photo-overlay">
-      <div className="upload-icon-cont">
-        <Icon action="image" name="add-a-photo" style={iconStyle} color={"#ffffff"} />
-      </div>
-      <Label label="UPLOAD" labelStyle={labelStyle} />
-      <Label label="PHOTOS" labelStyle={labelStyle} />
+    <div className="upload-photo-overlay" onClick={addImage}>
+      {images.length === 0 ? (
+        <div>
+          <div className="upload-icon-cont">
+            <Icon action="image" name="add-a-photo" style={iconStyle} color={"#ffffff"} />
+          </div>
+          <Label label="UPLOAD" labelStyle={labelStyle} fontSize="12px" />
+          <Label label="PHOTOS" labelStyle={labelStyle} fontSize="12px" />
+        </div>
+      ) : (
+        <div className="upload-images-cont">
+          {images.map((image, index) => {
+            return (
+              <div className="upload-image-cont" key={index}>
+                <Image source={image.imageSource} style={{ height: "100%" }} />
+                <div className="image-remove" onClick={removeImage}>
+                  <Icon action="navigation" name="close" color="#ffffff" style={{ width: "14px", height: "14px" }} />
+                </div>
+              </div>
+            );
+          })}
+          {images.length < 3 ? (
+            <div className="upload-placeholder">
+              <FloatingActionButton
+                backgroundColor="#767676"
+                iconStyle={{ height: "40px", width: "40px" }}
+                style={{ boxShadow: 0, marginBottom: "4px" }}
+              >
+                <Icon name="add-a-photo" action="image" style={{ height: "20px", width: "20px" }} />
+              </FloatingActionButton>
+              <Label label="UPLOAD" labelStyle={labelStyle} fontSize="12px" />
+              <Label label="PHOTOS" labelStyle={labelStyle} fontSize="12px" />
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+      )}
     </div>
   );
 };
