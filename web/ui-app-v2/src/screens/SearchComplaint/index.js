da292b86f6cd1386ea096b13fe8df697e7a54efe
Varun Hegde <1hvarun@gmail.com>
2018-03-16 13:01:50 +0530

    serch background changed to white

--- web/ui-app-v2/src/screens/SearchComplaint/index.js
+++ web/ui-app-v2/src/screens/SearchComplaint/index.js
@@ -93,7 +93,7 @@ export default class SearchComplaint extends Component {
           }}
           textFieldStyle={{
             boxShadow: "0 2px 2px 0 rgba(0, 0, 0, 0.24), 0 0 2px 0 rgba(0, 0, 0, 0.12)",
-            backgroundColor: "#ffffff",
+            background: "#ffffff",
           }}
           dataSource={transformedDataSource}
           searchInputText="Search"
