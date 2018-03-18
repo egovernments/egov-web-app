6be8ddfd99e80d83c5174163feeec571f3e9ee37
varun93 <1hvarun@gmail.com>
2018-03-18 19:22:22 +0530

    converted homepage components into stateless components

--- web/ui-app-v2/src/screens/Home/components/NewAndOldComplaints/index.js
+++ web/ui-app-v2/src/screens/Home/components/NewAndOldComplaints/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, Icon, Label } from "../../../../components";
 import { withRouter } from "react-router-dom";
 import "./index.css";
@@ -15,34 +15,31 @@ const iconStyle = {
   borderRadius: "50%",
 };
 
-class NewAndOldComplaints extends Component {
-  render() {
-    let { history } = this.props;
-    return (
-      <Card
-        style={cardStyle}
-        textChildren={
-          <div className="row newAndOldComplaints-content-section">
-            <div id="home-new-complaint" className="col-xs-6">
-              <Icon style={{ ...iconStyle, background: "#f5a623" }} action="content" name="add" />
-              <Label containerStyle={{ marginTop: "10px" }} color="#484848" bold={true} fontSize={16} label="File Complaint" />
-            </div>
-            <div
-              id="home-old-complaint"
-              className="col-xs-6"
-              onClick={(e) => {
-                history.push("/my-complaints");
-              }}
-            >
-              <Icon style={{ height: "48px", color: "#fff", width: "48px" }} action="custom" name="my-complaint" />
-
-              <Label containerStyle={{ marginTop: "10px" }} color="#484848" bold={true} fontSize={16} label="My Complaints" />
-            </div>
+const NewAndOldComplaints = ({ history }) => {
+  return (
+    <Card
+      style={cardStyle}
+      textChildren={
+        <div className="row newAndOldComplaints-content-section">
+          <div id="home-new-complaint" className="col-xs-6">
+            <Icon style={{ ...iconStyle, background: "#f5a623" }} action="content" name="add" />
+            <Label containerStyle={{ marginTop: "10px" }} color="#484848" bold={true} fontSize={16} label="File Complaint" />
           </div>
-        }
-      />
-    );
-  }
-}
+          <div
+            id="home-old-complaint"
+            className="col-xs-6"
+            onClick={(e) => {
+              history.push("/my-complaints");
+            }}
+          >
+            <Icon style={{ height: "48px", color: "#fff", width: "48px" }} action="custom" name="my-complaint" />
 
-export default withRouter(NewAndOldComplaints);
+            <Label containerStyle={{ marginTop: "10px" }} color="#484848" bold={true} fontSize={16} label="My Complaints" />
+          </div>
+        </div>
+      }
+    />
+  );
+};
+
+export default NewAndOldComplaints;
