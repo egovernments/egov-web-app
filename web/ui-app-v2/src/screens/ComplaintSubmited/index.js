c4023981b52c39983097ec0d7b3ce4611ea9400a
varun93 <1hvarun@gmail.com>
2018-03-20 10:41:59 +0530

    added default background white to selected screens

--- web/ui-app-v2/src/screens/ComplaintSubmited/index.js
+++ web/ui-app-v2/src/screens/ComplaintSubmited/index.js
@@ -6,28 +6,29 @@ import "./index.css";
 
 class ComplaintDetails extends Component {
   continueComplaintSubmit = () => {
-    console.log(this.props);
-    this.props.history.push("");
+    this.props.history.push("/");
   };
   render() {
     let complaintnumber = 25436789;
     return (
-      <Screen className="complaint-submitted-card">
-        <div className="complaint-submitted-boldlabel">
-          <Label label={"Complaint registered successfully"} fontSize="16px" />
-          <FloatingActionButton backgroundColor="#22b25f" style={{ marginBottom: "16px" }}>
-            <Icon name={"check"} action={"navigation"} />
-          </FloatingActionButton>
-          <Label label={"Thank You!"} fontSize="16px" />
-          <Label label={`Complaint No. ${complaintnumber}`} fontSize="16px" />
-        </div>
-        <div className="complaint-submitted-label">
-          <Label label={`You can track the status of your complaint on this app anytime!`} />
-        </div>
-        <div className="complaintsubmit-button">
-          <Button id="complaint-submit-action" primary={true} label="CONTINUE" fullWidth={true} onClick={this.continueComplaintSubmit} />
-        </div>
-      </Screen>
+      <div>
+        <Screen className="complaint-submitted-card">
+          <div className="complaint-submitted-boldlabel">
+            <Label label={"Complaint registered successfully"} fontSize="16px" />
+            <FloatingActionButton backgroundColor="#22b25f" style={{ marginBottom: "16px" }}>
+              <Icon name={"check"} action={"navigation"} />
+            </FloatingActionButton>
+            <Label label={"Thank You!"} fontSize="16px" />
+            <Label label={`Complaint No. ${complaintnumber}`} fontSize="16px" />
+          </div>
+          <div className="complaint-submitted-label">
+            <Label label={`You can track the status of your complaint on this app anytime!`} />
+          </div>
+          <div className="complaintsubmit-button">
+            <Button id="complaint-submit-action" primary={true} label="CONTINUE" fullWidth={true} onClick={this.continueComplaintSubmit} />
+          </div>
+        </Screen>
+      </div>
     );
   }
 }
