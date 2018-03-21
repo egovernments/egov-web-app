be96f74d4ded8e43ccc938f818cde98e450a69a1
varun93 <1hvarun@gmail.com>
2018-03-21 11:04:58 +0530

    fixed bug in  loading indicator

--- web/ui-app-v2/src/components/LoadingIndicator/index.js
+++ web/ui-app-v2/src/components/LoadingIndicator/index.js
@@ -2,10 +2,34 @@ import React from "react";
 import PropTypes from "prop-types";
 import RefreshIndicator from "material-ui/RefreshIndicator";
 
-const LoadingIndicator = ({ status, loadingColor, size, left, top, style }) => {
+const style = {
+  container: {
+    height: "100%",
+    width: "100%",
+    position: "fixed",
+    backgroundColor: "rgba(189,189,189,0.5)",
+    zIndex: 9998,
+    left: 0,
+    top: 0,
+  },
+  containerHide: {
+    display: "none",
+    position: "relative",
+  },
+  refresh: {
+    display: "block",
+    position: "relative",
+    zIndex: 9999,
+    marginLeft: "48%",
+    marginTop: "23%",
+    color: "#FF9800",
+  },
+};
+
+const LoadingIndicator = ({ status = "loading", loadingColor, size = 40, left = 10, top = 40 }) => {
   return (
     <div style={status === "hide" ? style.containerHide : style.container}>
-      <RefreshIndicator size={40} left={50} top={0} status="loading" loadingColor={loadingColor} style={style} />
+      <RefreshIndicator size={size} left={left} top={top} status={status} style={style.refresh} />
     </div>
   );
 };
