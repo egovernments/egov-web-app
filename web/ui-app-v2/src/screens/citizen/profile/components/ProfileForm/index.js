fd651c4589709a52d5644d53cd4d3a0482cd73ed
muralimgwl <murali.m@goodworklabs.com>
2018-03-21 15:45:46 +0530

    Updated npm egov-react-components

---
+++ web/ui-app-v2/src/screens/citizen/profile/components/ProfileForm/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button, TextField } from "../../../../../components";
+import CityPicker from "../../../../common/CityPicker";
+
+const ProfileForm = ({ name, emailId, handleMailChange, handleNameChange }) => {
+  return (
+    <div>
+      <form className="profileFormContainer">
+        <TextField
+          className="profile-form-field"
+          id="profile-form-name"
+          fullWidth={true}
+          value={name}
+          hintText="Enter your Name"
+          floatingLabelText="Name"
+          onChange={handleNameChange}
+          isRequired={true}
+        />
+        <CityPicker />
+        <TextField
+          className="profile-form-field"
+          id="profile-form-email"
+          fullWidth={true}
+          value={emailId}
+          floatingLabelText="Email Id"
+          hintText="Enter your Email Id"
+          onChange={handleMailChange}
+        />
+      </form>
+      <div className="profileBtnWrapper">
+        <Button className="profileBtn" id="profile-save-action" primary={true} label="SAVE" fullWidth={true} onClick={this.onSaveClick} />
+      </div>
+    </div>
+  );
+};
+
+export default ProfileForm;
---
+++ web/ui-app-v2/src/screens/employee/AllComplaints/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class AllComplaints extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>Hello Employee</div>;
+  }
+}
+
+export default AllComplaints;
