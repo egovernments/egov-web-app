5aa5d10b6bde19b99b5d35b8d19fc38499b26018
Nithin DV <nithin_1992@msn.com>
2018-03-21 12:14:03 +0530

    URL change

--- web/ui-dashboard/src/constants/index.js
+++ web/ui-dashboard/src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.REACT_APP_ES || 'http://es-client.es-cluster:9200/';
-export const API_URL_LOC =
-  process.env.REACT_APP_BOUNDARY_SERVICE_API || 'http://egov-location:8080';
+export var API_URL = process.env.REACT_APP_ES || 'http://egov-micro-dev.egovernments.org/elasticsearch';
+export var API_URL_LOC =
+  process.env.REACT_APP_BOUNDARY_SERVICE_API || 'http://egov-micro-dev.egovernments.org/egov-location';
 export const STATE = 'AP';
