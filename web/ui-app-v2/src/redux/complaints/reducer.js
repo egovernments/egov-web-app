db41d33eabc4809aacc54d7564422e8a928ae0c8
varun93 <1hvarun@gmail.com>
2018-03-26 16:00:03 +0530

    complaints reducer added

--- web/ui-app-v2/src/redux/complaints/reducer.js
+++ web/ui-app-v2/src/redux/complaints/reducer.js
@@ -8,6 +8,15 @@ const intialState = {
 };
 
 const complaintsReducer = (state = intialState, action) => {
+  const { type } = action;
+
+  switch (type) {
+    case actionTypes.COMPLAINTS_FETCH_PENDING:
+    case actionTypes.COMPLAINTS_FETCH_COMPLETE:
+    case actionTypes.COMPLAINTS_FETCH_ERROR:
+      return state;
+  }
+
   return state;
 };
 export default complaintsReducer;
