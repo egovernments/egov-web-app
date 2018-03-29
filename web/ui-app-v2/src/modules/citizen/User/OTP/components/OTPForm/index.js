f2a715ae0713991b6ec5fcbbc8de80e0308d12b2
varun93 <1hvarun@gmail.com>
2018-03-29 16:24:17 +0530

    added otp translation

--- web/ui-app-v2/src/modules/citizen/User/OTP/components/OTPForm/index.js
+++ web/ui-app-v2/src/modules/citizen/User/OTP/components/OTPForm/index.js
@@ -3,7 +3,7 @@ import { Button, TextField, Card } from "components";
 import Label from "utils/translationNode";
 import "./index.css";
 
-const OTP = ({ formKey, form, onChange, submitForm ,number}) => {
+const OTP = ({ formKey, form, onChange, submitForm, number }) => {
   const fields = form.fields || {};
   const submit = form.submit;
 
@@ -13,7 +13,8 @@ const OTP = ({ formKey, form, onChange, submitForm ,number}) => {
       textChildren={
         <div>
           <Label className="otp-heading text-center" bold={true} dark={true} fontSize={16} label="ENTER OTP" />
-          <Label className="otp-text" label={`An OTP has been sent to Mobile Number ${number}`} />
+          <Label label="CORE_OTP_SENT_MESSAGE" />
+          <Label label="9968739374" />
           <form>
             <TextField onChange={(e, value) => onChange(formKey, "otp", value)} id="otp" {...fields.otp} fullWidth={true} />
             <div style={{ marginBottom: "24px" }} className="text-right">
