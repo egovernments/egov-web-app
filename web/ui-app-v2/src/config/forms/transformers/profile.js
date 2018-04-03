92bafd23b237719d16d2362bb09e0cdc7f6dde24
varun93 <1hvarun@gmail.com>
2018-04-03 14:27:03 +0530

    tenant Id not set

--- web/ui-app-v2/src/config/forms/transformers/profile.js
+++ web/ui-app-v2/src/config/forms/transformers/profile.js
@@ -3,8 +3,9 @@ import { prepareFormData, getUserInfo } from "utils/commons";
 const viewModelToBusinessModelTransformer = (form, state) => {
   const { fields } = form;
   let user = getUserInfo();
-  user = { ...user, name: fields.name.value, tenantId: fields.city.value, emailId: fields.email.value };
+  user = { ...user, name: fields.name.value, emailId: fields.email.value };
   const photo = (form.files && form.files["photo"] && form.files["photo"].length && form.files["photo"][0].fileStoreId) || null;
+  const tenantId = fields.city.value;
   if (photo) {
     user = { ...user, photo };
   }
