28b48b64a0308e892f63bbeff8cc10718f04cf8c
sushant gwl <sushant.bhaskar@goodworklabs.com>
2018-04-03 14:32:17 +0530

    Localisation in Feedback Screen

--- web/ui-app-v2/src/modules/citizen/Feedback/components/CheckBoxGroup/index.js
+++ web/ui-app-v2/src/modules/citizen/Feedback/components/CheckBoxGroup/index.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Checkbox } from "../../../../../components";
+import Label from "utils/translationNode";
 import "./index.css";
 
 const checkboxOptions = [
-  { value: "Services", label: "Services" },
-  { value: "Resolution Time", label: "Resolution Time" },
-  { value: "Quality of work", label: "Quality of work" },
-  { value: "Others", label: "Others" },
+  { value: "Services", label: <Label label="CS_FEEDBACK_SERVICES" /> },
+  { value: "Resolution Time", label: <Label label="CS_FEEDBACK_RESOLUTION_TIME" /> },
+  { value: "Quality of work", label: <Label label="CS_FEEDBACK_QUALITY_OF_WORK" /> },
+  { value: "Others", label: <Label label="CS_FEEDBACK_OTHERS" /> },
 ];
 
 const CheckboxGroup = ({ selected, onCheck }) => {
   return (
     <div>
-      <span className="what-was-good">What was good ?</span>
+      <Label className="what-was-good" label="CS_FEEDBACK_WHAT_WAS_GOOD" />
       <Checkbox
         labelStyle={{ letterSpacing: "0.6px" }}
         options={checkboxOptions}
