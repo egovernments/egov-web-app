68b60384f576a3bab61fac5f4e3278df0e07be92
Shreya <shreya.b@egovernments.org>
2018-04-04 12:51:54 +0530

    Added localization to page headers

--- web/ui-app-v2/src/modules/Router.js
+++ web/ui-app-v2/src/modules/Router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import PrivateRoute from "modules/PrivateRoute";
+import Label from "utils/translationNode";
 
 // Home page
 import Home from "modules/citizen/Home";
@@ -32,51 +33,138 @@ import ReAssignSuccess from "modules/employee/ReAssignSuccess";
 import ComplaintSummary from "modules/employee/ComplaintDetails";
 import EmpUser from "modules/employee/User";
 import AssignComplaint from "modules/employee/AssignComplaint";
+import "assets/styles/app.css";
 
 const Main = () => {
   return (
     <main>
       <Switch>
         <Route path="/citizen/user" component={Citizen} />
-        <PrivateRoute exact path="/citizen/" title="Home" component={Home} />
-        <PrivateRoute exact hideBottomNavigation={true} path="/citizen/contact-us" title="Contact Us" component={ContactUs} />
-        <PrivateRoute exact path="/citizen/my-complaints" title="My Complaints" component={MyComplaints} />
-        <PrivateRoute exact path="/citizen/complaint-details/:serviceRequestId?" title="Complaint Details" component={ComplaintDetails} />
-        <PrivateRoute exact path="/citizen/map" hideBottomNavigation={true} hideAppBar={true} title="Track Location" component={TrackLocation} />
+        <PrivateRoute exact path="/citizen/" title={<Label className="screenHeaderLabelStyle" label="CS_HOME_HEADER_HOME" />} component={Home} />
+        <PrivateRoute
+          exact
+          hideBottomNavigation={true}
+          path="/citizen/contact-us"
+          title={<Label className="screenHeaderLabelStyle" label="CS_HOME_HEADER_CONTACT_US" />}
+          component={ContactUs}
+        />
+        <PrivateRoute
+          exact
+          path="/citizen/my-complaints"
+          title={<Label className="screenHeaderLabelStyle" label="CS_HOME_MY_COMPLAINTS" />}
+          component={MyComplaints}
+        />
+        <PrivateRoute
+          exact
+          path="/citizen/complaint-details/:serviceRequestId?"
+          title={<Label className="screenHeaderLabelStyle" label="CS_COMPLAINT_DETAILS_COMPLAINT_DETAILS" />}
+          component={ComplaintDetails}
+        />
+        <PrivateRoute
+          exact
+          path="/citizen/map"
+          hideBottomNavigation={true}
+          hideAppBar={true}
+          title={<Label className="screenHeaderLabelStyle" label="Track Location" />}
+          component={TrackLocation}
+        />
         <PrivateRoute
           exact
           path="/citizen/complaint-submitted"
           hideBottomNavigation={true}
-          title="Complaint Submitted"
+          title={<Label className="screenHeaderLabelStyle" label="Complaint Submitted" />}
           component={ComplaintSubmited}
         />
-        <PrivateRoute exact path="/citizen/reopen-complaint/:serviceRequestId?" title="Reopen Complaint" component={ReOpenComplaint} />
-        <PrivateRoute exact path="/citizen/feedback/:serviceRequestId?" title="Feedback" component={Feedback} />
-        <PrivateRoute exact path="/citizen/feedback-acknowledgement" title="Feedback" component={FeedbackAcknowledge} />
-        <PrivateRoute exact hideBottomNavigation={true} path="/citizen/complaint-type" title="Complaint Type" component={ComplaintType} />
-        <PrivateRoute exact hideBottomNavigation={true} path="/citizen/how-it-works" title="How it works" component={HowItWorks} />
-        <PrivateRoute exact hideBottomNavigation={true} path="/citizen/add-complaint" title="Add Complaint" component={AddComplaint} />
+        <PrivateRoute
+          exact
+          path="/citizen/reopen-complaint/:serviceRequestId?"
+          title={<Label className="screenHeaderLabelStyle" label="Reopen Complaint" />}
+          component={ReOpenComplaint}
+        />
+        <PrivateRoute
+          exact
+          path="/citizen/feedback/:serviceRequestId?"
+          title={<Label className="screenHeaderLabelStyle" label="Feedback" />}
+          component={Feedback}
+        />
+        <PrivateRoute
+          exact
+          path="/citizen/feedback-acknowledgement"
+          title={<Label className="screenHeaderLabelStyle" label="CS_HOME_MY_COMPLAINTS" />}
+          component={FeedbackAcknowledge}
+        />
+        <PrivateRoute
+          exact
+          hideBottomNavigation={true}
+          path="/citizen/complaint-type"
+          title={<Label className="screenHeaderLabelStyle" label="CS_ADDCOMPLAINT_COMPLAINT_TYPE" />}
+          component={ComplaintType}
+        />
+        <PrivateRoute
+          exact
+          hideBottomNavigation={true}
+          path="/citizen/how-it-works"
+          title={<Label className="screenHeaderLabelStyle" label="CS_HOME_HEADER_HOW_IT_WORKS" />}
+          component={HowItWorks}
+        />
+        <PrivateRoute
+          exact
+          hideBottomNavigation={true}
+          path="/citizen/add-complaint"
+          title={<Label className="screenHeaderLabelStyle" label="Add Complaint" />}
+          component={AddComplaint}
+        />
         {/* Employee routes */}
-        <PrivateRoute exact hideBottomNavigation={true} path="/employee/all-complaints" title="All Complaints" component={AllComplaints} />
-        <PrivateRoute exact hideBottomNavigation={true} path="/employee/reassign-complaint" title="Request Re-Assign" component={ReAssignComplaint} />
+        <PrivateRoute
+          exact
+          hideBottomNavigation={true}
+          path="/employee/all-complaints"
+          title={<Label className="screenHeaderLabelStyle" label="All Complaints" />}
+          component={AllComplaints}
+        />
+        <PrivateRoute
+          exact
+          hideBottomNavigation={true}
+          path="/employee/reassign-complaint"
+          title={<Label className="screenHeaderLabelStyle" label="Request Re-Assign" />}
+          component={ReAssignComplaint}
+        />
         <PrivateRoute
           exact
           hideBottomNavigation={true}
           path="/employee/complaint-resolved"
-          title="Complaint Resolved"
+          title={<Label className="screenHeaderLabelStyle" label="CS_COMPLAINT_DETAILS_COMPLAINT_RESOLVED" />}
           component={ComplaintResolved}
         />
         <PrivateRoute
           exact
           hideBottomNavigation={true}
           path="/employee/complaint-details/:serviceRequestId?"
-          title="Complaint Summary"
+          title={<Label className="screenHeaderLabelStyle" label="Complaint Summary" />}
           component={ComplaintSummary}
         />
-        <PrivateRoute exact hideBottomNavigation={true} path="/employee/resolve-success" title="Complaint Resolved" component={ResolveSuccess} />
-        <PrivateRoute exact hideBottomNavigation={true} path="/employee/reassign-success" title="Re-Assign Complaint" component={ReAssignSuccess} />
+        <PrivateRoute
+          exact
+          hideBottomNavigation={true}
+          path="/employee/resolve-success"
+          title={<Label className="screenHeaderLabelStyle" label="CS_COMPLAINT_DETAILS_COMPLAINT_RESOLVED" />}
+          component={ResolveSuccess}
+        />
+        <PrivateRoute
+          exact
+          hideBottomNavigation={true}
+          path="/employee/reassign-success"
+          title={<Label className="screenHeaderLabelStyle" label="Re-Assign Complaint" />}
+          component={ReAssignSuccess}
+        />
         <Route path="/employee/user" component={EmpUser} />
-        <PrivateRoute exact hideBottomNavigation={true} path="/employee/assign-complaint" title="Assign Complaint" component={AssignComplaint} />
+        <PrivateRoute
+          exact
+          hideBottomNavigation={true}
+          path="/employee/assign-complaint"
+          title={<Label className="screenHeaderLabelStyle" label="Assign Complaint" />}
+          component={AssignComplaint}
+        />
       </Switch>
     </main>
   );
