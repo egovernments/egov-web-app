834699c446b12e59d3cf58b1d7d6bb840afd34cb
Gyan4497 <gyanprakash92@gmail.com>
2018-04-04 13:22:02 +0530

    changes for textfield with icon and citypicker

--- web/ui-app-v2/src/components/TextFieldIcon/index.js
+++ web/ui-app-v2/src/components/TextFieldIcon/index.js
@@ -11,7 +11,8 @@ const containerStyle = {
 };
 
 const getStyles = (iconPosition, textFieldProps) => {
-  const textFieldStyle = {};
+  const textFieldStyle = {},
+    inputStyle = {};
 
   const iconStyle = {
     position: "absolute",
@@ -21,7 +22,7 @@ const getStyles = (iconPosition, textFieldProps) => {
   };
   iconStyle[iconPosition === "before" ? "left" : "right"] = 0;
   textFieldStyle["textIndent"] = iconPosition === "before" ? 40 : 0;
-  textFieldStyle["width"] = iconPosition === "after" ? "90%" : "100%";
+  inputStyle["width"] = iconPosition === "after" ? "90%" : "100%";
 
   if (textFieldProps.floatingLabelText) {
     iconStyle.top = 30;
@@ -30,6 +31,7 @@ const getStyles = (iconPosition, textFieldProps) => {
   return {
     iconStyle,
     textFieldStyle,
+    inputStyle,
   };
 };
 
@@ -38,7 +40,12 @@ const TextFieldIcon = ({ Icon, iconStyle = {}, onClick, textFieldStyle = {}, ico
   return (
     <div onClick={onClick} style={containerStyle}>
       <Icon style={{ ...style.iconStyle, ...iconStyle }} />
-      <TextField name="textfield-icon" style={{ ...style.textFieldStyle, ...textFieldStyle }} {...textFieldProps} />
+      <TextField
+        name="textfield-icon"
+        style={{ ...style.textFieldStyle, ...textFieldStyle }}
+        inputStyle={{ ...style.inputStyle }}
+        {...textFieldProps}
+      />
     </div>
   );
 };
