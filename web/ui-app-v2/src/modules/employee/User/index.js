546db46f87169e5d2ba9a2b9b0dc040c526bdd0d
varun93 <1hvarun@gmail.com>
2018-04-06 06:56:21 +0530

    employee related bug

--- web/ui-app-v2/src/modules/employee/User/index.js
+++ web/ui-app-v2/src/modules/employee/User/index.js
@@ -5,18 +5,18 @@ import { Route } from "react-router-dom";
 import Login from "./Login";
 import OTP from "./OTP";
 import LanguageSelection from "./LanguageSelection";
-import ForgotPassword from "./ForgotPassword";
+// import ForgotPassword from "./ForgotPassword";
 import Profile from "./Profile";
 
-const Citizen = ({ match }) => {
+const Employee = ({ match }) => {
   return (
     <div>
       <Route exact path={`${match.url}/login`} component={Login} />
       <Route exact path={`${match.url}/otp`} component={OTP} />
-      <Route exact path={`${match.url}/forgot-password`} component={ForgotPassword} />
+      {/* <Route exact path={`${match.url}/forgot-password`} component={ForgotPassword} /> */}
       <Route exact path={`${match.url}/language-selection`} component={LanguageSelection} />
     </div>
   );
 };
 
-export default Citizen;
+export default Employee;
