81af0f8aaa4219700394f30b0e4b125f2bce0495
varun93 <1hvarun@gmail.com>
2018-04-16 10:27:20 +0530

    added support for geolocation

--- web/android-webview/app/src/main/java/org/egovernment/rainmaker/AppJavaScriptProxy.java
+++ web/android-webview/app/src/main/java/org/egovernment/rainmaker/AppJavaScriptProxy.java
@@ -3,17 +3,9 @@ package org.egovernment.rainmaker;
 import android.app.Activity;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.webkit.JavascriptInterface;
 import android.Manifest;
-
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by varun on 09/04/18.
@@ -33,26 +25,6 @@ public class AppJavaScriptProxy  {
 		String permission = Manifest.permission.RECEIVE_SMS;
 		int res = activity.getApplicationContext().checkCallingOrSelfPermission(permission);
 		return (res == PackageManager.PERMISSION_GRANTED);
-	}
-
-	@JavascriptInterface
-	public boolean hasLocationAccess(){
-		String permission = Manifest.permission.ACCESS_FINE_LOCATION;
-		int res = activity.getApplicationContext().checkCallingOrSelfPermission(permission);
-		return (res == PackageManager.PERMISSION_GRANTED);
-	}
-
-	public void requestLocationReadPermission() {
-		int GET_MY_PERMISSION = 2;
-		if (!hasLocationAccess()) {
-			if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
-					Manifest.permission.ACCESS_FINE_LOCATION)) {
-		/* do nothing*/
-			} else {
-				ActivityCompat.requestPermissions(activity,
-						new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, GET_MY_PERMISSION);
-			}
-		}
 	}
 
 
@@ -92,28 +64,5 @@ public class AppJavaScriptProxy  {
 	}
 
 
-	@JavascriptInterface
-	public String requestLocation(){
-		requestLocationReadPermission();
-		HashMap<String, String> location = new HashMap<>();
-		GPSTrack gps;
-		gps = new GPSTrack(activity);
-		double latitude = gps.getLatitude();
-		double longitude = gps.getLongitude();
-		if (gps.canGetLocation()) {
-			if (latitude != 0 || longitude != 0) {
-				location.put("lat" , String.valueOf(latitude));
-				location.put("lng",String.valueOf(longitude));
-				Log.d("Longitude", "long=" + longitude);
-				//Log.w("New Updated Location:", latitude + "," + longitude);
-				} else {
-					Log.w("New Updated Location:", "NULL");
-				}
-			} else {
-				Log.w("New Updated Location:", "FAIL");
-			}
-
-		return new JSONObject(location).toString();
-	}
 
 }
--- web/android-webview/app/src/main/java/org/egovernment/rainmaker/MainActivity.java
+++ web/android-webview/app/src/main/java/org/egovernment/rainmaker/MainActivity.java
@@ -9,7 +9,9 @@ import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
@@ -27,6 +29,7 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.webkit.GeolocationPermissions;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
@@ -38,16 +41,20 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import android.app.AlertDialog;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-	//https://egov-micro-dev.egovernments.org/app/v3/citizen/employee/language-selection
+	final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124;
 	private static String URL   = "https://egov-micro-dev.egovernments.org/app/v3/citizen/user/language-selection"; //complete URL of your website or webpage
 	private String FILE_TYPE       = "image/*";  //to upload any file type using "*/*"; check file type references for more
-
 
 	//Careful with these variable names if altering
     private WebView webView;
@@ -111,14 +118,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+		if (Build.VERSION.SDK_INT >= 23) {
+			// Marshmallow+ Permission APIs
+			handleMarshMellow();
+		}
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+			if (0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE)) {
+				WebView.setWebContentsDebuggingEnabled(true);
+			}
+		}
+
         //Move this to Javascript Proxy
 
 		webView = (WebView) findViewById(R.id.webview);
 		webView.addJavascriptInterface(proxy, "androidAppProxy");
 
-		//get_location();
-		//Webview settings; defaults are customized for best performance
-        WebSettings webSettings = webView.getSettings();
+
+		WebSettings webSettings = webView.getSettings();
 
 		webSettings.setJavaScriptEnabled(true);
 		webSettings.setGeolocationEnabled(true);
@@ -139,12 +157,21 @@ public class MainActivity extends AppCompatActivity {
         }
         webView.setVerticalScrollBarEnabled(false);
         webView.setWebViewClient(new CustomWebView());
+		webView.getSettings().setGeolocationDatabasePath(getFilesDir().getPath());
 
         //Rendering the default URL
         aswm_view(URL);
 
         webView.setWebChromeClient(new WebChromeClient() {
-            //Handling input[type="file"] requests for android API 16+
+            // handling geolocation
+
+
+			@Override
+			public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+				callback.invoke(origin, true, false);
+			}
+
+			//Handling input[type="file"] requests for android API 16+
             public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture){
 		  			asw_file_message = uploadMsg;
                     Intent i = new Intent(Intent.ACTION_GET_CONTENT);
@@ -248,7 +275,8 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Something Went Wrong!", Toast.LENGTH_SHORT).show();
         }
 
-        //Overriding org.egovernment.org.egovernment.org.egovernment.rainmaker URLs
+
+		//Overriding org.egovernment.org.egovernment.org.egovernment.rainmaker URLs
 		@SuppressWarnings("deprecation")
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -393,4 +421,101 @@ public class MainActivity extends AppCompatActivity {
         super.onRestoreInstanceState(savedInstanceState);
         webView.restoreState(savedInstanceState);
     }
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		switch (requestCode) {
+			case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS: {
+				Map<String, Integer> perms = new HashMap<String, Integer>();
+				// Initial
+				perms.put(Manifest.permission.ACCESS_FINE_LOCATION, PackageManager.PERMISSION_GRANTED);
+
+
+				// Fill with results
+				for (int i = 0; i < permissions.length; i++)
+					perms.put(permissions[i], grantResults[i]);
+
+				// Check for ACCESS_FINE_LOCATION
+				if (perms.get(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+
+						) {
+					// All Permissions Granted
+
+					// Permission Denied
+					Toast.makeText(MainActivity.this, "All Permissions Granted)", Toast.LENGTH_SHORT)
+							.show();
+
+				} else {
+					// Permission Denied
+					Toast.makeText(MainActivity.this, "One or More Permissions are DENIED Exiting App", Toast.LENGTH_SHORT)
+							.show();
+					finish();
+				}
+			}
+			break;
+			default:
+				super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+	}
+
+	@TargetApi(Build.VERSION_CODES.M)
+	private void handleMarshMellow() {
+		List<String> permissionsNeeded = new ArrayList<String>();
+
+		final List<String> permissionsList = new ArrayList<String>();
+		if (!addPermission(permissionsList, Manifest.permission.ACCESS_FINE_LOCATION))
+			permissionsNeeded.add("Show Location");
+
+		if (permissionsList.size() > 0) {
+			if (permissionsNeeded.size() > 0) {
+
+				// Need Rationale
+				String message = "App need access to " + permissionsNeeded.get(0);
+
+				for (int i = 1; i < permissionsNeeded.size(); i++)
+					message = message + ", " + permissionsNeeded.get(i);
+
+				showMessageOKCancel(message,
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								requestPermissions(permissionsList.toArray(new String[permissionsList.size()]),
+										REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+							}
+						});
+				return;
+			}
+			requestPermissions(permissionsList.toArray(new String[permissionsList.size()]),
+					REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+			return;
+		}
+
+		Toast.makeText(MainActivity.this, "No new Permission Required- Launching App .You are Awesome!!", Toast.LENGTH_SHORT)
+				.show();
+	}
+
+
+	private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+		new AlertDialog.Builder(MainActivity.this)
+				.setMessage(message)
+				.setPositiveButton("OK", okListener)
+				.setNegativeButton("Cancel", null)
+				.create()
+				.show();
+	}
+
+	@TargetApi(Build.VERSION_CODES.M)
+	private boolean addPermission(List<String> permissionsList, String permission) {
+
+		if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+			permissionsList.add(permission);
+			// Check for Rationale Option
+			if (!shouldShowRequestPermissionRationale(permission))
+				return false;
+		}
+		return true;
+	}
+
 }
