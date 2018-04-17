f879080e779ea225745dd95a42df7164dc055421
Shreya <shreya.b@egovernments.org>
2018-04-17 18:54:13 +0530

    Resolved issues

--- web/ui-app-v2/src/modules/citizen/User/Register/components/RegisterForm/index.js
+++ web/ui-app-v2/src/modules/citizen/User/Register/components/RegisterForm/index.js
@@ -13,20 +13,17 @@ const RegisterForm = ({ formKey, form, onChange, navigateToLogin, submitForm }) 
       textChildren={
         <div>
           <Label className="heading text-center" bold={true} dark={true} fontSize={16} label="CORE_REGISTER_HEADING" />
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              submitForm(formKey);
-            }}
-          >
+          <form>
             <MobileNumberField {...fields.phone} onChange={(e, value) => onChange(formKey, "phone", value)} />
             <TextField {...fields.name} onChange={(e, value) => onChange(formKey, "name", value)} />
             <CityPicker onChange={onChange} formKey={formKey} fieldKey="city" field={fields.city} />
-            <div onClick={navigateToLogin} style={{ marginBottom: "24px" }} className="text-right">
+            <div style={{ marginBottom: "24px" }} className="text-right">
               <Label id="otp-trigger" className="otp-prompt" label="CORE_REGISTER_HAVE_ACCOUNT" />
-              <Label containerStyle={{ cursor: "pointer" }} id="otp-resend" className="otp-resend" label="CORE_COMMON_LOGIN" />
+              <div onClick={navigateToLogin} style={{ display: "inline-block" }}>
+                <Label containerStyle={{ cursor: "pointer" }} id="otp-resend" className="otp-resend" label="CORE_COMMON_LOGIN" />
+              </div>
             </div>
-            <Button primary={true} fullWidth={true} {...submit} />
+            <Button onClick={() => submitForm(formKey)} primary={true} fullWidth={true} {...submit} />
           </form>
         </div>
       }
