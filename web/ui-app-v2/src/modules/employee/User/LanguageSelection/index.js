ba15a972a80a5084739b782476858b3876a5d7bf
varun93 <1hvarun@gmail.com>
2018-04-18 11:24:12 +0530

    language selection screen added

---
+++ web/ui-app-v2/src/modules/employee/User/LanguageSelection/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import Banner from "modules/common/Banner";
+import { connect } from "react-redux";
+import LanguageSelectionForm from "modules/common/User/components/LanguageSelectionForm";
+import { setRoute, fetchLocalizationLabel } from "redux/app/actions";
+
+class LanguageSelection extends Component {
+  state = {
+    value: localStorage.getItem("locale"),
+    items: [
+      {
+        label: "ENGLISH",
+        value: "en_IN",
+      },
+      {
+        label: "हिंदी",
+        value: "hi_IN",
+      },
+      {
+        label: "ਪੰਜਾਬੀ",
+        value: "pn_IN",
+      },
+    ],
+  };
+
+  onClick = (value) => {
+    this.setState({ value });
+    this.props.fetchLocalizationLabel(value);
+  };
+
+  onLanguageSelect = () => {
+    this.props.setRoute("/employee/user/login");
+  };
+
+  render() {
+    const { items, value } = this.state;
+    const { onLanguageSelect, onClick } = this;
+
+    return (
+      <Banner className="language-selection col-lg-offset-2 col-md-offset-2 col-md-8 col-lg-8">
+        <LanguageSelectionForm items={items} value={value} onLanguageSelect={onLanguageSelect} onClick={onClick} />
+      </Banner>
+    );
+  }
+}
+
+const dispatchToProps = (dispatch) => {
+  return {
+    fetchLocalizationLabel: (locale) => dispatch(fetchLocalizationLabel(locale)),
+    setRoute: (route) => dispatch(setRoute(route)),
+  };
+};
+
+export default connect(null, dispatchToProps)(LanguageSelection);
--- web/ui-app-v2/src/modules/employee/User/index.js
+++ web/ui-app-v2/src/modules/employee/User/index.js
@@ -6,6 +6,7 @@ import withoutAuthorization from "hocs/withoutAuthorization";
 // routes
 import Login from "./Login";
 import OTP from "./OTP";
+import LanguageSelection from "./LanguageSelection";
 import ChangePassword from "./ChangePassword";
 import ForgotPassword from "./ForgotPassword";
 import Profile from "./Profile";
@@ -19,6 +20,7 @@ const Employee = ({ match }) => {
       <Route exact path={`${match.url}/otp`} component={withoutAuthorization(OTP, redirectionUrl)} />
       <Route exact path={`${match.url}/change-password`} component={withoutAuthorization(ChangePassword, redirectionUrl)} />
       <Route exact path={`${match.url}/forgot-password`} component={withoutAuthorization(ForgotPassword, redirectionUrl)} />
+      <Route exact path={`${match.url}/language-selection`} component={withoutAuthorization(LanguageSelection, redirectionUrl)} />
       <PrivateRoute exact path={`${match.url}/profile`} hideBottomNavigation={true} title="Edit Profile" component={Profile} />
     </Switch>
   );
