652ead036735abd1c6c52a179533fddd798f89dd
sushant gwl <sushant.bhaskar@goodworklabs.com>
2018-04-24 16:51:51 +0530

    Fixed footer text break on small mobiles

--- web/ui-app-v2/src/components/Tabs/index.js
+++ web/ui-app-v2/src/components/Tabs/index.js
@@ -14,16 +14,12 @@ const inkBarStyle = {
   backgroundColor: "#f89a3f",
 };
 
-const Tabs = ({ tabs = []}) => {
+const Tabs = ({ tabs = [] }) => {
   const renderTabs = () => {
     return tabs.map((tab, index) => {
       const { route, label, children } = tab;
       return (
-        <Tab
-          key={index}
-          data-route={route}
-          label={label}
-        >
+        <Tab key={index} data-route={route} label={label}>
           {children}
         </Tab>
       );
