0cad70d2a29ce28a51033defba7588ea8c8cada9
Shreya <shreya.b@egovernments.org>
2018-05-14 15:17:30 +0530

    Added dialogue page for property tax

---
+++ web/ui-app-v2/src/modules/citizen/PropertyTax/PaymentStepOne/components/YearDialogue/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Dialog } from "components";
+import SingleButton from "./components/SingleButton/index";
+import Label from "utils/translationNode";
+import "./index.css";
+
+const styles = {
+  logoutContentStyle: { textAlign: "center", padding: "24px 20px" },
+};
+
+class YearDialog extends Component {
+  render() {
+    let { open, yearList, closeDialogue } = this.props;
+    return (
+      <Dialog
+        open={open}
+        children={[
+          <div>
+            <div className="dialogue-question">Which year’s taxes would you like to pay? </div>
+            <div className="year-range-botton-cont">
+              {yearList.map((item, index) => <SingleButton key={index} label={item} handleClose={closeDialogue} />)}
+            </div>
+          </div>,
+        ]}
+        bodyStyle={{ backgroundColor: "#ffffff" }}
+        isClose={false}
+      />
+    );
+  }
+}
+
+export default YearDialog;
