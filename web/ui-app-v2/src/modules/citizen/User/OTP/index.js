2771cdbf1ac451941e42cae02ec1fa37b0a93008
varun93 <1hvarun@gmail.com>
2018-05-22 14:54:57 +0530

    otp form migrated to form hoc

--- web/ui-app-v2/src/modules/citizen/User/OTP/index.js
+++ web/ui-app-v2/src/modules/citizen/User/OTP/index.js
@@ -1,17 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import formHoc from "hocs/form";
 import Banner from "modules/common/common/Banner";
 import OTPForm from "./components/OTPForm";
-import { handleFieldChange, initForm, submitForm } from "redux/form/actions";
+import { handleFieldChange, submitForm } from "redux/form/actions";
 import { sendOTP } from "redux/auth/actions";
 import Screen from "modules/common/common/Screen";
 
-class OTP extends Component {
-  constructor(props) {
-    super(props);
-    this.formConfig = require("config/forms/otp").default;
-  }
+const OTPFormHOC = formHoc(OTPForm, "otp");
 
+class OTP extends Component {
   componentWillMount() {
     const { previousRoute } = this.props;
     if (previousRoute.length === 0) {
@@ -20,15 +18,13 @@ class OTP extends Component {
   }
 
   componentDidMount() {
-    const { initForm, submitForm, handleFieldChange } = this.props;
+    const { submitForm, handleFieldChange } = this.props;
     const otpElement = document.getElementById("otp");
     otpElement.addEventListener("smsReceived", (e) => {
       const { otp } = e.detail;
       handleFieldChange("otp", "otp", otp);
       submitForm("otp");
     });
-
-    initForm(this.formConfig);
   }
 
   componentWillUnmount() {
@@ -42,21 +38,13 @@ class OTP extends Component {
   };
 
   render() {
-    const { form, handleFieldChange, submitForm, phoneNumber, loading } = this.props;
+    const { handleFieldChange, submitForm, phoneNumber, loading } = this.props;
     const { resendOTP } = this;
-    const { name: formKey } = this.formConfig;
 
     return (
       <Screen loading={loading}>
         <Banner className="col-lg-offset-2 col-md-offset-2 col-md-8 col-lg-8">
-          <OTPForm
-            submitForm={submitForm}
-            resendOTP={resendOTP}
-            form={form}
-            formKey={formKey}
-            phoneNumber={phoneNumber}
-            onChange={handleFieldChange}
-          />
+          <OTPFormHOC resendOTP={resendOTP} phoneNumber={phoneNumber} />
         </Banner>
       </Screen>
     );
@@ -65,8 +53,6 @@ class OTP extends Component {
 
 const mapStateToProps = (state) => {
   const formKey = "otp";
-  const form = state.form[formKey] || {};
-  const { loading } = form || false;
   const { authenticating } = state.auth;
   const { previousRoute } = state.app;
   const intent = previousRoute.endsWith("register") ? "register" : previousRoute.endsWith("login") ? "login" : null;
@@ -74,14 +60,13 @@ const mapStateToProps = (state) => {
   if (intent) {
     phoneNumber = state.form[intent].fields.phone.value;
   }
-  return { form, previousRoute, intent, phoneNumber, loading: loading || authenticating };
+  return { previousRoute, intent, phoneNumber, loading: authenticating };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     handleFieldChange: (formKey, fieldKey, value) => dispatch(handleFieldChange(formKey, fieldKey, value)),
     submitForm: (formKey) => dispatch(submitForm(formKey)),
-    initForm: (form) => dispatch(initForm(form)),
     sendOTP: (otp) => dispatch(sendOTP(otp)),
   };
 };
