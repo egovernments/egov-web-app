7a127e03d86741070caa1cbb7b740f211f2a48f1
varun93 <1hvarun@gmail.com>
2018-05-23 13:02:06 +0530

    migrated property taxes to form hoc

--- web/ui-app-v2/src/modules/citizen/PropertyTax/AssessmentFormWizard/components/TaxAssessmentDetailsOne/index.js
+++ web/ui-app-v2/src/modules/citizen/PropertyTax/AssessmentFormWizard/components/TaxAssessmentDetailsOne/index.js
@@ -1,24 +1,23 @@
 import React from "react";
 import { TextField, DropDown } from "components";
 
-const TaxAssessmentDetailsOne = ({ fields, handleFieldChange }) => {
+const TaxAssessmentDetailsOne = ({ form, wizardFields, handleFieldChange }) => {
+  const fields = wizardFields(form.fields || {});
   return (
     <div className="tax-assessment-details-cont-1">
-      <form>
-        <DropDown
-          onChange={(e, value) => handleFieldChange("propertyType", value)}
-          {...fields.propertyType}
-          dropDownData={[{ label: "property1", value: "prop1" }, { label: "property2", value: "prop2" }]}
-          fullWidth={true}
-        />
-        <TextField {...fields.plotSize} onChange={(e, value) => handleFieldChange("plotSize", value)} id="plot-size" />
-        <DropDown
-          {...fields.floorCount}
-          onChange={(e, value) => handleFieldChange("floorCount", value)}
-          dropDownData={[{ label: "1", value: "1" }, { label: "2", value: "2" }]}
-          fullWidth={true}
-        />
-      </form>
+      <DropDown
+        onChange={(e, value) => handleFieldChange("propertyType", value)}
+        {...fields.propertyType}
+        dropDownData={[{ label: "property1", value: "prop1" }, { label: "property2", value: "prop2" }]}
+        fullWidth={true}
+      />
+      <TextField {...fields.plotSize} onChange={(e, value) => handleFieldChange("plotSize", value)} id="plot-size" />
+      <DropDown
+        {...fields.floorCount}
+        onChange={(e, value) => handleFieldChange("floorCount", value)}
+        dropDownData={[{ label: "1", value: "1" }, { label: "2", value: "2" }]}
+        fullWidth={true}
+      />
     </div>
   );
 };
