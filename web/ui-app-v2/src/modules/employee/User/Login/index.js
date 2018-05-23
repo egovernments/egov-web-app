367fef7d73f09ba92c26afd066942aa90ebfe8e6
varun93 <1hvarun@gmail.com>
2018-05-23 10:29:18 +0530

    Migrated employee login form to form hoc

--- web/ui-app-v2/src/modules/employee/User/Login/index.js
+++ web/ui-app-v2/src/modules/employee/User/Login/index.js
@@ -1,58 +1,19 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React from "react";
+import formHoc from "hocs/form";
 import Banner from "modules/common/common/Banner";
 import Screen from "modules/common/common/Screen";
 import LoginForm from "./components/LoginForm";
-import { handleFieldChange, initForm, submitForm } from "redux/form/actions";
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.formConfig = require("config/forms/employeeLogin").default;
-  }
+const LoginFormHOC = formHoc(LoginForm, "employeeLogin");
 
-  componentDidMount() {
-    this.props.initForm(this.formConfig);
-  }
-
-  handleForgotPasswdCLick = () => {
-    this.props.history.push("/employee/user/forgot-password");
-  };
-
-  render() {
-    const { form, handleFieldChange, submitForm, loading } = this.props;
-    const { name: formKey } = this.formConfig;
-    const { handleForgotPasswdCLick } = this;
-
-    return (
-      <Screen loading={loading}>
-        <Banner className="col-lg-offset-2 col-md-offset-2 col-md-8 col-lg-8">
-          <LoginForm
-            form={form}
-            submitForm={submitForm}
-            formKey={formKey}
-            onChange={handleFieldChange}
-            onForgotPasswdCLick={handleForgotPasswdCLick}
-          />
-        </Banner>
-      </Screen>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  const formKey = "employeeLogin";
-  const form = state.form[formKey] || {};
-  const { loading } = form || false;
-  return { form, loading };
+const Login = () => {
+  return (
+    <Screen>
+      <Banner className="col-lg-offset-2 col-md-offset-2 col-md-8 col-lg-8">
+        <LoginFormHOC />
+      </Banner>
+    </Screen>
+  );
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    handleFieldChange: (formKey, fieldKey, value) => dispatch(handleFieldChange(formKey, fieldKey, value)),
-    submitForm: (formKey) => dispatch(submitForm(formKey)),
-    initForm: (form) => dispatch(initForm(form)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default Login;
