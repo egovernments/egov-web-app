180d7f3ad091ee896ca69e09f348c9be6ee8dfd0
varun93 <1hvarun@gmail.com>
2018-05-23 10:53:35 +0530

    migrated to change form to hoc

--- web/ui-app-v2/src/modules/employee/User/ChangePassword/index.js
+++ web/ui-app-v2/src/modules/employee/User/ChangePassword/index.js
@@ -1,58 +1,27 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import formHoc from "hocs/form";
 import Screen from "modules/common/common/Screen";
 import PasswordForm from "./components/PasswordForm";
-import { handleFieldChange, initForm, submitForm } from "redux/form/actions";
 import { toggleSnackbarAndSetText } from "redux/app/actions";
 
+const PasswordFormHOC = formHoc(PasswordForm, "employeeChangePassword");
+
 class ChangePassword extends Component {
-  constructor(props) {
-    super(props);
-    this.formConfig = require("config/forms/employeeChangePassword").default;
-  }
-
-  componentDidMount() {
-    this.props.initForm(this.formConfig);
-  }
-
-  submitChangePasswdForm = (formKey) => {
-    const { form, submitForm, toggleSnackbarAndSetText } = this.props;
-    const { newpassword, confirmnewpassword } = form && form.fields;
-    if (newpassword.value === confirmnewpassword.value) {
-      submitForm(formKey);
-    } else {
-      toggleSnackbarAndSetText(true, "Password do not match", true);
-    }
-  };
-
   render() {
-    const { form, handleFieldChange, loading } = this.props;
-    const { name: formKey } = this.formConfig;
-
+    const { toggleSnackbarAndSetText } = this.props;
     return (
-      <Screen loading={loading} className="employee-change-passwd-screen">
-        <div className="row">
-          <PasswordForm form={form} submitChangePasswdForm={this.submitChangePasswdForm} formKey={formKey} onChange={handleFieldChange} />
-        </div>
+      <Screen className="employee-change-passwd-screen">
+        <PasswordFormHOC toggleSnackbarAndSetText={toggleSnackbarAndSetText} />
       </Screen>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  const formKey = "employeeChangePassword";
-  const form = state.form[formKey] || {};
-  const { loading } = form || false;
-  return { form, loading };
-};
-
 const mapDispatchToProps = (dispatch) => {
   return {
-    handleFieldChange: (formKey, fieldKey, value) => dispatch(handleFieldChange(formKey, fieldKey, value)),
-    submitForm: (formKey) => dispatch(submitForm(formKey)),
-    initForm: (form) => dispatch(initForm(form)),
     toggleSnackbarAndSetText: (open, message, error) => dispatch(toggleSnackbarAndSetText(open, message, error)),
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(ChangePassword);
+export default connect(null, mapDispatchToProps)(ChangePassword);
