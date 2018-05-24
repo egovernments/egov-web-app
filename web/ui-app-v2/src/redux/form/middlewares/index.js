7069ef43f2f84daa00f2e15d18f1bf5f77e6a08e
varun93 <1hvarun@gmail.com>
2018-05-24 13:45:06 +0530

    added hooks for field change

--- web/ui-app-v2/src/redux/form/middlewares/index.js
+++ web/ui-app-v2/src/redux/form/middlewares/index.js
@@ -1,7 +1,8 @@
 import formSubmit from "./formSubmit";
 import validation from "./validation";
+import formHooks from "./formHooks.js";
 import translateFieldText from "./translateFieldText";
 import initForm from "./initForm";
 
-const composedMiddleware = [initForm, formSubmit, translateFieldText, validation];
+const composedMiddleware = [initForm, formSubmit, translateFieldText, validation, formHooks];
 export default composedMiddleware;
