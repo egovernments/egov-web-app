79f834e1fcb0973dbf5c0b78744bbd2ad2d262be
Gyan4497 <gyanprakash92@gmail.com>
2018-05-24 15:44:24 +0530

    button add

--- web/ui-app-v2/src/modules/common/MDMS/index.js
+++ web/ui-app-v2/src/modules/common/MDMS/index.js
@@ -3,47 +3,58 @@ import { render } from "react-dom";
 import { connect } from "react-redux";
 import { fetchSpecs } from "redux/mdms/actions";
 import Screen from "modules/common/common/Screen";
-import {Icon,Button} from "components";
+import { Icon, Button } from "components";
+import "./index.css";
 
 // Import React Table
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 
+const addIconStyle = { width: 12, height: 12, marginLeft: 8, color: "#ffffff" };
+
 class MDMS extends React.Component {
   constructor() {
     super();
     this.state = {
-      data: [{
-        name:"murali",
-        info:"male",
-        stats:"test"
-      },
-      {
-        name:"tharu",
-        info:"female",
-        stats:"test"
-      }],
-      columns:[
+      data: [
+        {
+          name: "murali",
+          info: "male",
+          stats: "test",
+        },
+        {
+          name: "tharu",
+          info: "female",
+          stats: "test",
+        },
+      ],
+      columns: [
         {
           Header: "Name",
-          accessor:"name"
+          accessor: "name",
         },
         {
           Header: "Info",
-          accessor: "info"
+          accessor: "info",
         },
         {
-          Header: 'Stats',
-          accessor: "stats"
+          Header: "Stats",
+          accessor: "stats",
         },
         {
-          Header:'Actions',
-          Cell: row => <Icon onClick={()=>{
-            console.log(row.index);
-          }} action="image" name="edit"/>
-        }
+          Header: "Actions",
+          Cell: (row) => (
+            <Icon
+              onClick={() => {
+                console.log(row.index);
+              }}
+              action="image"
+              name="edit"
+            />
+          ),
+        },
       ],
-      defaultPageSize:5
+      defaultPageSize: 5,
     };
   }
 
@@ -53,29 +64,27 @@ class MDMS extends React.Component {
   };
 
   render() {
-    const { data ,defaultPageSize,columns} = this.state;
+    const { data, defaultPageSize, columns } = this.state;
     return (
       <Screen>
-        <div className="row" style={{margingTop:"33px",margingBottom:"12px"}}>
-          <div className="col-md-6 text-left">
-              Property Tax
-          </div>
+        <div className="row" style={{ margingTop: "33px", margingBottom: "12px" }}>
+          <div className="col-md-6 text-left">Property Tax</div>
 
           <div className="col-md-6 text-right">
-            <Button label="ADD" labelPosition="after" backgroundColor="#f2f2f2" icon={<Icon action="content" name="add"/>}/>
+            <Button
+              label="ADD"
+              className="add-row-mdms-table"
+              labelPosition="after"
+              backgroundColor="#fe7a51"
+              icon={<Icon action="content" name="add" color="#ffffff" style={addIconStyle} />}
+            />
           </div>
         </div>
-        <ReactTable
-          data={data}
-          columns={columns}
-          defaultPageSize={defaultPageSize}
-          className="-stripped -highlight text-center"
-        />
+        <ReactTable data={data} columns={columns} defaultPageSize={defaultPageSize} className="-stripped -highlight text-center" />
       </Screen>
     );
   }
 }
-
 
 const mapDispatchToProps = (dispatch) => {
   return {
