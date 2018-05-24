b3e47a22c6b9021a5026438058de92e8d600849a
sushant gwl <sushant.bhaskar@goodworklabs.com>
2018-05-24 16:44:23 +0530

    Added Spec Transformation

--- web/ui-app-v2/src/modules/common/MDMS/index.js
+++ web/ui-app-v2/src/modules/common/MDMS/index.js
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 import { connect } from "react-redux";
 import { fetchSpecs } from "redux/mdms/actions";
 import Screen from "modules/common/common/Screen";
-import {Icon,Button} from "components";
+import { Icon, Button } from "components";
 
 // Import React Table
 import ReactTable from "react-table";
@@ -13,73 +13,73 @@ class MDMS extends React.Component {
   constructor() {
     super();
     this.state = {
-      data: [{
-        name:"murali",
-        info:"male",
-        stats:"test"
-      },
-      {
-        name:"tharu",
-        info:"female",
-        stats:"test"
-      }],
-      columns:[
+      data: [
+        {
+          name: "murali",
+          info: "male",
+          stats: "test",
+        },
+        {
+          name: "tharu",
+          info: "female",
+          stats: "test",
+        },
+      ],
+      columns: [
         {
           Header: "Name",
-          accessor:"name"
+          accessor: "name",
         },
         {
           Header: "Info",
-          accessor: "info"
+          accessor: "info",
         },
         {
-          Header: 'Stats',
-          accessor: "stats"
+          Header: "Stats",
+          accessor: "stats",
         },
         {
-          Header:'Actions',
-          Cell: row => <Icon onClick={()=>{
-            console.log(row.index);
-          }} action="image" name="edit"/>
-        }
+          Header: "Actions",
+          Cell: (row) => (
+            <Icon
+              onClick={() => {
+                console.log(row.index);
+              }}
+              action="image"
+              name="edit"
+            />
+          ),
+        },
       ],
-      defaultPageSize:5
+      defaultPageSize: 5,
     };
   }
 
   componentDidMount = () => {
     const { fetchSpecs, match } = this.props;
-    // fetchSpecs([], match.params.moduleName, match.params.masterName, true);
+    fetchSpecs([], match.params.moduleName, match.params.masterName);
   };
 
   render() {
-    const { data ,defaultPageSize,columns} = this.state;
+    const { data, defaultPageSize, columns } = this.state;
     return (
       <Screen>
-        <div className="row" style={{margingTop:"33px",margingBottom:"12px"}}>
-          <div className="col-md-6 text-left">
-              Property Tax
-          </div>
+        <div className="row" style={{ margingTop: "33px", margingBottom: "12px" }}>
+          <div className="col-md-6 text-left">Property Tax</div>
 
           <div className="col-md-6 text-right">
-            <Button label="ADD" labelPosition="after" backgroundColor="#f2f2f2" icon={<Icon action="content" name="add"/>}/>
+            <Button label="ADD" labelPosition="after" backgroundColor="#f2f2f2" icon={<Icon action="content" name="add" />} />
           </div>
         </div>
-        <ReactTable
-          data={data}
-          columns={columns}
-          defaultPageSize={defaultPageSize}
-          className="-stripped -highlight text-center"
-        />
+        <ReactTable data={data} columns={columns} defaultPageSize={defaultPageSize} className="-stripped -highlight text-center" />
       </Screen>
     );
   }
 }
 
-
 const mapDispatchToProps = (dispatch) => {
   return {
-    fetchSpecs: (queryObj, moduleName, masterName, customEndPoint) => dispatch(fetchSpecs(queryObj, moduleName, masterName, customEndPoint)),
+    fetchSpecs: (queryObj, moduleName, masterName) => dispatch(fetchSpecs(queryObj, moduleName, masterName)),
   };
 };
 
