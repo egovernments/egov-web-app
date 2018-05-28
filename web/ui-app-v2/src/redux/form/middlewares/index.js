f72ed55040f374d6b13824b2da3c189acce1e6d9
varun93 <1hvarun@gmail.com>
2018-05-28 17:04:05 +0530

    commented hooks

--- web/ui-app-v2/src/redux/form/middlewares/index.js
+++ web/ui-app-v2/src/redux/form/middlewares/index.js
@@ -4,5 +4,5 @@ import formHooks from "./formHooks.js";
 import translateFieldText from "./translateFieldText";
 import initForm from "./initForm";
 
-const composedMiddleware = [initForm, formSubmit, translateFieldText, validation, formHooks];
+const composedMiddleware = [initForm, formSubmit, translateFieldText, validation];
 export default composedMiddleware;
