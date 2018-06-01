7eeac3bb195d13769852084d4d7eb0eddf84f074
BabikaB <babika.b@goodworklabs.com>
2018-06-01 18:16:26 +0530

    Added onClick event to dialog button

--- web/ui-app-v2/src/modules/citizen/PropertyTax/PaymentStepOne/components/YearDialogue/components/SingleButton/index.js
+++ web/ui-app-v2/src/modules/citizen/PropertyTax/PaymentStepOne/components/YearDialogue/components/SingleButton/index.js
@@ -5,13 +5,9 @@ import "./index.css";
 
 const SingleButton = ({ label, handleClose }) => {
   return (
-    <Button
-      onClick={handleClose}
-      className="year-range-button"
-      label={label}
-      labelColor="#00bbd3"
-      buttonStyle={{ borderRadius: "50px", border: "1px solid #00bbd3" }}
-    />
+    <Link to="/citizen/pt-payment/property-address">
+      <Button className="year-range-button" label={label} labelColor="#00bbd3" buttonStyle={{ borderRadius: "50px", border: "1px solid #00bbd3" }} />
+    </Link>
   );
 };
 
