7da693859c0b72b7aa76104055377e19f3a65445
Shreya <shreya.b@egovernments.org>
2018-06-05 20:37:40 +0530

    Gyans changes

--- web/rainmaker/packages/egov-ui-kit/utils/field.js
+++ web/rainmaker/packages/egov-ui-kit/utils/field.js
@@ -10,9 +10,7 @@ var _extends3 = _interopRequireDefault(_extends2);
 
 var _objectWithoutProperties2 = require("babel-runtime/helpers/objectWithoutProperties");
 
-var _objectWithoutProperties3 = _interopRequireDefault(
-  _objectWithoutProperties2
-);
+var _objectWithoutProperties3 = _interopRequireDefault(_objectWithoutProperties2);
 
 var _react = require("react");
 
@@ -20,88 +18,58 @@ var _react2 = _interopRequireDefault(_react);
 
 var _components = require("components");
 
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var Field = function Field(_ref) {
-  var fieldKey = _ref.fieldKey,
-    handleFieldChange = _ref.handleFieldChange,
-    _ref$field = _ref.field,
-    field = _ref$field === undefined ? {} : _ref$field,
-    rest = (0, _objectWithoutProperties3.default)(_ref, [
-      "fieldKey",
-      "handleFieldChange",
-      "field"
-    ]);
+var ToolTip = function ToolTip(_ref) {
+  var placement = _ref.placement,
+      show = _ref.show,
+      title = _ref.title,
+      id = _ref.id;
+
+  return _react2.default.createElement(_components.Tooltip, { enterDelay: 300, id: id, leaveDelay: 300, open: show, placement: placement, title: title });
+};
+
+var Field = function Field(_ref2) {
+  var fieldKey = _ref2.fieldKey,
+      handleFieldChange = _ref2.handleFieldChange,
+      _ref2$field = _ref2.field,
+      field = _ref2$field === undefined ? {} : _ref2$field,
+      rest = (0, _objectWithoutProperties3.default)(_ref2, ["fieldKey", "handleFieldChange", "field"]);
 
   var renderField = function renderField() {
     var type = field.type,
-      tooltip = field.tooltip,
-      label = field.label,
-      fieldProps = (0, _objectWithoutProperties3.default)(field, [
-        "type",
-        "tooltip",
-        "label"
-      ]);
+        tooltip = field.tooltip,
+        label = field.label,
+        fieldProps = (0, _objectWithoutProperties3.default)(field, ["type", "tooltip", "label"]);
 
     switch (type) {
       case "textfield":
       case "textarea":
-        return _react2.default.createElement(
-          _components.TextField,
-          (0, _extends3.default)({}, rest, fieldProps, {
-            onChange: function onChange(e, value) {
-              return handleFieldChange(fieldKey, value);
-            },
-            multiLine: type === "textarea"
-          })
-        );
+        return _react2.default.createElement(_components.TextField, (0, _extends3.default)({}, rest, fieldProps, { onChange: function onChange(e, value) {
+            return handleFieldChange(fieldKey, value);
+          }, multiLine: type === "textarea" }));
       case "mobilenumber":
-        return _react2.default.createElement(
-          _components.MobileNumberField,
-          (0, _extends3.default)({}, rest, fieldProps, {
-            onChange: function onChange(e, value) {
-              return handleFieldChange(fieldKey, value);
-            }
-          })
-        );
+        return _react2.default.createElement(_components.MobileNumberField, (0, _extends3.default)({}, rest, fieldProps, { onChange: function onChange(e, value) {
+            return handleFieldChange(fieldKey, value);
+          } }));
       case "number":
       case "password":
-        return _react2.default.createElement(
-          _components.TextField,
-          (0, _extends3.default)({}, rest, fieldProps, {
-            type: type,
-            onChange: function onChange(e, value) {
-              return handleFieldChange(fieldKey, value);
-            }
-          })
-        );
+        return _react2.default.createElement(_components.TextField, (0, _extends3.default)({}, rest, fieldProps, { type: type, onChange: function onChange(e, value) {
+            return handleFieldChange(fieldKey, value);
+          } }));
       case "checkbox":
-        return _react2.default.createElement(
-          _components.SingleCheckbox,
-          (0, _extends3.default)({}, rest, fieldProps, {
-            style: { marginTop: "27px" },
-            onCheck: function onCheck(e) {
-              return handleFieldChange(fieldKey, e.target.checked);
-            }
-          })
-        );
+        return _react2.default.createElement(_components.SingleCheckbox, (0, _extends3.default)({}, rest, fieldProps, { style: { marginTop: "27px" }, onCheck: function onCheck(e) {
+            return handleFieldChange(fieldKey, e.target.checked);
+          } }));
       case "label":
-        return _react2.default.createElement(
-          _components.Label,
-          (0, _extends3.default)({}, rest, fieldProps)
-        );
+        return _react2.default.createElement(_components.Label, (0, _extends3.default)({}, rest, fieldProps));
       case "singleValueList":
-        return _react2.default.createElement(
-          _components.DropDown,
-          (0, _extends3.default)({}, rest, fieldProps, {
-            dropDownData: fieldProps.dropDownData || [],
-            onChange: function onChange(e, value, selectedValue) {
-              return handleFieldChange(fieldKey, selectedValue);
-            }
-          })
-        );
+        return _react2.default.createElement(_components.DropDown, (0, _extends3.default)({}, rest, fieldProps, {
+          dropDownData: fieldProps.dropDownData || [],
+          onChange: function onChange(e, value, selectedValue) {
+            return handleFieldChange(fieldKey, selectedValue);
+          }
+        }));
       default:
         return null;
     }
