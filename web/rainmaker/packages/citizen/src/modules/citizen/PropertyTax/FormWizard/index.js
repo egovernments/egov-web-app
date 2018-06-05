c4516a05b7217c0ad2e8f312dc33159c4caf0993
sushant gwl <sushant.bhaskar@goodworklabs.com>
2018-06-05 13:37:18 +0530

    FormWizard Added

---
+++ web/rainmaker/packages/citizen/src/modules/citizen/PropertyTax/FormWizard/index.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import WizardComponent from "./components/WizardComponent";
+import Screen from "modules/common/common/Screen";
+import PropertyAddress from "./components/PropertyAddress";
+import BasicInformation from "./components/BasicInformation";
+import "./index.css";
+
+class FormWizard extends Component {
+  state = {
+    selected: 0,
+  };
+
+  getFormContent = (index) => {
+    switch (index) {
+      case 0:
+        return {
+          component: <PropertyAddress />,
+        };
+        break;
+      case 1:
+        return {
+          component: <BasicInformation />,
+        };
+        break;
+      default:
+        return {
+          component: null,
+        };
+    }
+  };
+
+  handleNext = () => {
+    const { selected } = this.state;
+    if (selected < 4) {
+      this.setState({ selected: selected + 1 });
+    }
+  };
+
+  handlePrev = () => {
+    const { selected } = this.state;
+    if (selected > 0) {
+      this.setState({ selected: selected - 1 });
+    }
+  };
+
+  onTabClick = (index) => {
+    this.setState({ selected: index });
+  };
+
+  render() {
+    const { selected } = this.state;
+    const { component } = this.getFormContent(selected);
+    return (
+      <div className="wizard-form-main-cont">
+        <WizardComponent
+          content={component}
+          onTabClick={this.onTabClick}
+          selected={selected}
+          handleNext={this.handleNext}
+          handlePrev={this.handlePrev}
+        />
+      </div>
+    );
+  }
+}
+
+export default FormWizard;
