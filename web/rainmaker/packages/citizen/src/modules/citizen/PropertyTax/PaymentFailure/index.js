743a5ba3aadffc73d6632a10bd7d308a12f8f9e2
Shreya <shreya.b@egovernments.org>
2018-06-13 14:53:29 +0530

    Added Payment Status screens

--- web/rainmaker/packages/citizen/src/modules/citizen/PropertyTax/PaymentFailure/index.js
+++ web/rainmaker/packages/citizen/src/modules/citizen/PropertyTax/PaymentFailure/index.js
@@ -5,29 +5,26 @@ import Label from "egov-ui-kit/utils/translationNode";
 import PaymentStatus from "../common/PaymentStatus";
 
 const receiptDetails = {
-  ReceiptNo: "PT03-067-03-117",
-  TransactionID: "AB-879-67",
-  payedDate: "24.04.18",
   OwnerName: "Harishikesh Anand",
   PropertyID: "PID-78-567",
   Property: "EB-154, Maya Enclave Harinagar, KT Marg Amritsar - 53",
   PaymentTerm: "2017-18",
-  AmountPaid: "1432.47",
+  PropertyTaxDue: "1432.47",
   button1: "Link previous payments",
   button2: "Finish",
 };
 
-const successMessages = {
-  Message1: "Thank you !",
-  Message2: "PT_RECEIPT_SUCCESS_MESSAGE",
+const failureMessages = {
+  Message1: "OOPS !",
+  Message2: "PT_RECEIPT_FAILURE_MESSAGE",
 };
 
-const icon = <Icon action="navigation" name="check" />;
+const icon = <Icon action="navigation" name="close" />;
 
 const PaymentSuccess = () => {
   return (
     <Screen>
-      <PaymentStatus receiptDetails={receiptDetails} floatingButtonColor="#22b25f" icon={icon} messages={successMessages} />
+      <PaymentStatus receiptDetails={receiptDetails} floatingButtonColor="#e74c3c" icon={icon} messages={failureMessages} />
     </Screen>
   );
 };
