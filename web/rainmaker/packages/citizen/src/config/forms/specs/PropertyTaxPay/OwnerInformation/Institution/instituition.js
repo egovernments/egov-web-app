64e4744d0466d9f0a30f3f33dfd849b54fae8892
BabikaB <babika.b@goodworklabs.com>
2018-06-14 16:37:00 +0530

    added institution formconfig

---
+++ web/rainmaker/packages/citizen/src/config/forms/specs/PropertyTaxPay/OwnerInformation/Institution/instituition.js
@@ -0,0 +1,45 @@
+const formConfig = {
+  name: "institutionDetails",
+  fields: {
+    name: {
+      id: "institution-name",
+      jsonPath: "",
+      type: "textfield",
+      floatingLabelText: "Name of Institution",
+      hintText: "Enter Institute's name",
+      errorMessage: "Enter a valid name",
+      numCols: 6,
+    },
+    type: {
+      id: "institution-type",
+      jsonPath: "",
+      type: "singleValueList",
+      floatingLabelText: "Type of Institution",
+      required: true,
+      numCols: 6,
+      hintText: "Select",
+    },
+    authorizedPerson: {
+      id: "institution-authorized-person",
+      jsonPath: "",
+      type: "textfield",
+      floatingLabelText: "Name of Authorized Person",
+      hintText: "Enter name",
+      errorMessage: "Enter a valid name",
+      required: true,
+      numCols: 6,
+    },
+    designation: {
+      id: "institution-authorized-person-designation",
+      jsonPath: "",
+      type: "textfield",
+      floatingLabelText: "Designation",
+      hintText: "Enter designation",
+      errorMessage: "Enter a valid designation",
+      required: true,
+      numCols: 6,
+    },
+  },
+};
+
+export default formConfig;
