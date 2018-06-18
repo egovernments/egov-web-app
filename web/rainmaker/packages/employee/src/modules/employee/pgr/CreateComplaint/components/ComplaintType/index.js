d9c1e211db32c2ecf564a01bbc86af82fe131a08
BabikaB <babika.b@goodworklabs.com>
2018-06-18 19:11:52 +0530

    added complaint type dialog

--- web/rainmaker/packages/employee/src/modules/employee/pgr/CreateComplaint/components/ComplaintType/index.js
+++ web/rainmaker/packages/employee/src/modules/employee/pgr/CreateComplaint/components/ComplaintType/index.js
@@ -1,28 +1,82 @@
-import React from "react";
-import { Card, TextFieldIcon } from "components";
+import React, { Component } from "react";
+import { Card, TextFieldIcon, Dialog } from "components";
 import { Link } from "react-router-dom";
 import DownArrow from "material-ui/svg-icons/navigation/arrow-drop-down";
 import "./index.css";
+import { Label } from "egov-ui-kit/utils/translationNode";
+import ComplaintType from "egov-ui-kit/common/pgr/ComplaintType";
 
-const ComplaintTypeField = ({ categories, localizationLabels, complaintType = {} }) => {
-  const complainTypeMessage =
-    (complaintType && complaintType.value && (localizationLabels["SERVICEDEFS." + (complaintType.value || "").toUpperCase()] || {}).message) || "";
+class ComplaintTypeField extends Component {
+  state = { open: false };
 
-  return (
-    <div className="complaint-type-main-cont">
-      {/* <Link to="/citizen/complaint-type"> */}
-      <TextFieldIcon
-        {...{ ...complaintType, value: complainTypeMessage }}
-        iconPosition="after"
-        fullWidth={true}
-        Icon={DownArrow}
-        name="complaint-type"
-        isRequired={true}
-        disabled={false}
-      />
-      {/* </Link> */}
-    </div>
-  );
-};
+  onClose = () => {
+    this.setState({ open: false });
+  };
+
+  onFieldClicked = () => {
+    this.setState({
+      open: true,
+    });
+  };
+
+  render() {
+    let { categories, localizationLabels, complaintType } = this.props;
+    let { onClose, onFieldClicked } = this;
+    let { open } = this.state;
+    let complainTypeMessage =
+      (complaintType && complaintType.value && (localizationLabels["SERVICEDEFS." + (complaintType.value || "").toUpperCase()] || {}).message) || "";
+    return (
+      <div className="complaint-type-main-cont">
+        <div onClick={onFieldClicked}>
+          <TextFieldIcon
+            {...{ ...complaintType, value: complainTypeMessage }}
+            iconPosition="after"
+            fullWidth={true}
+            Icon={DownArrow}
+            name="complaint-type"
+            isRequired={true}
+            disabled={false}
+          />
+        </div>
+        <Dialog
+          open={open}
+          title="Choose ComplaintType"
+          children={[<ComplaintType />]}
+          bodyStyle={{ backgroundColor: "#ffffff" }}
+          isClose={false}
+          onRequestClose={onClose}
+          contentStyle={{ width: "50%" }}
+          autoScrollBodyContent={true}
+          style={{
+            paddingTop: "0",
+            marginTop: "-30px",
+            bottom: "0",
+            height: "auto",
+          }}
+        />
+        {/* <Dialog
+          className="complaint-type-dialog"
+          titleStyle={{ textAlign: "left", padding: "24px 16px" }}
+          handleClose={onClose}
+          bodyStyle={{ padding: "0px", overflowX: "hidden", maxHeight: "100%", minHeight: "100px" }}
+          title="Choose ComplaintType"
+          modal={false}
+          open={open}
+          autoScrollBodyContent={true}
+          onRequestClose={onClose}
+          style={{
+            paddingTop: "0",
+            marginTop: "-30px",
+            bottom: "0",
+            height: "auto",
+          }}
+          isClose={true}
+        >
+          <p>hiiiiiiii</p>
+        </Dialog> */}
+      </div>
+    );
+  }
+}
 
 export default ComplaintTypeField;
