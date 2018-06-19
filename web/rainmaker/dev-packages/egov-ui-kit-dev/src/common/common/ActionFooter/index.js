97e7cf62f2ff43351cbe1771d3863ff34c1c7ed7
sushant gwl <sushant.bhaskar@goodworklabs.com>
2018-06-19 17:38:26 +0530

    Added Search feature for CSR Screens

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/ActionFooter/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/ActionFooter/index.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import { Card, Button } from "components";
 
-const ActionFooter = ({ label1, label2 }) => {
+const ActionFooter = ({ label1, label2, primaryAction, secondaryAction }) => {
   return (
-    <div className="wizard-footer col-xs-12" style={{ textAlign: "right" }}>
-      <div className="col-xs-6" style={{ float: "right" }}>
+    <div className="wizard-footer col-xs-12" style={{ textAlign: "right", padding: 0 }}>
+      <div className="col-xs-6" style={{ float: "right", padding: 0 }}>
         {label1 && (
           <Button
             label={label1}
             labelStyle={{ letterSpacing: 0.7, padding: 0, color: "#fe7a51" }}
             buttonStyle={{ border: "1px solid #fe7a51" }}
-            style={{ marginRight: 45, width: "50%" }}
+            style={{ marginRight: 45, width: "36%" }}
+            onClick={secondaryAction}
           />
         )}
         <Button
@@ -19,6 +20,7 @@ const ActionFooter = ({ label1, label2 }) => {
           backgroundColor="#fe7a51"
           labelStyle={{ letterSpacing: 0.7, padding: 0, color: "#fff" }}
           buttonStyle={{ border: 0 }}
+          onClick={primaryAction}
         />
       </div>
     </div>
--- web/rainmaker/packages/egov-ui-kit/common/common/ActionFooter/index.js
+++ web/rainmaker/packages/egov-ui-kit/common/common/ActionFooter/index.js
@@ -14,26 +14,30 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var ActionFooter = function ActionFooter(_ref) {
   var label1 = _ref.label1,
-      label2 = _ref.label2;
+      label2 = _ref.label2,
+      primaryAction = _ref.primaryAction,
+      secondaryAction = _ref.secondaryAction;
 
   return _react2.default.createElement(
     "div",
-    { className: "wizard-footer col-xs-12", style: { textAlign: "right" } },
+    { className: "wizard-footer col-xs-12", style: { textAlign: "right", padding: 0 } },
     _react2.default.createElement(
       "div",
-      { className: "col-xs-6", style: { float: "right" } },
+      { className: "col-xs-6", style: { float: "right", padding: 0 } },
       label1 && _react2.default.createElement(_components.Button, {
         label: label1,
         labelStyle: { letterSpacing: 0.7, padding: 0, color: "#fe7a51" },
         buttonStyle: { border: "1px solid #fe7a51" },
-        style: { marginRight: 45, width: "50%" }
+        style: { marginRight: 45, width: "36%" },
+        onClick: secondaryAction
       }),
       _react2.default.createElement(_components.Button, {
         label: label2,
         style: { width: "36%" },
         backgroundColor: "#fe7a51",
         labelStyle: { letterSpacing: 0.7, padding: 0, color: "#fff" },
-        buttonStyle: { border: 0 }
+        buttonStyle: { border: 0 },
+        onClick: primaryAction
       })
     )
   );
