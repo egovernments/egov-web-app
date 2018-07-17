b0699b3a79e2a9b2d2f6ea9082f91412ad3e60a0
nithindv <36623418+nithindv@users.noreply.github.com>
2018-07-17 13:20:09 +0530

    Update for https

--- web/ui-dashboard/src/constants/index.js
+++ web/ui-dashboard/src/constants/index.js
@@ -1,6 +1,6 @@
 export const API_URL =
-  process.env.REACT_APP_ES || 'http://egov-micro-dev.egovernments.org/elasticsearch';
+  process.env.REACT_APP_ES || 'https://egov-micro-dev.egovernments.org/elasticsearch';
 export const API_URL_LOC =
   process.env.REACT_APP_BOUNDARY_SERVICE_API ||
-  'http://egov-micro-dev.egovernments.org/egov-location';
+  'https://egov-micro-dev.egovernments.org/egov-location';
 export const STATE = 'AP';
