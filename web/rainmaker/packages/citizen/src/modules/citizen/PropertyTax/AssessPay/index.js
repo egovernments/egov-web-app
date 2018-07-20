408069ce257867e2cb51ba2b31724965f1c2f85f
Gyan4497 <gyanprakash92@gmail.com>
2018-07-20 09:54:56 +0530

    changes for adding table and search page in property

--- web/rainmaker/packages/citizen/src/modules/citizen/PropertyTax/AssessPay/index.js
+++ web/rainmaker/packages/citizen/src/modules/citizen/PropertyTax/AssessPay/index.js
@@ -28,9 +28,9 @@ class AssessPay extends Component {
           leftIcon: <Icon action="content" name="add" color="#484848" style={IconStyle} />,
         },
         {
-          primaryText: <Label label="Search Property" fontSize="16px" color="#484848" labelStyle={{ fontWeight: 500 }} />,
+          primaryText: "Search Property",
           route: "/search-property",
-          leftIcon: <Icon action="action" name="search" color="#484848" style={IconStyle} />,
+          leftIcon: <Icon action="action" name="search" color="#484848" style={{ marginLeft: 0, marginTop: 3 }} />,
         },
       ],
     };
