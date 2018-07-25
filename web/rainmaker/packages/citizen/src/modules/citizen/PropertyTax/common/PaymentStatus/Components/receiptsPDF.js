4335a59fb5946f2d81a7315e911dde17358f4797
BabikaB <babika.b@goodworklabs.com>
2018-07-25 11:55:52 +0530

    style changes to pt receipt

--- web/rainmaker/packages/citizen/src/modules/citizen/PropertyTax/common/PaymentStatus/Components/receiptsPDF.js
+++ web/rainmaker/packages/citizen/src/modules/citizen/PropertyTax/common/PaymentStatus/Components/receiptsPDF.js
@@ -1,12 +1,11 @@
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
-//import pdfConfig from "./pdfConfig";
-import msevaLogo from "egov-ui-kit/assets/images/pblogo.png";
+
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const generateReceipt = (role, details) => {
   let data;
-  let { owners, address, propertyDetails, tax, receipts } = details;
+  let { owners, address, propertyDetails, tax, receipts, header } = details;
   let tableborder = {
     hLineColor: function(i, node) {
       return "#979797";
@@ -23,7 +22,6 @@ const generateReceipt = (role, details) => {
   };
   switch (role) {
     case "pt-reciept-citizen":
-      let floorData = propertyDetails[0].noOfFloors || 1;
       // data for floor details
       let getFloorDetails = () => {
         let bodyData = [];
@@ -63,12 +61,29 @@ const generateReceipt = (role, details) => {
               body: [
                 [
                   {
-                    image: msevaLogo,
+                    image: header.logo || "",
                     width: 30,
                     margin: [10, 2, 10, 2],
                   },
-                  { text: "AMRITSAR MUNICIPAL CORPORATION\nProperty Tax Payment Receipt (Citizen Copy)", alignment: "center", margin: [0, 10, 0, 0] },
-                  { text: `Receipt No.:	${details.ReceiptNo}`, margin: [10, 10, 10, 2] },
+                  {
+                    //stack is used here to give multiple sections one after another in same body
+                    stack: [
+                      { text: header.header || "", style: "receipt-logo-header" },
+                      { text: header.subheader || "", style: "receipt-logo-sub-header" },
+                    ],
+                    alignment: "center",
+                    margin: [0, 5, 0, 0],
+                  },
+                  {
+                    text: [
+                      {
+                        text: "Receipt No.: ",
+                        bold: true,
+                      },
+                      details.ReceiptNo || "",
+                    ],
+                    margin: [10, 10, 10, 2],
+                  },
                 ],
               ],
             },
@@ -78,12 +93,24 @@ const generateReceipt = (role, details) => {
             style: "receipt-header-details",
             columns: [
               {
-                text: `Date: ${receipts.paymentDate}` || ``,
+                text: [
+                  {
+                    text: "Date: ",
+                    bold: true,
+                  },
+                  receipts.paymentDate || "",
+                ],
+
                 alignment: "left",
               },
               {
-                width: "*",
-                text: `Contact Us:${"080 - 09567743"}`,
+                text: [
+                  {
+                    text: "Contact Us: ",
+                    bold: true,
+                  },
+                  "080 - 09567743",
+                ],
                 alignment: "right",
               },
             ],
@@ -92,12 +119,23 @@ const generateReceipt = (role, details) => {
             style: "receipt-header-details",
             columns: [
               {
-                text: `Assessment Year: ${propertyDetails[0].financialYear || ""}`,
+                text: [
+                  {
+                    text: "Assessment Year: ",
+                    bold: true,
+                  },
+                  propertyDetails[0].financialYear || "",
+                ],
                 alignment: "left",
               },
               {
-                width: "*",
-                text: "Visit Us:	www.pmidc.com",
+                text: [
+                  {
+                    text: "Visit Us: ",
+                    bold: true,
+                  },
+                  "www.pmidc.com",
+                ],
                 alignment: "right",
               },
             ],
@@ -105,14 +143,13 @@ const generateReceipt = (role, details) => {
           {
             style: "pt-reciept-citizen-table",
             table: {
-              // widths: ["*", "*", "*", "*", "*", "*"],
               body: [
                 [
-                  { text: "Receipt No:", border: [true, true, false, true] },
+                  { text: "Receipt No:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: details.ReceiptNo || "", border: [false, true, true, true] },
-                  { text: "Property System ID:", border: [true, true, false, true] },
+                  { text: "Property System ID:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: details.propertyId || "", border: [false, true, true, true] }, //need to confirm this data
-                  { text: "Assessment ID:", border: [true, true, false, true] },
+                  { text: "Assessment ID:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: propertyDetails[0].assessmentNumber || "", border: [false, true, true, true] },
                 ],
               ],
@@ -126,15 +163,15 @@ const generateReceipt = (role, details) => {
               widths: ["*", "*", "*", "*"],
               body: [
                 [
-                  { text: "House/Door No.:", border: [true, true, false, true] },
+                  { text: "House/Door No.:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: address.doorNo || "", border: [false, true, true, true] },
-                  { text: "Building/Colony Name.:", border: [true, true, false, true] },
+                  { text: "Building/Colony Name.:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: address.buildingName || "", border: [false, true, true, true] },
                 ],
                 [
-                  { text: "Street Name:", border: [true, true, false, true] },
+                  { text: "Street Name:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: address.street || "", border: [false, true, true, true] },
-                  { text: "Locality/Mohalla:", border: [true, true, false, true] },
+                  { text: "Locality/Mohalla:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: address.locality.code || "", border: [false, true, true, true] },
                 ],
               ],
@@ -148,9 +185,9 @@ const generateReceipt = (role, details) => {
               widths: ["*", "*", "*", "*"],
               body: [
                 [
-                  { text: "Plot Size:", border: [true, true, false, true] },
+                  { text: "Plot Size:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: propertyDetails[0].landArea || "", border: [false, true, true, true] },
-                  { text: "Type of Building:", border: [true, true, false, true] },
+                  { text: "Type of Building:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: propertyDetails[0].propertySubType || "", border: [false, true, true, true] },
                 ],
               ],
@@ -172,21 +209,21 @@ const generateReceipt = (role, details) => {
               widths: ["*", "*", "*", "*"],
               body: [
                 [
-                  { text: "Owner/Company Name:", border: [true, true, false, true] },
+                  { text: "Owner/Company Name:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: owners.name || "", border: [false, true, true, true] },
-                  { text: "Type of Ownership:", border: [true, true, false, true] },
+                  { text: "Type of Ownership:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: owners.OwnershipType || "", border: [false, true, true, true] },
                 ],
                 [
-                  { text: "Mobile No.:", border: [true, true, false, true] },
+                  { text: "Mobile No.:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: owners.mobileNumber || "", border: [false, true, true, true] },
-                  { text: "Owner Category:", border: [true, true, false, true] },
+                  { text: "Owner Category:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: owners.ownerType || "", border: [false, true, true, true] },
                 ],
                 [
-                  { text: "Correspondence Address:", border: [true, true, false, true] },
+                  { text: "Correspondence Address:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: owners.correspondenceAddress || "", border: [false, true, true, true] },
-                  { text: "Owner Category ID No.:", border: [true, true, false, true] },
+                  { text: "Owner Category ID No.:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: propertyDetails[0].documents[0].id || "", border: [false, true, true, true] },
                 ],
               ],
@@ -200,15 +237,15 @@ const generateReceipt = (role, details) => {
               widths: ["*", "*", "*", "*"],
               body: [
                 [
-                  { text: "Property Tax:", border: [true, true, false, true] },
+                  { text: "Property Tax:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: tax.AmountPaid || "", border: [false, true, true, true] },
-                  { text: "Fire Cess (10%):", border: [true, true, false, true] },
+                  { text: "Fire Cess (10%):", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: tax.fireCess || "", border: [false, true, true, true] },
                 ],
                 [
-                  { text: "Rebate :", border: [true, true, false, true] },
+                  { text: "Rebate :", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: tax.rebate || "", border: [false, true, true, true] },
-                  { text: "TOTAL:", border: [true, true, false, true] },
+                  { text: "TOTAL:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: tax.total || "", border: [false, true, true, true] },
                 ],
               ],
@@ -222,19 +259,19 @@ const generateReceipt = (role, details) => {
               widths: ["*", "*", "*", "*"],
               body: [
                 [
-                  { text: "Total Amount Paid:", border: [true, true, false, true] },
-                  { text: receipts.AmountPaid || "", border: [false, true, true, true] },
-                  { text: "Payment Mode:", border: [true, true, false, true] },
+                  { text: "Total Amount Paid:", border: [true, true, false, true], style: "receipt-table-key" },
+                  { text: receipts.AmountPaid || "", border: [false, true, true, true], style: "receipt-table-value" },
+                  { text: "Payment Mode:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: receipts.payMode || "", border: [false, true, true, true] },
                 ],
                 [
-                  { text: "Transaction ID:", border: [true, true, false, true] },
+                  { text: "Transaction ID:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: receipts.transactionId || "", border: [false, true, true, true] },
-                  { text: "Bank Name:", border: [true, true, false, true] },
+                  { text: "Bank Name:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: receipts.bankName || "", border: [false, true, true, true] },
                 ],
                 [
-                  { text: "Pending Amount:", border: [true, true, false, true] },
+                  { text: "Pending Amount:", border: [true, true, false, true], style: "receipt-table-key" },
                   { text: receipts.pendingAmt || "", border: [false, true, true, true] },
                   { text: "", border: [true, true, false, true] },
                   { text: "", border: [false, true, true, true] },
@@ -243,18 +280,29 @@ const generateReceipt = (role, details) => {
             },
             layout: tableborder,
           },
+          { text: "Commissioner/EO", alignment: "right", color: "#484848", fontSize: 12, bold: true, margin: [0, 30, 0, 30] },
+          { text: "Note:", alignment: "left", style: "receipt-footer" },
+          {
+            ol: [
+              { text: "Payment received by cheque/demand draft shall be subject to realization.", margin: [0, 0, 0, 5] },
+              { text: "This document is not a proof of Property Ownership.", margin: [0, 0, 0, 5] },
+              { text: "This is a computer generated document, hence requires no signature.", margin: [0, 0, 0, 5] },
+            ],
+            alignment: "left",
+            style: "receipt-footer",
+          },
         ],
+        //define all the styles here
         styles: {
           "pt-reciept-citizen-subheader": {
             fontSize: 10,
             bold: true,
-            margin: [0, 16, 0, 8],
+            margin: [0, 16, 0, 8], //left top right bottom
             color: "#484848",
           },
           "pt-reciept-citizen-table": {
             fontSize: 10,
             color: "#484848",
-            lineColor: "red",
           },
           "receipt-assess-table": {
             fontSize: 10,
@@ -265,12 +313,38 @@ const generateReceipt = (role, details) => {
             bold: true,
             fillColor: "#D8D8D8",
             color: "#484848",
-            lineColor: "red",
           },
           "receipt-header-details": {
             fontSize: 9,
             margin: [0, 0, 0, 8],
-            // color: "484848",
+            color: "#484848",
+          },
+          "receipt-table-key": {
+            color: "#484848",
+            bold: true,
+          },
+          "receipt-table-value": {
+            color: "#484848",
+          },
+          "receipt-logo-header": {
+            color: "#484848",
+            fontSize: 16,
+            bold: true,
+            decoration: "underline",
+            // decorationStyle: "solid",
+            decorationColor: "#484848",
+          },
+          "receipt-logo-sub-header": {
+            color: "#484848",
+            fontSize: 13,
+            decoration: "underline",
+            // decorationStyle: "solid",
+            decorationColor: "#484848",
+          },
+          "receipt-footer": {
+            color: "#484848",
+            fontSize: 8,
+            margin: [0, 0, 0, 5],
           },
         },
       };
