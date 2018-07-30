60fa63cd2df06ce37250a44db78a0e0c06aa33bb
Gyan4497 <gyanprakash92@gmail.com>
2018-07-30 18:26:29 +0530

    changes for adding estimation data and important dates in review form

--- web/rainmaker/packages/citizen/src/modules/citizen/PropertyTax/ReviewForm/index.js
+++ web/rainmaker/packages/citizen/src/modules/citizen/PropertyTax/ReviewForm/index.js
@@ -5,7 +5,9 @@ import AssessmentInfo from "./components/AssessmentInfo";
 import OwnerInfo from "./components/OwnerInfo";
 import PropertyTaxDetailsCard from "./components/PropertyTaxDetails";
 import propertyAddressConfig from "./formConfigs/propertyAddress";
+import { httpRequest } from "egov-ui-kit/utils/api";
 import { connect } from "react-redux";
+import { MDMS } from "egov-ui-kit/utils/endPoints";
 
 import "./index.css";
 const defaultIconStyle = {
@@ -21,11 +23,86 @@ const AssessmentInfoIcon = <Icon style={defaultIconStyle} color="#ffffff" action
 const OwnerInfoIcon = <Icon style={defaultIconStyle} color="#ffffff" action="social" name="person" />;
 
 class ReviewForm extends Component {
-  componentDidMount() {}
+  state = {
+    importantDates: {},
+  };
+  componentDidMount() {
+    this.getImportantDates();
+  }
+
+  getImportantDates = async () => {
+    try {
+      let ImpDatesResponse = await httpRequest(MDMS.GET.URL, MDMS.GET.ACTION, [], {
+        MdmsCriteria: {
+          tenantId: "pb",
+          moduleDetails: [
+            {
+              moduleName: "PropertyTax",
+              masterDetails: [
+                {
+                  name: "Rebate",
+                },
+                {
+                  name: "Penalty",
+                },
+                {
+                  name: "Interest",
+                },
+                {
+                  name: "FireCess",
+                },
+              ],
+            },
+          ],
+        },
+      });
+      if (ImpDatesResponse && ImpDatesResponse.MdmsRes.PropertyTax) {
+        const { Interest, FireCess, Rebate, Penalty } = ImpDatesResponse.MdmsRes.PropertyTax;
+        const { value } = this.props.financialYr;
+        const intrest = this.findCorrectDateObj(value, Interest);
+        const fireCess = this.findCorrectDateObj(value, FireCess);
+        const rebate = this.findCorrectDateObj(value, Rebate);
+        const penalty = this.findCorrectDateObj(value, Penalty);
+        this.setState({
+          importantDates: {
+            intrest,
+            fireCess,
+            rebate,
+            penalty,
+          },
+        });
+      }
+    } catch (e) {
+      alert(e);
+    }
+  };
+
+  findCorrectDateObj = (financialYear, category) => {
+    const categoryYear = category.reduce((categoryYear, item) => {
+      const year = item.fromFY.slice(0, 4);
+      categoryYear.push(year);
+      return categoryYear;
+    }, []);
+    const assessYear = financialYear.slice(0, 4);
+    let chosenDateObj = {};
+    const index = categoryYear.indexOf(assessYear);
+    if (index > -1) {
+      chosenDateObj = category[index];
+    } else {
+      categoryYear.sort((a, b) => a > b);
+      for (let i = 0; i < categoryYear.length; i++) {
+        if (assessYear > categoryYear[i]) {
+          chosenDateObj = category[i - 1];
+          break;
+        }
+      }
+    }
+    return chosenDateObj;
+  };
 
   editIcon = <Icon onClick={this.handleEdit} style={defaultIconStyle} color="#ffffff" action="image" name="edit" />;
   render() {
-    let { updateIndex,stepZero,stepTwo,stepOne } = this.props;
+    let { updateIndex, stepZero, stepTwo, stepOne, estimationDetails } = this.props;
     return (
       <div>
         <PropertyAddress
@@ -75,7 +152,7 @@ class ReviewForm extends Component {
           form={propertyAddressConfig}
           component={stepTwo}
         />
-        <PropertyTaxDetailsCard />
+        <PropertyTaxDetailsCard estimationDetails={CalculationCriteria} importantDates={this.state.importantDates} />
       </div>
     );
   }
@@ -88,3 +165,44 @@ export default connect(
   null,
   mapDispatchToProps
 )(ReviewForm);
+
+const CalculationCriteria = [
+  {
+    serviceNumber: null,
+    totalAmount: 0,
+    taxAmount: 850,
+    penalty: 0,
+    exemption: 850,
+    rebate: 0,
+    fromDate: 1522540800000,
+    toDate: 1554076799000,
+    tenantId: "pb.patiala",
+    taxHeadEstimates: [
+      {
+        taxHeadCode: "PT_TAX",
+        estimateAmount: 850,
+        category: "TAX",
+      },
+      {
+        taxHeadCode: "PT_UNIT_USAGE_EXEMPTION",
+        estimateAmount: 0,
+        category: "EXEMPTION",
+      },
+      {
+        taxHeadCode: "PT_OWNER_EXEMPTION",
+        estimateAmount: 850,
+        category: "EXEMPTION",
+      },
+      {
+        taxHeadCode: "PT_TIME_REBATE",
+        estimateAmount: 0,
+        category: "EXEMPTION",
+      },
+      {
+        taxHeadCode: "PT_TIME_PENALTY",
+        estimateAmount: 0,
+        category: "EXEMPTION",
+      },
+    ],
+  },
+];
