eebf849d13bb5e51a320ffefdb11197b6dc056c1
Girish Patro <girish.prasad@goodworklabs.com>
2018-08-16 22:42:31 +0530

    add multiple owner call
    added multiple owner call

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/redux/form/middlewares/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/redux/form/middlewares/index.js
@@ -5,6 +5,7 @@ import initField from "./initField.js";
 import translateFieldText from "./translateFieldText";
 import initForm from "./initForm";
 import removeForm from "./removeForm";
+import onSetField from "./onSetField";
 
-const composedMiddleware = [initForm, formHooks, initField, formSubmit, translateFieldText, validation, removeForm];
+const composedMiddleware = [initForm, formHooks, initField, formSubmit, translateFieldText, validation, removeForm, onSetField];
 export default composedMiddleware;
