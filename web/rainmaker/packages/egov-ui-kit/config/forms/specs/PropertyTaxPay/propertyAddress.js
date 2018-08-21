ec3cd5dd6e3ce0337117471c5da5d2027a250c57
Gyan <gyanprakash92@gmail.com>
2018-08-21 18:58:12 +0530

    property address made proper

--- web/rainmaker/packages/egov-ui-kit/config/forms/specs/PropertyTaxPay/propertyAddress.js
+++ web/rainmaker/packages/egov-ui-kit/config/forms/specs/PropertyTaxPay/propertyAddress.js
@@ -6,13 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var _endPoints = require("egov-ui-kit/utils/endPoints");
 
-var _search = require("material-ui/svg-icons/action/search");
-
-var _search2 = _interopRequireDefault(_search);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var cityCode = "";
+var _actions = require("egov-ui-kit/redux/common/actions");
 
 var formConfig = {
   name: "propertyAddress",
@@ -21,14 +15,11 @@ var formConfig = {
       id: "city",
       jsonPath: "Properties[0].address.city",
       required: true,
-      toolTip: true,
-      toolTipMessage: "",
-      value: localStorage.getItem("tenant-id"),
       type: "singleValueList",
       floatingLabelText: "CORE_COMMON_CITY",
       errorStyle: { position: "absolute", bottom: -8, zIndex: 5 },
       fullWidth: true,
-      hintText: "Select",
+      hintText: "PT_COMMONS_SELECT_PLACEHOLDER",
       numcols: 6,
       dataFetchConfig: {
         url: _endPoints.CITY.GET.URL,
@@ -50,6 +41,47 @@ var formConfig = {
           fieldKey: "mohalla",
           hierarchyType: "REVENUE"
         }]
+      },
+      updateDependentFields: function updateDependentFields(_ref) {
+        var formKey = _ref.formKey,
+            field = _ref.field,
+            dispatch = _ref.dispatch,
+            state = _ref.state;
+
+        dispatch((0, _actions.prepareFormData)("Properties[0].tenantId", field.value));
+        var requestBody = {
+          MdmsCriteria: {
+            tenantId: field.value,
+            moduleDetails: [{
+              moduleName: "PropertyTax",
+              masterDetails: [{
+                name: "Floor"
+              }, {
+                name: "OccupancyType"
+              }, {
+                name: "OwnerShipCategory"
+              }, {
+                name: "OwnerType"
+              }, {
+                name: "PropertySubType"
+              }, {
+                name: "PropertyType"
+              }, {
+                name: "SubOwnerShipCategory"
+              }, {
+                name: "UsageCategoryDetail"
+              }, {
+                name: "UsageCategoryMajor"
+              }, {
+                name: "UsageCategoryMinor"
+              }, {
+                name: "UsageCategorySubMinor"
+              }]
+            }]
+          }
+        };
+
+        dispatch((0, _actions.fetchGeneralMDMSData)(requestBody, "PropertyTax", ["Floor", "OccupancyType", "OwnerShipCategory", "OwnerType", "PropertySubType", "PropertyType", "SubOwnerShipCategory", "UsageCategoryDetail", "UsageCategoryMajor", "UsageCategoryMinor", "UsageCategorySubMinor", "Rebate", "Penalty", "Interest", "FireCess"]));
       }
     },
     dummy: {
@@ -64,42 +96,41 @@ var formConfig = {
       hintText: "PT_PROPERTY_DETAILS_DOOR_NUMBER_PLACEHOLDER",
       numcols: 6,
       errorMessage: "PT_PROPERTY_DETAILS_DOOR_NUMBER_ERRORMSG",
-      errorStyle: { position: "absolute", bottom: -8, zIndex: 5 }
+      errorStyle: { position: "absolute", bottom: -8, zIndex: 5 },
+      maxLength: 64
     },
     colony: {
       id: "property-colony",
       jsonPath: "Properties[0].address.buildingName",
       type: "textfield",
-      // floatingLabelText: "PT_PROPERTY_DETAILS_COLONY_NAME",
-      floatingLabelText: "Building/Colony Name",
-      // hintText: "PT_PROPERTY_DETAILS_COLONY_NAME_PLACEHOLDER",
-      hintText: "Enter buiding/colony name",
+      floatingLabelText: "PT_PROPERTY_DETAILS_BUILDING_COLONY_NAME",
+      hintText: "PT_PROPERTY_DETAILS_BUILDING_COLONY_NAME_PLACEHOLDER",
       numcols: 6,
       errorMessage: "PT_PROPERTY_DETAILS_COLONY_NAME_ERRORMSG",
-      errorStyle: { position: "absolute", bottom: -8, zIndex: 5 }
+      errorStyle: { position: "absolute", bottom: -8, zIndex: 5 },
+      maxLength: 64
     },
     street: {
       id: "property-street",
       jsonPath: "Properties[0].address.street",
       type: "textfield",
-      // floatingLabelText: "PT_PROPERTY_DETAILS_STREET",
-      // hintText: "PT_PROPERTY_DETAILS_STREET_PLACEHOLDER"
-      floatingLabelText: "Street Name",
-      hintText: "Enter Street Name",
+      floatingLabelText: "PT_PROPERTY_DETAILS_STREET_NAME",
+      hintText: "PT_PROPERTY_DETAILS_STREET_NAME_PLACEHOLDER",
       numcols: 6,
       errorMessage: "PT_PROPERTY_DETAILS_STREET_ERRORMSG",
-      errorStyle: { position: "absolute", bottom: -8, zIndex: 5 }
+      errorStyle: { position: "absolute", bottom: -8, zIndex: 5 },
+      maxLength: 64
     },
     mohalla: {
       id: "mohalla",
       jsonPath: "Properties[0].address.locality.code",
       type: "autoSuggestDropdown",
       floatingLabelText: "PT_PROPERTY_DETAILS_MOHALLA",
-      //hintText: "PT_COMMON_SELECT_PLACEHOLDER",
-      hintText: "Select",
+      hintText: "PT_COMMONS_SELECT_PLACEHOLDER",
       fullWidth: true,
       toolTip: true,
-      toolTipMessage: "Name of the area in which your property is located",
+      toolTipMessage: "PT_MOHALLA_TOOLTIP_MESSAGE",
+      //toolTipMessage: "Name of the area in which your property is located",
       boundary: true,
       numcols: 6,
       errorMessage: "PT_PROPERTY_DETAILS_MOHALLA_ERRORMSG",
@@ -110,33 +141,47 @@ var formConfig = {
         requestBody: {},
         isDependent: true
       },
-      dropDownData: [],
       errorStyle: { position: "absolute", bottom: -8, zIndex: 5 },
-      required: true
+      required: true,
+      updateDependentFields: function updateDependentFields(_ref2) {
+        var formKey = _ref2.formKey,
+            field = _ref2.field,
+            dispatch = _ref2.dispatch;
+
+        var mohalla = field.dropDownData.find(function (option) {
+          return option.value === field.value;
+        });
+        dispatch((0, _actions.prepareFormData)("Properties[0].address.locality.area", mohalla.area));
+      }
     },
     pincode: {
       id: "pincode",
-      type: "textfield",
+      type: "number",
       jsonPath: "Properties[0].address.pincode",
       floatingLabelText: "PT_PROPERTY_DETAILS_PINCODE",
       hintText: "PT_PROPERTY_DETAILS_PINCODE_PLACEHOLDER",
       numcols: 6,
-      errorMessage: "PT_PROPERTY_DETAILS_PINCODE_ERRORMSG",
-      errorStyle: { position: "absolute", bottom: -8, zIndex: 5 }
+      //errorMessage: "PT_PROPERTY_DETAILS_PINCODE_ERRORMSG",
+      errorMessage: "Pincode should be 6 digits",
+      errorStyle: { position: "absolute", bottom: -8, zIndex: 5 },
+      pattern: "^([0-9]){6}$"
     },
     oldPID: {
       id: "oldpid",
       type: "textFieldIcon",
-      Icon: _search2.default,
-      iconRedirectionURL: "https://www.google.co.in/",
+      className: "pt-old-pid-text-field",
+      text: "SEARCH",
+      iconRedirectionURL: "https://pmidc.punjab.gov.in/propertymis/search.php",
       jsonPath: "Properties[0].oldPropertyId",
-      floatingLabelText: "PT_PROPERTY_ADDRESS_OLDPID",
-      hintText: "PT_PROPERTY_ADDRESS_OLDPID_PLACEHOLDER",
+      floatingLabelText: "PT_PROPERTY_ADDRESS_EXISTING_PID",
+      hintText: "PT_PROPERTY_ADDRESS_EXISTING_PID_PLACEHOLDER",
       numcols: 6,
       errorMessage: "PT_PROPERTY_DETAILS_PINCODE_ERRORMSG",
       errorStyle: { position: "absolute", bottom: -8, zIndex: 5 },
       toolTip: true,
-      toolTipMessage: "ID assigned to your property by your Municipality "
+      pattern: /^[a-zA-Z0-9\\\/\-_\s]{1,64}$/i,
+      toolTipMessage: "PT_OLDPID_TOOLTIP_MESSAGE",
+      maxLength: 64
     }
   },
 
