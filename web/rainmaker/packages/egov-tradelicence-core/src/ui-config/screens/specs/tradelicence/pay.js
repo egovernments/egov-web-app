72363313edb79b55349fdab62a4d55faf96b53fb
avijeet7 <avijeet7@gmail.com>
2018-09-21 15:53:03 +0530

    Application no. in pay page

--- web/rainmaker/packages/egov-tradelicence-core/src/ui-config/screens/specs/tradelicence/pay.js
+++ web/rainmaker/packages/egov-tradelicence-core/src/ui-config/screens/specs/tradelicence/pay.js
@@ -1,19 +1,26 @@
 import {
-  getStepperObject,
+  getCommonContainer,
   getCommonHeader,
   getCommonCard,
   getCommonTitle,
-  getCommonParagraph,
-  getLabel
+  getCommonParagraph
 } from "mihy-ui-framework/ui-config/screens/specs/utils";
 
 import { footer } from "./payResource/footer";
 import estimateDetails from "./payResource/estimate-details";
-import g8Details from "./payResource/g8-details"
+import g8Details from "./payResource/g8-details";
 import capturePaymentDetails from "./payResource/capture-payment-details";
 
-const header = getCommonHeader("Payment for New Trade License (2018-2019)");
-
+const header = getCommonContainer({
+  header: getCommonHeader("Payment for New Trade License (2018-2019)"),
+  applicationNumber: {
+    uiFramework: "custom-atoms-local",
+    componentPath: "ApplicationNoContainer",
+    props: {
+      number: 5434
+    }
+  }
+});
 
 const screenConfig = {
   uiFramework: "material-ui",
@@ -26,14 +33,14 @@ const screenConfig = {
         className: "common-div-css"
       },
       children: {
-        headerDiv:{
+        headerDiv: {
           uiFramework: "custom-atoms",
           componentPath: "Container",
-          children:{
-            header:{
-              gridDefination:{
-                xs:"12",
-                sm:"10"
+          children: {
+            header: {
+              gridDefination: {
+                xs: "12",
+                sm: "10"
               },
               ...header
             }
@@ -43,18 +50,17 @@ const screenConfig = {
           uiFramework: "custom-atoms",
           componentPath: "Div",
           children: {
-            paymentDetails:getCommonCard({
-              header: getCommonTitle("Review your estimated fees and enter the payment collection details"),
+            paymentDetails: getCommonCard({
+              header: getCommonTitle(
+                "Review your estimated fees and enter the payment collection details"
+              ),
               paragraph: getCommonParagraph(
                 "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard Lorem Ipsum has been the industry's standard."
               ),
               estimateDetails,
               capturePaymentDetails,
               g8Details
-
             })
-
-
           }
         },
         footer
