0f7f655cec2a4386d7815c8252ce8ffc11dbdee1
gopesh <gopesh.yadav@egovernments.org>
2018-09-24 13:34:11 +0530

    added pending approval table and added functionality of disabling 'New Application' button and 'pending approval' table in 'Search Trade License Application'

--- web/rainmaker/packages/egov-tradelicence-core/src/ui-config/screens/specs/tradelicence/search.js
+++ web/rainmaker/packages/egov-tradelicence-core/src/ui-config/screens/specs/tradelicence/search.js
@@ -1,80 +1,171 @@
 import {
-    getCommonHeader,
-    getLabel,
-  } from "mihy-ui-framework/ui-config/screens/specs/utils";
-  import React from "react";
-  import { tradeLicenseApplication } from "./searchResource/tradeLicenseApplication";
+  getCommonHeader,
+  getLabel,
+  getBreak
+} from "mihy-ui-framework/ui-config/screens/specs/utils";
+import React from "react";
+import { tradeLicenseApplication } from "./searchResource/tradeLicenseApplication";
 
-  const header = getCommonHeader("Trade License");
+import { getQueryArg } from "mihy-ui-framework/ui-utils/commons";
 
-  const tradeLicenseSearchBox = {
-    uiFramework: "material-ui",
-    name: "search",
-    components: {
-      div: {
-        uiFramework: "custom-atoms",
-        componentPath: "Div",
-        props: {
-          className: "common-div-css"
-        },
-        children:
-        {
-          headerDiv: {
-            uiFramework: "custom-atoms",
-            componentPath: "Container",
+const hasButton = getQueryArg(window.location.href, "hasButton");
+const hasApproval = getQueryArg(window.location.href, "hasApproval");
+let enableInbox,
+  enableButton = true;
+enableInbox = hasApproval && hasApproval == "false" ? false : true;
+enableButton = hasButton && hasButton == "false" ? false : true;
 
-            children: {
-              header: {
-                gridDefination: {
-                  xs: "12",
-                  sm: "6"
-                },
-                ...header
+const header = getCommonHeader("Trade License");
+const tradeLicenseSearchAndResult = {
+  uiFramework: "material-ui",
+  name: "search",
+  components: {
+    div: {
+      uiFramework: "custom-atoms",
+      componentPath: "Div",
+      props: {
+        className: "common-div-css"
+      },
+      children: {
+        headerDiv: {
+          uiFramework: "custom-atoms",
+          componentPath: "Container",
+
+          children: {
+            header: {
+              gridDefination: {
+                xs: "12",
+                sm: "6"
               },
-              newApplicationButton: {
-                componentPath: "Button",
-                gridDefination: {
-                  xs: "12",
-                  sm: "6",
-                  align: "right"
-                },
-                props: {
-                  variant: "contained",
-                  color: "primary",
-                  style: {
-                    color: "white",
-                    borderRadius: "2px",
-                    width: "250px",
-                    height: "48px"
+              ...header
+            },
+            newApplicationButton: {
+              componentPath: "Button",
+              gridDefination: {
+                xs: "12",
+                sm: "6",
+                align: "right"
+              },
+              visible: enableButton,
+              props: {
+                variant: "contained",
+                color: "primary",
+                style: {
+                  color: "white",
+                  borderRadius: "2px",
+                  width: "250px",
+                  height: "48px"
+                }
+              },
+
+              children: {
+                plusIconInsideButton: {
+                  uiFramework: "custom-atoms",
+                  componentPath: "Icon",
+                  props: {
+                    iconName: "add",
+                    style: {
+                      fontSize: "36px"
+                    }
                   }
                 },
 
-                children: {
+                buttonLabel: getLabel("NEW APPLICATION")
+              }
+            }
+          }
+        },
 
-                  plusIconInsideButton: {
-                    uiFramework: "custom-atoms",
-                    componentPath: "Icon",
-                    props:
-                    {
-                      iconName: "add",
-                      style: {
-                        fontSize: "36px"
-                      }
-                    }
+        divForPendingApprovals: {
+          uiFramework: "custom-atoms",
+          componentPath: "Div",
+          visible: enableInbox,
+          children: {
+            breakPending: getBreak(),
+            pendingApprovals: {
+              uiFramework: "custom-molecules-local",
+              componentPath: "Table",
+              props: {
+                data: [
+                  {
+                    "Application No": 1234,
+                    "Trade Name": "Matchbox Plant",
+                    "Owner Name": "Satinder Singh",
+                    "Locality/Mohalla": "Gurudwara Mohalla",
+                    "Payment Date": "12/08/2018",
+                    "Days Elapsed": "2 Days"
                   },
-
-                  buttonLabel: getLabel("NEW APPLICATION"),
+                  {
+                    "Application No": 1234,
+                    "Trade Name": "Matchbox Plant",
+                    "Owner Name": "Satinder Singh",
+                    "Locality/Mohalla": "Railway Colony",
+                    "Payment Date": "12/08/2018",
+                    "Days Elapsed": "10 Days"
+                  },
+                  {
+                    "Application No": 1234,
+                    "Trade Name": "Matchbox Plant",
+                    "Owner Name": "Satinder Singh",
+                    "Locality/Mohalla": "Gurudwara Mohalla",
+                    "Payment Date": "12/08/2018",
+                    "Days Elapsed": "2 Days"
+                  },
+                  {
+                    "Application No": 1234,
+                    "Trade Name": "Matchbox Plant",
+                    "Owner Name": "Satinder Singh",
+                    "Locality/Mohalla": "Assi Mohalla",
+                    "Payment Date": "12/08/2018",
+                    "Days Elapsed": "2 Days"
+                  }
+                ],
+                columns: {
+                  "Application No": {},
+                  "Trade Name": {},
+                  "Owner Name": {},
+                  "Locality/Mohalla": {},
+                  "Payment Date": {},
+                  "Days Elapsed": {
+                    format: value => {
+                      let color = "";
+                      if (value.toLowerCase().indexOf("10") !== -1) {
+                        color = "green";
+                      } else if (value.toLowerCase().indexOf("2") !== -1) {
+                        color = "red";
+                      }
+                      return (
+                        <span
+                          style={{
+                            color: color,
+                            fontSize: "14px",
+                            fontWeight: 400
+                          }}
+                        >
+                          {value}
+                        </span>
+                      );
+                    }
+                  }
+                },
+                title: "Pending for your Approval (4)",
+                options: {
+                  filterType: "dropdown",
+                  responsive: "scroll",
+                  selectableRows: false
                 }
               }
             }
-          },
-          tradeLicenseApplication,
-          searchResults: {
-            uiFramework: "custom-molecules-local",
-            componentPath: "Table",
-            props:
-            {
-              data:[{
+          }
+        },
+        tradeLicenseApplication,
+        breakAfterSearch: getBreak(),
+        searchResults: {
+          uiFramework: "custom-molecules-local",
+          componentPath: "Table",
+          props: {
+            data: [
+              {
                 "Application No": 1234,
                 "License No": 345,
                 "Trade Name": "Name of the trade",
@@ -129,41 +220,47 @@ import {
                 "Owner Name": "Satinder Singh",
                 "Application Date": "12/08/2018",
                 Status: "Approved"
-              },],
-              columns:{
-                "Application No": {},
-                "License No": {},
-                "Trade Name": {},
-                "Owner Name": {},
-                "Application Date": {},
-                Status: {
-                  format: value => {
-                    let color = "";
-                    if (value.toLowerCase().indexOf("approved") !== -1) {
-                      color = "green";
-                    } else if (value.toLowerCase().indexOf("pending") !== -1) {
-                      color = "red";
-                    }
-                    return (
-                      <span style={{ color: color, fontSize: "14px", fontWeight: 400 }}>
-                        {value}
-                      </span>
-                    );
-                  }
-                }
-              },
-              title:"Search Results for Trade License Applications (27)",
-              options: {
-                filterType: "dropdown",
-                responsive: "scroll",
-                selectableRows: false
               }
+            ],
+            columns: {
+              "Application No": {},
+              "License No": {},
+              "Trade Name": {},
+              "Owner Name": {},
+              "Application Date": {},
+              Status: {
+                format: value => {
+                  let color = "";
+                  if (value.toLowerCase().indexOf("approved") !== -1) {
+                    color = "green";
+                  } else if (value.toLowerCase().indexOf("pending") !== -1) {
+                    color = "red";
+                  }
+                  return (
+                    <span
+                      style={{
+                        color: color,
+                        fontSize: "14px",
+                        fontWeight: 400
+                      }}
+                    >
+                      {value}
+                    </span>
+                  );
+                }
+              }
+            },
+            title: "Search Results for Trade License Applications (27)",
+            options: {
+              filterType: "dropdown",
+              responsive: "scroll",
+              selectableRows: false
             }
           }
         }
       }
     }
+  }
+};
 
-  };
-
-  export default tradeLicenseSearchBox;
+export default tradeLicenseSearchAndResult;
