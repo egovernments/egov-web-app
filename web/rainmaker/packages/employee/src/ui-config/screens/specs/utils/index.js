3a6258712ca4a28a14b1011fd041c5ebfd1bfd2a
muralimgwl <murali.m@goodworklabs.com>
2018-10-09 03:14:21 +0530

    added scss file

--- web/rainmaker/packages/employee/src/ui-config/screens/specs/utils/index.js
+++ web/rainmaker/packages/employee/src/ui-config/screens/specs/utils/index.js
@@ -12,7 +12,8 @@ import set from "lodash/set";
 import { httpRequest } from "../../../../ui-utils/api";
 import { prepareFinalObject } from "mihy-ui-framework/ui-redux/screen-configuration/actions";
 
-const queryValue = getQueryArg(window.location.href, "purpose");
+const queryValue = "purpose";
+// getQueryArg(window.location.href, "purpose");  
 
 export const getCommonApplyFooter = children => {
   return {
