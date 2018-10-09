f14c7c14192d19cb79f044a44951c68ae588577a
muralimgwl <murali.m@goodworklabs.com>
2018-10-09 22:17:31 +0530

    cleaned up code

--- web/rainmaker/packages/employee/src/ui-utils/index.js
+++ web/rainmaker/packages/employee/src/ui-utils/index.js
@@ -1,8 +1,6 @@
-import {addComponentJsonpath} from "mihy-ui-framework/ui-utils/commons";
 import {httpRequest,loginRequest,logoutRequest} from "mihy-ui-framework/ui-utils/api";
 
 export {
-  addComponentJsonpath,
   httpRequest,
   loginRequest,
   logoutRequest
