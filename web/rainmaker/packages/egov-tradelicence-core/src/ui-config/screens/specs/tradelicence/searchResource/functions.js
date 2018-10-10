3b7b9611a1a07824672964b8599a7d6e43313e01
gopesh <gopesh.yadav@egovernments.org>
2018-10-10 16:00:45 +0530

    replaced alert with toast in search

--- web/rainmaker/packages/egov-tradelicence-core/src/ui-config/screens/specs/tradelicence/searchResource/functions.js
+++ web/rainmaker/packages/egov-tradelicence-core/src/ui-config/screens/specs/tradelicence/searchResource/functions.js
@@ -2,25 +2,41 @@ import get from "lodash/get";
 import { handleScreenConfigurationFieldChange as handleField } from "mihy-ui-framework/ui-redux/screen-configuration/actions";
 import { getSearchResults } from "mihy-ui-framework/ui-utils/commons";
 import { convertEpochToDate, convertDateToEpoch } from "../../utils/index";
+import { toggleSnackbarAndSetText } from "mihy-ui-framework/ui-redux/app/actions";
 
 export const searchApiCall = async (state, dispatch) => {
-  let queryObject = [{ key: "tenantId", value: "pb.amritsar" },
-  { key: "limit", value: "200" }, { key: "offset", value: "0" }];
+  let queryObject = [
+    { key: "tenantId", value: "pb.amritsar" },
+    { key: "limit", value: "200" },
+    { key: "offset", value: "0" }
+  ];
   let searchScreenObject = get(
     state.screenConfiguration.preparedFinalObject,
     "searchScreen",
     {}
   );
 
-
-  if ((Object.keys(searchScreenObject).length == 0) || (Object.values(searchScreenObject).every(x => (x === "")))) {
-    alert("Please fill at least one field to start search");
-  }
-  else if (((searchScreenObject["fromDate"] === undefined) || (searchScreenObject["fromDate"].length === 0)) && (searchScreenObject["toDate"] !== undefined) && (searchScreenObject["toDate"].length !== 0)) {
-
-    alert("Please fill From Date");
-  }
-  else {
+  if (
+    Object.keys(searchScreenObject).length == 0 ||
+    Object.values(searchScreenObject).every(x => x === "")
+  ) {
+    dispatch(
+      toggleSnackbarAndSetText(
+        true,
+        "Please fill at least one field to start search",
+        "warning"
+      )
+    );
+  } else if (
+    (searchScreenObject["fromDate"] === undefined ||
+      searchScreenObject["fromDate"].length === 0) &&
+    searchScreenObject["toDate"] !== undefined &&
+    searchScreenObject["toDate"].length !== 0
+  ) {
+    dispatch(
+      toggleSnackbarAndSetText(true, "Please fill From Date", "warning")
+    );
+  } else {
     showHideTable(false, dispatch);
     showHideProgress(true, dispatch);
     for (var key in searchScreenObject) {
@@ -52,7 +68,7 @@ export const searchApiCall = async (state, dispatch) => {
         "Trade Name": item.tradeName || "-",
         "Owner Name": item.tradeLicenseDetail.owners[0].name || "-",
         "Application Date": convertEpochToDate(item.applicationDate) || "-",
-        "tenantId": item.tenantId,
+        tenantId: item.tenantId,
         Status: item.status || "-"
       }));
 
@@ -70,7 +86,7 @@ export const searchApiCall = async (state, dispatch) => {
           "components.div.children.searchResults",
           "props.title",
           `Search Results for Trade License Applications (${
-          response.Licenses.length
+            response.Licenses.length
           })`
         )
       );
@@ -78,6 +94,7 @@ export const searchApiCall = async (state, dispatch) => {
       showHideTable(true, dispatch);
     } catch (error) {
       showHideProgress(false, dispatch);
+      dispatch(toggleSnackbarAndSetText(true, error.message, "error"));
       console.log(error);
     }
   }
@@ -103,5 +120,3 @@ const showHideTable = (booleanHideOrShow, dispatch) => {
     )
   );
 };
-
-
