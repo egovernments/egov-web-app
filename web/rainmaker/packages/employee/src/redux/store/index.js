ec74d589e74b19245f47e2c0d5571c7393d99977
muralimgwl <murali.m@goodworklabs.com>
2018-10-10 17:30:28 +0530

    updated iframecomponent

--- web/rainmaker/packages/employee/src/redux/store/index.js
+++ web/rainmaker/packages/employee/src/redux/store/index.js
@@ -1,5 +1,4 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import { combineReducers } from "redux";
+import {combineReducers, createStore, applyMiddleware, compose } from "redux";
 import storeConfigs from "egov-ui-kit/redux/store";
 import employeeReducer from "./reducer";
 import screenConfigurationMiddleware from "mihy-ui-framework/ui-redux/screen-configuration/middlewares";
