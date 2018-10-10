66c3d2299d5fe7e8fe534367bcc3a373c23111c8
Gyan <gyanprakash92@gmail.com>
2018-10-10 12:16:35 +0530

    commented index.css import

--- web/rainmaker/packages/employee/src/ui-containers-local/iframe/index.js
+++ web/rainmaker/packages/employee/src/ui-containers-local/iframe/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./index.css";
+// import "./index.css";
 
 const Iframe = (props) => {
   return <iframe src={props.src} frameBorder="0" allowFullScreen className="iframe-style" {...props} />;
