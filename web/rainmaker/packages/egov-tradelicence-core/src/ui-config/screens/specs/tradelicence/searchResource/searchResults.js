821f88ac10c72044856c6ccaa04c87e90af3743c
avijeet7 <avijeet7@gmail.com>
2018-10-11 16:10:32 +0530

    Error handling in search results

--- web/rainmaker/packages/egov-tradelicence-core/src/ui-config/screens/specs/tradelicence/searchResource/searchResults.js
+++ web/rainmaker/packages/egov-tradelicence-core/src/ui-config/screens/specs/tradelicence/searchResource/searchResults.js
@@ -2,28 +2,54 @@ import React from "react";
 import { Link } from "react-router-dom";
 import get from "lodash/get";
 
-const getLocalTextFromCode = (localCode) => {
-  return JSON.parse(
-    localStorage.getItem("localization_en_IN")
-  ).find(item =>
-    (item.code == localCode)
+const getLocalTextFromCode = localCode => {
+  return JSON.parse(localStorage.getItem("localization_en_IN")).find(
+    item => item.code == localCode
   );
 };
 
 export const textToLocalMapping = {
-  "Application No": getLocalTextFromCode("TL_COMMON_TABLE_COL_APP_NO")["message"],
-  "License No": getLocalTextFromCode("TL_COMMON_TABLE_COL_LIC_NO")["message"],
-  "Trade Name": getLocalTextFromCode("TL_COMMON_TABLE_COL_TRD_NAME")["message"],
-  "Owner Name": getLocalTextFromCode("TL_COMMON_TABLE_COL_OWN_NAME")["message"],
-  "Application Date": getLocalTextFromCode("TL_COMMON_TABLE_COL_APP_DATE")["message"],
-  "Status": getLocalTextFromCode("TL_COMMON_TABLE_COL_STATUS")["message"],
-  "INITIATED": getLocalTextFromCode("TL_INITIATED")["message"],
-  "APPLIED": getLocalTextFromCode("TL_APPLIED")["message"],
-  "PAID": getLocalTextFromCode("TL_PAID")["message"],
-  "APPROVED": getLocalTextFromCode("TL_APPROVED")["message"],
-  "REJECTED": getLocalTextFromCode("TL_REJECTED")["message"],
-  "CANCELLED": getLocalTextFromCode("TL_CANCELLED")["message"],
-  "Search Results for Trade License Applications": getLocalTextFromCode("TL_HOME_SEARCH_RESULTS_TABLE_HEADING")["message"]
+  "Application No": get(
+    getLocalTextFromCode("TL_COMMON_TABLE_COL_APP_NO"),
+    "message",
+    "Application No"
+  ),
+  "License No": get(
+    getLocalTextFromCode("TL_COMMON_TABLE_COL_LIC_NO"),
+    "message",
+    "License No"
+  ),
+  "Trade Name": get(
+    getLocalTextFromCode("TL_COMMON_TABLE_COL_TRD_NAME"),
+    "message",
+    "Trade Name"
+  ),
+  "Owner Name": get(
+    getLocalTextFromCode("TL_COMMON_TABLE_COL_OWN_NAME"),
+    "message",
+    "Owner Name"
+  ),
+  "Application Date": get(
+    getLocalTextFromCode("TL_COMMON_TABLE_COL_APP_DATE"),
+    "message",
+    "Application Date"
+  ),
+  Status: get(
+    getLocalTextFromCode("TL_COMMON_TABLE_COL_STATUS"),
+    "message",
+    "Status"
+  ),
+  INITIATED: get(getLocalTextFromCode("TL_INITIATED"), "message", "INITIATED"),
+  APPLIED: get(getLocalTextFromCode("TL_APPLIED"), "message", "APPLIED"),
+  PAID: get(getLocalTextFromCode("TL_PAID"), "message", "PAID"),
+  APPROVED: get(getLocalTextFromCode("TL_APPROVED"), "message", "APPROVED"),
+  REJECTED: get(getLocalTextFromCode("TL_REJECTED"), "message", "REJECTED"),
+  CANCELLED: get(getLocalTextFromCode("TL_CANCELLED"), "message", "CANCELLED"),
+  "Search Results for Trade License Applications": get(
+    getLocalTextFromCode("TL_HOME_SEARCH_RESULTS_TABLE_HEADING"),
+    "message",
+    "Search Results for Trade License Applications"
+  )
 };
 
 export const searchResults = {
@@ -36,7 +62,9 @@ export const searchResults = {
       [get(textToLocalMapping, "Application No")]: {
         format: rowData => {
           return (
-            <Link to={onRowClick(rowData)}>{rowData[get(textToLocalMapping, "Application No")]}</Link>
+            <Link to={onRowClick(rowData)}>
+              {rowData[get(textToLocalMapping, "Application No")]}
+            </Link>
           );
         }
       },
@@ -67,14 +95,17 @@ export const searchResults = {
         }
       }
     },
-    title: get(textToLocalMapping, "Search Results for Trade License Applications"),
+    title: get(
+      textToLocalMapping,
+      "Search Results for Trade License Applications"
+    ),
     options: {
       filter: false,
       download: false,
       responsive: "stacked",
       selectableRows: false,
       hover: true,
-      rowsPerPageOptions: [10, 20, 40],
+      rowsPerPageOptions: [10, 20, 40]
     }
   }
 };
@@ -83,28 +114,29 @@ const onRowClick = rowData => {
   switch (rowData[get(textToLocalMapping, "Status")]) {
     case get(textToLocalMapping, "APPLIED"):
       return `/mihy-ui-framework/tradelicence/search-preview?status=pending_payment&role=approver&applicationNumber=${
-        rowData[get(textToLocalMapping, "Application No")]}&tenantId=${rowData["tenantId"]}`;
+        rowData[get(textToLocalMapping, "Application No")]
+      }&tenantId=${rowData["tenantId"]}`;
     case get(textToLocalMapping, "APPROVED"):
       return `/mihy-ui-framework/tradelicence/search-preview?status=approved&role=approver&applicationNumber=${
         rowData[get(textToLocalMapping, "Application No")]
-        }&tenantId=${rowData["tenantId"]}`;
+      }&tenantId=${rowData["tenantId"]}`;
 
     case get(textToLocalMapping, "PAID"):
       return `/mihy-ui-framework/tradelicence/search-preview?status=pending_approval&role=approver&applicationNumber=${
         rowData[get(textToLocalMapping, "Application No")]
-        }&tenantId=${rowData["tenantId"]}`;
+      }&tenantId=${rowData["tenantId"]}`;
     case get(textToLocalMapping, "CANCELLED"):
       return `/mihy-ui-framework/tradelicence/search-preview?status=cancelled&role=approver&applicationNumber=${
         rowData[get(textToLocalMapping, "Application No")]
-        }&tenantId=${rowData["tenantId"]}`;
+      }&tenantId=${rowData["tenantId"]}`;
     case get(textToLocalMapping, "INITIATED"):
       return `/mihy-ui-framework/tradelicence/apply?applicationNumber=${
         rowData[get(textToLocalMapping, "Application No")]
-        }&tenantId=${rowData["tenantId"]}`;
+      }&tenantId=${rowData["tenantId"]}`;
     case get(textToLocalMapping, "REJECTED"):
       return `/mihy-ui-framework/tradelicence/search-preview?status=rejected&role=approver&applicationNumber=${
         rowData[get(textToLocalMapping, "Application No")]
-        }&tenantId=${rowData["tenantId"]}`;
+      }&tenantId=${rowData["tenantId"]}`;
     default:
       return `/mihy-ui-framework/tradelicence/search`;
   }
