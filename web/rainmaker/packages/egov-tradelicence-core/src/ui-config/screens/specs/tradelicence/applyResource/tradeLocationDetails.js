7b7032adef6b6a09a0f7177982c26ca01d03845f
Gyan <gyanprakash92@gmail.com>
2018-10-12 13:55:56 +0530

    boundary made non-mandatory and updated package.json to 40 version

--- web/rainmaker/packages/egov-tradelicence-core/src/ui-config/screens/specs/tradelicence/applyResource/tradeLocationDetails.js
+++ web/rainmaker/packages/egov-tradelicence-core/src/ui-config/screens/specs/tradelicence/applyResource/tradeLocationDetails.js
@@ -79,9 +79,9 @@ export const tradeLocationDetails = getCommonCard({
           gridDefination: { xs: 1 }
         }
       },
-      gridDefination:{
-        xs:12,
-        sm:6
+      gridDefination: {
+        xs: 12,
+        sm: 6
       }
     },
     tradeLocCity: {
@@ -185,7 +185,7 @@ export const tradeLocationDetails = getCommonCard({
         jsonPath: "Licenses[0].tradeLicenseDetail.address.locality.code",
         sourceJsonPath: "applyScreenMdmsData.tenant.localities",
         fullwidth: true,
-        // required: true,
+        required: false,
         inputLabelProps: {
           shrink: true
         }
@@ -193,7 +193,7 @@ export const tradeLocationDetails = getCommonCard({
       gridDefination: {
         xs: 12,
         sm: 6
-      },
+      }
       // required: true
     },
     tradeLocPincode: getTextField({
@@ -259,7 +259,7 @@ export const tradeLocationDetails = getCommonCard({
       },
       pattern: getPattern("ElectricityConnNo"),
       jsonPath:
-        "Licenses[0].tradeLicenseDetail.additionalDetail.electricityConnNo"
+        "Licenses[0].tradeLicenseDetail.additionalDetail.electricityConnectionNo"
     })
   }),
   mapsDialog: {
