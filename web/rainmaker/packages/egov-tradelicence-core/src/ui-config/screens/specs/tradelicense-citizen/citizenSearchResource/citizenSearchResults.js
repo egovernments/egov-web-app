2dae2e5cee91507f997dd24f93d2f139bb04b141
avijeet7 <avijeet7@gmail.com>
2018-10-15 22:43:34 +0530

    Citizen search/home page

---
+++ web/rainmaker/packages/egov-tradelicence-core/src/ui-config/screens/specs/tradelicense-citizen/citizenSearchResource/citizenSearchResults.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import get from "lodash/get";
+import { textToLocalMapping } from "./citizenFunctions";
+
+export const searchResults = {
+  uiFramework: "custom-molecules-local",
+  componentPath: "Table",
+  visible: true,
+  props: {
+    data: [],
+    columns: {
+      [get(textToLocalMapping, "Application No")]: {
+        format: rowData => {
+          return (
+            <Link to={onRowClick(rowData)}>
+              {rowData[get(textToLocalMapping, "Application No")]}
+            </Link>
+          );
+        }
+      },
+      [get(textToLocalMapping, "License No")]: {},
+      [get(textToLocalMapping, "Trade Name")]: {},
+      [get(textToLocalMapping, "Owner Name")]: {},
+      [get(textToLocalMapping, "Application Date")]: {},
+      [get(textToLocalMapping, "Status")]: {
+        format: rowData => {
+          let value = rowData[get(textToLocalMapping, "Status")];
+          let color = "";
+          if (value.indexOf(get(textToLocalMapping, "APPROVED")) !== -1) {
+            color = "green";
+          } else {
+            color = "red";
+          }
+          return (
+            <span
+              style={{
+                color: color,
+                fontSize: "14px",
+                fontWeight: 400
+              }}
+            >
+              {value}
+            </span>
+          );
+        }
+      }
+    },
+    title: get(textToLocalMapping, "MY_APPLICATIONS"),
+    options: {
+      filter: false,
+      download: false,
+      responsive: "stacked",
+      selectableRows: false,
+      hover: true,
+      rowsPerPageOptions: [10, 20, 40]
+    }
+  }
+};
+
+const onRowClick = rowData => {
+  switch (rowData[get(textToLocalMapping, "Status")]) {
+    case get(textToLocalMapping, "APPLIED"):
+      return `/mihy-ui-framework/tradelicense-citizen/search-preview?status=pending_payment&role=approver&applicationNumber=${
+        rowData[get(textToLocalMapping, "Application No")]
+      }&tenantId=${rowData["tenantId"]}`;
+    case get(textToLocalMapping, "APPROVED"):
+      return `/mihy-ui-framework/tradelicense-citizen/search-preview?status=approved&role=approver&applicationNumber=${
+        rowData[get(textToLocalMapping, "Application No")]
+      }&tenantId=${rowData["tenantId"]}`;
+
+    case get(textToLocalMapping, "PAID"):
+      return `/mihy-ui-framework/tradelicense-citizen/search-preview?status=pending_approval&role=approver&applicationNumber=${
+        rowData[get(textToLocalMapping, "Application No")]
+      }&tenantId=${rowData["tenantId"]}`;
+    case get(textToLocalMapping, "CANCELLED"):
+      return `/mihy-ui-framework/tradelicense-citizen/search-preview?status=cancelled&role=approver&applicationNumber=${
+        rowData[get(textToLocalMapping, "Application No")]
+      }&tenantId=${rowData["tenantId"]}`;
+    case get(textToLocalMapping, "INITIATED"):
+      return `/mihy-ui-framework/tradelicense-citizen/apply?applicationNumber=${
+        rowData[get(textToLocalMapping, "Application No")]
+      }&tenantId=${rowData["tenantId"]}`;
+    case get(textToLocalMapping, "REJECTED"):
+      return `/mihy-ui-framework/tradelicense-citizen/search-preview?status=rejected&role=approver&applicationNumber=${
+        rowData[get(textToLocalMapping, "Application No")]
+      }&tenantId=${rowData["tenantId"]}`;
+    default:
+      return `/mihy-ui-framework/tradelicense-citizen/home`;
+  }
+};
