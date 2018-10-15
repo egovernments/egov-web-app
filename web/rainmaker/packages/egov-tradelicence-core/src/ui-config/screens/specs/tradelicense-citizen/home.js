65c6342c43d7bc5e97f70ad89fa80d05acd879a0
avijeet7 <avijeet7@gmail.com>
2018-10-15 22:43:34 +0530

    Citizen search/home page

--- web/rainmaker/packages/egov-tradelicence-core/src/ui-config/screens/specs/tradelicense-citizen/home.js
+++ web/rainmaker/packages/egov-tradelicence-core/src/ui-config/screens/specs/tradelicense-citizen/home.js
@@ -1,30 +1,91 @@
 import {
-  getLabel,
+  getBreak,
   getCommonCard,
-  getCommonGrayCard,
-  getCommonTitle,
-  getCommonSubHeader,
-  getCommonParagraph,
-  getTextField,
-  getDateField,
-  getSelectField,
   getCommonContainer,
-  getPattern,
-  getCommonHeader
+  getCommonHeader,
+  getCommonParagraph,
+  getLabel
 } from "mihy-ui-framework/ui-config/screens/specs/utils";
-import tradeLicenseSearchAndResult from "../tradelicence/search";
+
+import { searchResults } from "./citizenSearchResource/citizenSearchResults";
+import { fetchData } from "./citizenSearchResource/citizenFunctions";
 
 const header = getCommonHeader({
   labelName: "Trade License",
   labelKey: "TL_COMMON_TL"
 });
 
-const screenConfig = {
+const tradeLicenseSearchAndResult = {
+  uiFramework: "material-ui",
+  name: "search",
   beforeInitScreen: (action, state, dispatch) => {
-    // console.log(action);
+    fetchData(action, state, dispatch);
     return action;
   },
-  ...tradeLicenseSearchAndResult
+  components: {
+    div: {
+      uiFramework: "custom-atoms",
+      componentPath: "Div",
+      props: {
+        className: "common-div-css"
+      },
+      children: {
+        header: header,
+        applyCard: {
+          uiFramework: "custom-atoms",
+          componentPath: "Div",
+          children: {
+            card: getCommonCard({
+              applicationSuccessContainer: getCommonContainer({
+                body: {
+                  uiFramework: "custom-atoms",
+                  componentPath: "Div",
+                  children: {
+                    header: getCommonHeader({
+                      labelName: "Apply for New Trade License",
+                      labelKey: "TL_COMMON_APPL_NEW_LICe"
+                    }),
+                    paragraph: getCommonParagraph({
+                      labelName:
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard Lorem Ipsum has been the industry's standard."
+                    }),
+                    collectPaymentButton: {
+                      componentPath: "Button",
+                      props: {
+                        variant: "contained",
+                        color: "primary",
+                        style: {
+                          width: "200px",
+                          height: "48px",
+                          marginRight: "40px"
+                        }
+                      },
+                      children: {
+                        collectPaymentButtonLabel: getLabel({
+                          labelName: "APPLY",
+                          labelKey: "TL_APPLY"
+                        })
+                      },
+                      onClickDefination: {
+                        action: "page_change",
+                        path: "/mihy-ui-framework/tradelicense-citizen/apply"
+                      },
+                      roleDefination: {
+                        rolePath: "user-info.roles",
+                        roles: ["EMPLOYEE"]
+                      }
+                    }
+                  }
+                }
+              })
+            }),
+            break: getBreak(),
+            searchResults: searchResults
+          }
+        }
+      }
+    }
+  }
 };
 
-export default screenConfig;
+export default tradeLicenseSearchAndResult;
