64cc139befa0ea82685d260f60c52cc9ac98ebfe
Gyan <gyanprakash92@gmail.com>
2018-10-16 09:54:47 +0530

    fetch bill moved to utils

--- web/rainmaker/packages/egov-tradelicence-core/src/ui-config/screens/specs/tradelicence/pay.js
+++ web/rainmaker/packages/egov-tradelicence-core/src/ui-config/screens/specs/tradelicence/pay.js
@@ -20,7 +20,7 @@ import {
 import { getQueryArg } from "mihy-ui-framework/ui-utils/commons";
 import set from "lodash/set";
 import { getSearchResults } from "ui-utils/commons";
-import { createEstimateData } from "../utils";
+import { fetchBill } from "../utils";
 
 const header = getCommonContainer({
   header: getCommonHeader({
@@ -35,73 +35,6 @@ const header = getCommonContainer({
     }
   }
 });
-
-export const fetchBill = async (action, state, dispatch) => {
-  //get bill and populate estimate card
-  const payload = await createEstimateData(
-    [],
-    "LicensesTemp[0].estimateCardData",
-    dispatch,
-    window.location.href
-  );
-
-  //For Adhoc
-  // Search License
-  let queryObject = [
-    { key: "tenantId", value: getQueryArg(window.location.href, "tenantId") },
-    {
-      key: "applicationNumber",
-      value: getQueryArg(window.location.href, "applicationNumber")
-    }
-  ];
-  const LicensesPayload = await getSearchResults(queryObject);
-  console.log(LicensesPayload);
-  //set in redux to be used for adhoc
-  dispatch(prepareFinalObject("Licenses[0]", LicensesPayload.Licenses[0]));
-
-  //initiate receipt object
-  payload &&
-    dispatch(prepareFinalObject("ReceiptTemp[0].Bill[0]", payload.Bill[0]));
-
-  //set amount paid as total amount from bill
-  payload &&
-    dispatch(
-      prepareFinalObject(
-        "ReceiptTemp[0].Bill[0].billDetails[0].amountPaid",
-        payload.Bill[0].billDetails[0].totalAmount
-      )
-    );
-
-  //set total amount in instrument
-  payload &&
-    dispatch(
-      prepareFinalObject(
-        "ReceiptTemp[0].instrument.amount",
-        payload.Bill[0].billDetails[0].totalAmount
-      )
-    );
-
-  //Initially select instrument type as Cash
-  dispatch(
-    prepareFinalObject("ReceiptTemp[0].instrument.instrumentType.name", "Cash")
-  );
-
-  //set tenantId
-  dispatch(
-    prepareFinalObject(
-      "ReceiptTemp[0].tenantId",
-      getQueryArg(window.location.href, "tenantId")
-    )
-  );
-
-  //set tenantId in instrument
-  dispatch(
-    prepareFinalObject(
-      "ReceiptTemp[0].instrument.tenantId",
-      getQueryArg(window.location.href, "tenantId")
-    )
-  );
-};
 
 const screenConfig = {
   uiFramework: "material-ui",
