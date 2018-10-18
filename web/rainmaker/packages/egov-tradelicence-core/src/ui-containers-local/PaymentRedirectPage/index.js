17a22424923169b34a20f613e6c9abfc4da4857b
Gyan <gyanprakash92@gmail.com>
2018-10-18 14:41:30 +0530

    window.location.href

--- web/rainmaker/packages/egov-tradelicence-core/src/ui-containers-local/PaymentRedirectPage/index.js
+++ web/rainmaker/packages/egov-tradelicence-core/src/ui-containers-local/PaymentRedirectPage/index.js
@@ -19,16 +19,11 @@ class PaymentRedirect extends Component {
       let tenantId = get(pgUpdateResponse, "Transaction[0].tenantId");
       let txnAmount = get(pgUpdateResponse, "Transaction[0].txnAmount");
       if (get(pgUpdateResponse, "Transaction[0].txnStatus") === "FAILURE") {
-       
-        history.push(
-          `/mihy-ui-framework/tradelicence/acknowledgement?purpose=${"pay"}&status=${"failure"}&applicationNumber=${moduleId}&tenantId=${tenantId}`
-        );
+        window.location.href = `/mihy-ui-framework/tradelicence/acknowledgement?purpose=${"pay"}&status=${"failure"}&applicationNumber=${moduleId}&tenantId=${tenantId}`;
       } else {
         let transactionId = get(pgUpdateResponse, "Transaction[0].txnId");
 
-        history.push(
-          `/mihy-ui-framework/tradelicence/acknowledgement?purpose=${"pay"}&status=${"success"}&applicationNumber=${moduleId}&tenantId=${tenantId}&secondNumber=${transactionId}`
-        );
+        window.location.href = `/mihy-ui-framework/tradelicence/acknowledgement?purpose=${"pay"}&status=${"success"}&applicationNumber=${moduleId}&tenantId=${tenantId}&secondNumber=${transactionId}`;
       }
     } catch (e) {
       alert(e);
