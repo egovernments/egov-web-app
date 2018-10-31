a91084584d794a86c4890cffaa3773d6322d93a1
RamakoteswraRao K <ramakoteswararao.koti@walkingtree.tech>
2018-10-31 15:59:50 +0530

    production url change

--- web/rainmaker/packages/employee/src/modules/employee/Erp/EGF/index.js
+++ web/rainmaker/packages/employee/src/modules/employee/Erp/EGF/index.js
@@ -14,27 +14,33 @@ class EGFFinance extends Component{
 
     render(){ 
     
-            let auth_token = localStorage.getItem('token'),
-                menuUrl = this.props.location.pathname,
-                loc = window.location,
-                subdomainurl,
-                hostname = loc.hostname,
-                winheight = window.innerHeight-100.;
+            let auth_token = localStorage.getItem("token"),
+                  menuUrl = this.props.location.pathname,
+                  loc = window.location,
+                  subdomainurl,
+                  hostname = loc.hostname,
+                  winheight = window.innerHeight - 100,
+                  erp_url,
+                  tenantId = localStorage.getItem("tenant-id");
 
-            if(hostname.search('dev')!=-1){
-                subdomainurl = hostname.substring(hostname.search('dev'),hostname.length);
-            }else if(hostname.search('qa')!=-1){
-                subdomainurl = hostname.substring(hostname.search('qa'),hostname.length);
-            }else if(hostname.search('uat')!=-1){
-                // subdomainurl = hostname.substring(hostname.search('uat'),hostname.length);
+                if (hostname.search("dev") != -1) {
+                  subdomainurl = hostname.substring(hostname.search("dev"), hostname.length);
+                  erp_url = loc.protocol + "//" + localStorage.getItem("tenant-id").split(".")[1] + "-" + subdomainurl + menuUrl;
+                } else if (hostname.search("qa") != -1) {
+                  subdomainurl = hostname.substring(hostname.search("qa"), hostname.length);
+                  erp_url = loc.protocol + "//" + localStorage.getItem("tenant-id").split(".")[1] + "-" + subdomainurl + menuUrl;
+                } else if (hostname.search("uat") != -1) {
+                  // subdomainurl = hostname.substring(hostname.search('uat'),hostname.length);
                   subdomainurl = "uat.egovernments.org";
-            }else
-                subdomainurl = hostname;
+                  erp_url = loc.protocol + "//" + localStorage.getItem("tenant-id").split(".")[1] + "-" + subdomainurl + menuUrl;
+                } else {
+                  subdomainurl = hostname.substring(hostname.indexOf(".") + 1);
+                  erp_url = loc.protocol + "//" + localStorage.getItem("tenant-id").split(".")[1] + "." + subdomainurl + menuUrl;
+                }
 
-            let erp_url= loc.protocol+"//"+localStorage.getItem('tenant-id').split('.')[1]+"-"+subdomainurl+menuUrl;
             // let erp_url='http://jalandhar.test.egov.com:8080'+menuUrl;
             console.log("ERP URL : "+erp_url);
-            let tenantId = localStorage.getItem('tenant-id');
+            
 
             return (
                     <div>
