3b37bdb29ee455daeecb5fe1634743f9fb87534c
Shreya <shreya.b@egovernments.org>
2018-11-28 19:04:16 +0530

    Fixed Trello Common Bugs

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/Dashboard/component/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/Dashboard/component/index.js
@@ -9,6 +9,7 @@ const moduleCardButtons = {
 const moduleCardButtonsContainer = {
   padding: "0px",
   margin: "0px",
+  width: "50%",
 };
 const ModuleCard = ({ items, onButton1Click, onPGRClick, onButton2Click, history }) => {
   return (
@@ -22,97 +23,62 @@ const ModuleCard = ({ items, onButton1Click, onPGRClick, onButton2Click, history
                 className="clearfix landingPageCard"
                 style={item.borderLeftColor}
                 textChildren={
-                  <div className="clearfix">
-                    {item.moduleDescription && (
-                      <div className="col-sm-12" style={{ padding: "0", margin: "0", minHeight: "190px" }}>
-                        <div className="col-sm-2" style={{ padding: "0", margin: "0" }}>
-                          <Icon action={item.iconAction} name={item.iconName} style={item.iconStyle} />
-                        </div>
-                        <div className="col-sm-10">
-                          <Label className="moduleCardTitle" label={item.moduleTitle} />
-                          <Label className="moduleCardDescription" label={item.moduleDescription} />
-                        </div>
+                  <div className="clearfix" style={{ position: "relative", minHeight: item.moduleDescription ? "200px" : "inherit" }}>
+                    <div style={{ padding: "0", margin: "0", display: "flex" }}>
+                      <Icon action={item.iconAction} name={item.iconName} style={item.iconStyle} />
+                      <Label className="moduleCardTitle" label={item.moduleTitle} />
+                    </div>
+                    <div>
+                      <Label className="moduleCardDescription" label={item.moduleDescription} />
+                    </div>
+                    <div
+                      className={
+                        item.moduleDescription ? "col-sm-12 landing-page-button landing-page-button-container" : "col-sm-12 landing-page-button"
+                      }
+                    >
+                      <div className="col-sm-6" style={moduleCardButtonsContainer}>
+                        <Button
+                          onClick={() => {
+                            onButton1Click(item, history, onPGRClick);
+                          }}
+                          label={<Label label={item.button1} color="#fff" />}
+                          primary={true}
+                          style={{ align: "left" }}
+                          style={moduleCardButtons}
+                          buttonStyle={{ border: "1px solid #fe7a51" }}
+                          labelStyle={{
+                            padding: "0 12px 0 12px ",
+                            letterSpacing: "0.6px",
+                            display: "inline-block",
+                            height: "22px",
+                            lineHeight: "22px",
+                            fontSize: "14px",
+                          }}
+                        />
                       </div>
-                    )}
-                    {!item.moduleDescription && (
-                      <div className="col-sm-12" style={{ padding: "0", margin: "0" }}>
-                        <div className="col-sm-2" style={{ padding: "0", margin: "0" }}>
-                          <Icon action={item.iconAction} name={item.iconName} style={item.iconStyle} />
+                      {item.button2 && (
+                        <div className="col-sm-6" style={moduleCardButtonsContainer}>
+                          <Button
+                            onClick={() => {
+                              onButton2Click(item, history);
+                            }}
+                            label={<Label label={item.button2} color="#fe7a51" />}
+                            style={{ align: "right" }}
+                            style={moduleCardButtons}
+                            buttonStyle={{ border: "1px solid #fe7a51" }}
+                            className="moduleSecondButton"
+                            labelStyle={{
+                              padding: "0 12px 0 12px ",
+                              letterSpacing: "0.6px",
+                              display: "inline-block",
+                              height: "22px",
+                              lineHeight: "22px",
+                              fontSize: "14px",
+                            }}
+                          />
                         </div>
-                        <div className="col-sm-10">
-                          <Label className="moduleCardTitle" label={item.moduleTitle} />
-                          <div className="col-sm-6" style={moduleCardButtonsContainer}>
-                            <Button
-                              onClick={() => {
-                                onButton1Click(item, history, onPGRClick);
-                              }}
-                              label={<Label label={item.button1} color="#fff" />}
-                              primary={true}
-                              style={{ align: "left" }}
-                              style={moduleCardButtons}
-                              buttonStyle={{ border: "1px solid #fe7a51" }}
-                              labelStyle={{
-                                padding: "0 12px 0 12px ",
-                                letterSpacing: "0.6px",
-                                display: "inline-block",
-                                height: "22px",
-                                lineHeight: "22px",
-                                fontSize: "14px",
-                              }}
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    )}
-                    {item.moduleDescription && (
-                      <div className="col-sm-12" style={{ padding: "0", margin: "0" }}>
-                        <div className="col-sm-2">&nbsp;</div>
-                        <div className="col-sm-10" style={{ margin: "0" }}>
-                          <div className="col-sm-6" style={moduleCardButtonsContainer}>
-                            <Button
-                              onClick={() => {
-                                onButton1Click(item, history, onPGRClick);
-                              }}
-                              label={<Label label={item.button1} color="#fff" />}
-                              primary={true}
-                              style={{ align: "left" }}
-                              style={moduleCardButtons}
-                              buttonStyle={{ border: "1px solid #fe7a51" }}
-                              labelStyle={{
-                                padding: "0 12px 0 12px ",
-                                letterSpacing: "0.6px",
-                                display: "inline-block",
-                                height: "22px",
-                                lineHeight: "22px",
-                                fontSize: "14px",
-                              }}
-                            />
-                          </div>
-                          {item.button2 && (
-                            <div className="col-sm-6" style={moduleCardButtonsContainer}>
-                              <Button
-                                onClick={() => {
-                                  onButton2Click(item, history);
-                                }}
-                                label={<Label label={item.button2} color="#fe7a51" />}
-                                style={{ align: "right" }}
-                                style={moduleCardButtons}
-                                buttonStyle={{ border: "1px solid #fe7a51" }}
-                                className="moduleSecondButton"
-                                labelStyle={{
-                                  padding: "0 12px 0 12px ",
-                                  letterSpacing: "0.6px",
-                                  display: "inline-block",
-                                  height: "22px",
-                                  lineHeight: "22px",
-                                  fontSize: "14px",
-                                }}
-                              />
-                            </div>
-                          )}
-                        </div>
-                      </div>
-                    )}
+                      )}
+                    </div>
                   </div>
                 }
               />
