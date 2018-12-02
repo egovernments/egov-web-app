dad980fb8d451b7a7371fec07597e2ae3bdc28b2
Shreya <shreya.b@egovernments.org>
2018-12-02 23:39:42 +0530

    Fixed Trade Unit Delete card issue

--- web/rainmaker/packages/egov-tradelicence-core/src/ui-utils/commons.js
+++ web/rainmaker/packages/egov-tradelicence-core/src/ui-utils/commons.js
@@ -167,12 +167,16 @@ export const getBoundaryData = async (
   }
 };
 
-const getMultipleAccessories = licenses => {
-  let accessories = get(licenses, "tradeLicenseDetail.accessories");
-  let mergedAccessories =
-    accessories &&
-    accessories.reduce((result, item) => {
-      if (item && item !== null && item.hasOwnProperty("accessoryCategory")) {
+const getMultiUnits = multiUnits => {
+  let hasTradeType = false;
+  let hasAccessoryType = false;
+
+  let mergedUnits =
+    multiUnits &&
+    multiUnits.reduce((result, item) => {
+      hasTradeType = item.hasOwnProperty("tradeType");
+      hasAccessoryType = item.hasOwnProperty("accessoryCategory");
+      if (item && item !== null && (hasTradeType || hasAccessoryType)) {
         if (item.hasOwnProperty("id")) {
           if (item.hasOwnProperty("active") && item.active) {
             if (item.hasOwnProperty("isDeleted") && !item.isDeleted) {
@@ -191,8 +195,35 @@ const getMultipleAccessories = licenses => {
       return result;
     }, []);
 
-  return mergedAccessories;
+  return mergedUnits;
 };
+
+// const getMultipleAccessories = licenses => {
+//   let accessories = get(licenses, "tradeLicenseDetail.accessories");
+//   let mergedAccessories =
+//     accessories &&
+//     accessories.reduce((result, item) => {
+//       if (item && item !== null && item.hasOwnProperty("accessoryCategory")) {
+//         if (item.hasOwnProperty("id")) {
+//           if (item.hasOwnProperty("active") && item.active) {
+//             if (item.hasOwnProperty("isDeleted") && !item.isDeleted) {
+//               set(item, "active", false);
+//               result.push(item);
+//             } else {
+//               result.push(item);
+//             }
+//           }
+//         } else {
+//           if (!item.hasOwnProperty("isDeleted")) {
+//             result.push(item);
+//           }
+//         }
+//       }
+//       return result;
+//     }, []);
+
+//   return mergedAccessories;
+// };
 
 const getMultipleOwners = owners => {
   let mergedOwners =
@@ -264,11 +295,18 @@ export const applyTradeLicense = async (state, dispatch, activeIndex) => {
 
     if (queryObject[0].applicationNumber) {
       //call update
-
+      let tradeUnits = get(queryObject[0], "tradeLicenseDetail.tradeUnits");
+      let accessories = get(queryObject[0], "tradeLicenseDetail.accessories");
+      set(
+        queryObject[0],
+        "tradeLicenseDetail.tradeUnits",
+        getMultiUnits(tradeUnits)
+      );
       set(
         queryObject[0],
         "tradeLicenseDetail.accessories",
-        getMultipleAccessories(queryObject[0])
+        //getMultipleAccessories(queryObject[0]
+        getMultiUnits(accessories)
       );
       set(
         queryObject[0],
@@ -300,13 +338,18 @@ export const applyTradeLicense = async (state, dispatch, activeIndex) => {
       dispatch(prepareFinalObject("Licenses", response.Licenses));
     } else {
       let accessories = get(queryObject[0], "tradeLicenseDetail.accessories");
+      let tradeUnits = get(queryObject[0], "tradeLicenseDetail.tradeUnits");
       // let owners = get(queryObject[0], "tradeLicenseDetail.owners");
+      let mergedTradeUnits =
+        tradeUnits &&
+        tradeUnits.filter(item => !item.hasOwnProperty("isDeleted"));
       let mergedAccessories =
         accessories &&
         accessories.filter(item => !item.hasOwnProperty("isDeleted"));
       let mergedOwners =
         owners && owners.filter(item => !item.hasOwnProperty("isDeleted"));
 
+      set(queryObject[0], "tradeLicenseDetail.tradeUnits", mergedTradeUnits);
       set(queryObject[0], "tradeLicenseDetail.accessories", mergedAccessories);
       set(queryObject[0], "tradeLicenseDetail.owners", mergedOwners);
       set(queryObject[0], "action", "INITIATE");
