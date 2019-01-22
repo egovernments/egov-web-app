c1d0542c30a30c0fe87e8d129334bbca4300db92
muralimgwl <murali.m@goodworklabs.com>
2019-01-22 16:48:02 +0530

    updated packages

--- web/rainmaker/packages/egov-ui-framework/ui-redux/auth/actionTypes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var AUTHENTICATING = exports.AUTHENTICATING = "AUTHENTICATING";
-var AUTHENTICATED = exports.AUTHENTICATED = "AUTHENTICATED";
-var AUTHENTICATION_FAILED = exports.AUTHENTICATION_FAILED = "AUTHENTICATION_FAILED";
-var LOGOUT = exports.LOGOUT = "LOGOUT";
