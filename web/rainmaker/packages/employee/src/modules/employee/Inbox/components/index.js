017445d3623b38ebeb7f21c336a4b8d9a3c794b8
Shreya <shreya.b@egovernments.org>
2019-01-29 15:09:38 +0530

    Reverted Inbox Iframe url

--- web/rainmaker/packages/employee/src/modules/employee/Inbox/components/index.js
+++ web/rainmaker/packages/employee/src/modules/employee/Inbox/components/index.js
@@ -59,10 +59,10 @@ class InboxData extends React.Component {
       addWflowFileUrl(processInstances, prepareFinalObject);
     }
 
-    //let baseUrl = process.env.NODE_ENV === "development" ? "https://egov-micro-dev.egovernments.org" : window.origin;
-    let baseUrl = "http://localhost:3002/";
-    //let contextPath = status === "INITIATED" ? "/employee/integration/tradelicense/apply" : "/employee/integration/tradelicense/search-preview";
-    let contextPath = status === "INITIATED" ? "egov-ui-framework/tradelicence/apply" : "egov-ui-framework/tradelicence/search-preview";
+    let baseUrl = process.env.NODE_ENV === "development" ? "https://egov-micro-dev.egovernments.org" : window.origin;
+    //let baseUrl = "http://localhost:3002/";
+    let contextPath = status === "INITIATED" ? "/employee/integration/tradelicense/apply" : "/employee/integration/tradelicense/search-preview";
+    //let contextPath = status === "INITIATED" ? "egov-ui-framework/tradelicence/apply" : "egov-ui-framework/tradelicence/search-preview";
     let queryParams = `applicationNumber=${taskId}&tenantId=${tenantId}`;
     window.location.href = `${baseUrl}${contextPath}?${queryParams}`;
   };
