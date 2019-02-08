29a56a9f25427821cbc0f0eb3ab1065ca7106579
Gyan <gyanprakash92@gmail.com>
2019-02-08 12:31:36 +0530

    deleted rainmaker-v2 folder from repo

--- web/rainmaker-v2/packages/employee/src/modules/employee/PropertyTax/FormWizard/utils/enableDependentFields.js
+++
@@ -1,5 +0,0 @@
-import { setFieldProperty } from "egov-ui-kit/redux/form/actions"
-
-export const setDependentFields = (fields, dispatch, formKey, isEnabled) => fields.forEach((fieldName) => {
-  dispatch(setFieldProperty(formKey, fieldName, "hideField", isEnabled))
-})
