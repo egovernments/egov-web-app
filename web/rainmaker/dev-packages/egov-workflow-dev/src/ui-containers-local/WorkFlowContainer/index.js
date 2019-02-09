5c2bddd2331b7884ea92e869a4d5a4ddbe35775e
Shreya <shreya.b@egovernments.org>
2019-02-10 00:07:00 +0530

    Added Module filet in inbox

--- web/rainmaker/dev-packages/egov-workflow-dev/src/ui-containers-local/WorkFlowContainer/index.js
+++ web/rainmaker/dev-packages/egov-workflow-dev/src/ui-containers-local/WorkFlowContainer/index.js
@@ -111,7 +111,7 @@ class WorkFlowContainer extends React.Component {
   getRedirectUrl = (action, businessId) => {
     switch (action) {
       case "PAY":
-        return `employee/tradelicence/pay?applicationNumber=${businessId}&tenantId=${tenant}&businessService=TL`;
+        return `/employee/tradelicence/pay?applicationNumber=${businessId}&tenantId=${tenant}&businessService=TL`;
     }
   };
 
