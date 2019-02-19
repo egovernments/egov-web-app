2ff52bc7f9b34751057f145444b5da872c2c5b34
avijeet7 <avijeet7@gmail.com>
2019-02-19 13:29:56 +0530

    HRMS: Deactivate employee documents upload

--- web/rainmaker/dev-packages/egov-hrms-dev/src/ui-config/screens/specs/hrms/viewResource/deactivate-employee.js
+++ web/rainmaker/dev-packages/egov-hrms-dev/src/ui-config/screens/specs/hrms/viewResource/deactivate-employee.js
@@ -1,14 +1,16 @@
 import {
-  getCommonHeader,
-  getTextField,
-  getSelectField,
   getCommonContainer,
+  getCommonHeader,
+  getCommonParagraph,
+  getCommonSubHeader,
+  getDateField,
   getLabel,
   getPattern,
-  getDateField
+  getSelectField,
+  getTextField
 } from "egov-ui-framework/ui-config/screens/specs/utils";
-import { createUpdateEmployee } from "./functions";
 import { showHideAdhocPopup } from "../../utils";
+import { createUpdateEmployee } from "./functions";
 
 const deactivateEmployeeCallback = (state, dispatch) => {
   createUpdateEmployee(state, dispatch, "DEACTIVATE");
@@ -150,7 +152,13 @@ export const deactivateEmployee = getCommonContainer({
           xs: 12,
           sm: 12
         }
-      }),
+      })
+    }
+  },
+  nonMandatoryBody: {
+    uiFramework: "custom-atoms",
+    componentPath: "Container",
+    children: {
       orderNo: getTextField({
         label: {
           labelName: "Order No.",
@@ -171,6 +179,56 @@ export const deactivateEmployee = getCommonContainer({
           sm: 12
         }
       }),
+      upload: {
+        uiFramework: "custom-atoms",
+        componentPath: "Container",
+        props: {
+          style: {
+            marginLeft: 8
+          }
+        },
+        required: false,
+        children: {
+          uploadFileHeader: getCommonSubHeader(
+            {
+              labelName: "Supporting Documents",
+              labelKey: "HR_APPROVAL_UPLOAD_HEAD"
+            },
+            {
+              style: { marginTop: 15, width: "100%" }
+            }
+          ),
+          uploadFileInfo: getCommonParagraph(
+            {
+              labelName: "Only .jpg and .pdf files. 5MB max file size."
+            },
+            {
+              style: {
+                fontSize: 12,
+                marginBottom: 0,
+                marginTop: 5,
+                width: "100%",
+                color: "rgba(0, 0, 0, 0.6000000238418579)"
+              }
+            }
+          ),
+          uploadButton: {
+            uiFramework: "custom-molecules",
+            componentPath: "UploadMultipleFiles",
+            props: {
+              maxFiles: 4,
+              jsonPath: "deactivationDocuments",
+              inputProps: {
+                accept: "image/*, .pdf, .png, .jpeg"
+              },
+              buttonLabel: { labelName: "UPLOAD FILES" },
+              maxFileSize: 5000,
+              moduleName: "HR",
+              hasLocalization: false
+            }
+          }
+        }
+      },
       remarks: getTextField({
         label: {
           labelName: "Remarks",
