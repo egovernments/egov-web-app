5227a76541fd65a87326eac193bd678615397395
muralimgwl <murali.m@goodworklabs.com>
2019-03-01 20:17:23 +0530

    Report framework enhancement - Hide/Show Columns

--- web/rainmaker/packages/employee/src/modules/employee/reports/reportResult.js
+++ web/rainmaker/packages/employee/src/modules/employee/reports/reportResult.js
@@ -21,6 +21,7 @@ import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 import "datatables.net-buttons/js/buttons.html5.js"; // HTML 5 file export
 import "datatables.net-buttons/js/buttons.flash.js"; // Flash file export
+import "datatables.net-buttons/js/buttons.colVis.min.js"
 import { getResultUrl } from "./commons/url";
 import Label from "egov-ui-kit/utils/translationNode";
 import commonConfig from "config/common.js";
@@ -158,6 +159,7 @@ class ShowField extends Component {
         className: "report-excel-button",
         // exportOptions,
       },
+      'colvis'
     ];
     return buttons;
   };
@@ -177,7 +179,7 @@ class ShowField extends Component {
     rTable = $("#reportTable").DataTable({
       // dom: "<'&nbsp''row'<'col-sm-3'l><'col-sm-5'f><'col-sm-4'B>><'row'<'col-sm-12'tr>><'&nbsp''row'<'col-sm-5'i><'col-sm-7'p>>",
       // dom: "<'&nbsp''row'<'report-filter'f><'report-buttons'B>><'row'<'col-sm-12'tr>><'&nbsp''row'<'col-sm-5'i><'col-sm-7'p>>",
-      dom:"<'&nbsp''row'<'col-sm-3 col-xs-6 text-left'l><'col-sm-5 col-xs-6 text-right'f><'col-sm-4 col-xs-12 text-center'B>><'row'<'col-sm-12't>><'&nbsp''row'<'col-sm-5 col-xs-12'i><'col-xs-12'p>>",
+      dom:"<'&nbsp''row report_filters'<'col-sm-2 col-xs-6 text-left'l><'col-sm-4 col-xs-6 text-right'f><'col-sm-6 col-xs-12 text-right'B>><'row'<'col-sm-12't>><'&nbsp''row'<'col-sm-5 col-xs-12'i><'col-xs-12'p>>",
       order: [],
       // responsive: true,
       select: true,
