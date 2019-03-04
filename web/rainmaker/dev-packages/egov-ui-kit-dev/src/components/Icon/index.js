a4ca34e185e6e9414f02ac1fe84abf67a29a4b22
Shreya <shreya.b@egovernments.org>
2019-03-04 19:39:58 +0530

    Fixed Missig icon Page break

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/components/Icon/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/components/Icon/index.js
@@ -2,18 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Icon = ({ action, className, name, style = {}, color, onClick, id }) => {
-  let error = "";
+  let WrappedIcon = null;
   try {
-    let WrappedIcon = null;
-
     if (action === "custom") {
       WrappedIcon = require(`egov-ui-kit/custom-icons/${name}`).default;
     } else {
       WrappedIcon = require(`material-ui/svg-icons/${action}/${name}`).default;
     }
     return <WrappedIcon id={id} className={className} style={{ ...style }} color={color} onClick={onClick} />;
-  } catch (error) {}
-  throw new Error(`Icon with action ${action} and name ${name} not found`);
+  } catch (error) {
+    console.log(`Icon with action ${action} and name ${name} not found`);
+    const DefaultIcon = require(`material-ui/svg-icons/alert/error`).default;
+    return <DefaultIcon id={id} className={className} style={{ ...style }} color="red" fill="red" onClick={onClick} />;
+  }
+  // throw new Error(`Icon with action ${action} and name ${name} not found`);
 };
 
 Icon.propTypes = {
