c031d7d944653a6181b7793977cfa7a06d3d8746
Shreya <shreya.b@egovernments.org>
2019-03-05 19:07:30 +0530

    Modified code for Inbox Module card display

--- web/rainmaker/packages/employee/src/modules/employee/Inbox/index.js
+++ web/rainmaker/packages/employee/src/modules/employee/Inbox/index.js
@@ -2,40 +2,11 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import TableData from "./components/TableData";
 import Label from "egov-ui-kit/utils/translationNode";
-import Icon from "egov-ui-kit/components/Icon";
 import { Boxboard } from "./components/actionItems";
-import howtovoteIcon from "@material-ui/icons/HowToVote";
 
 const iconStyle = {
   width: "48px",
   height: "46.02px",
-};
-
-const boxes = {
-  Complaints: {
-    head: <Icon action="action" name="announcement" style={iconStyle} />,
-    route: process.env.NODE_ENV === "production" ? "/employee/all-complaints" : "/all-complaints",
-  },
-  "Property Tax": {
-    head: <Icon action="action" name="store" style={iconStyle} />,
-    route: process.env.NODE_ENV === "production" ? "/employee/property-tax" : "/property-tax",
-  },
-  TradeLicense: {
-    head: <Icon action="places" name="business-center" style={iconStyle} />,
-    route: process.env.NODE_ENV === "production" ? "/employee/tradelicence/search" : "/tradelicence/search",
-  },
-  "Trade License": {
-    head: <Icon action="places" name="business-center" style={iconStyle} />,
-    route: process.env.NODE_ENV === "production" ? "/employee/tradelicence/search" : "/tradelicence/search",
-  },
-  Finance: {
-    head: <Icon action="editor" name="insert-chart" style={iconStyle} />,
-    route: process.env.NODE_ENV === "production" ? "/employee/services/EGF/inbox" : "/services/EGF/inbox",
-  },
-  Collections: {
-    head: <Icon action="image" name="collections" style={iconStyle} />,
-    route: process.env.NODE_ENV === "production" ? "" : "",
-  },
 };
 
 class Inbox extends Component {
@@ -55,22 +26,10 @@ class Inbox extends Component {
         hasWorkflow: false,
       });
     }
-    const list = menu && menu.filter((item) => item.url === "url");
-    const finalList =
-      list &&
-      list.reduce((result, item) => {
-        const body = item.path.split(".")[0];
-        if (!result.some((obj) => obj.body === body) && boxes.hasOwnProperty(body)) {
-          result.push({
-            head: boxes[body].head,
-            body: body,
-            route: boxes[body].route,
-          });
-        }
-        return result;
-      }, []);
+
+    const list = menu && menu.filter((item) => item.url === "card");
     this.setState({
-      actionList: finalList,
+      actionList: list,
     });
   }
   render() {
