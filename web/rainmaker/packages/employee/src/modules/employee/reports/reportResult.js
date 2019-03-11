6ddb06f83f6375272c0ab10041142639be0800fb
sushant gwl <sushant.bhaskar@goodworklabs.com>
2019-03-11 15:41:33 +0530

    Fixed table alignment and extra row bug

--- web/rainmaker/packages/employee/src/modules/employee/reports/reportResult.js
+++ web/rainmaker/packages/employee/src/modules/employee/reports/reportResult.js
@@ -21,7 +21,7 @@ import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 import "datatables.net-buttons/js/buttons.html5.js"; // HTML 5 file export
 import "datatables.net-buttons/js/buttons.flash.js"; // Flash file export
-import "datatables.net-buttons/js/buttons.colVis.min.js"
+import "datatables.net-buttons/js/buttons.colVis.min.js";
 import { getResultUrl } from "./commons/url";
 import Label from "egov-ui-kit/utils/translationNode";
 import commonConfig from "config/common.js";
@@ -148,7 +148,7 @@ class ShowField extends Component {
         className: "report-excel-button",
         // exportOptions,
       },
-      'colvis'
+      "colvis",
     ];
     return buttons;
   };
@@ -168,7 +168,8 @@ class ShowField extends Component {
     rTable = $("#reportTable").DataTable({
       // dom: "<'&nbsp''row'<'col-sm-3'l><'col-sm-5'f><'col-sm-4'B>><'row'<'col-sm-12'tr>><'&nbsp''row'<'col-sm-5'i><'col-sm-7'p>>",
       // dom: "<'&nbsp''row'<'report-filter'f><'report-buttons'B>><'row'<'col-sm-12'tr>><'&nbsp''row'<'col-sm-5'i><'col-sm-7'p>>",
-      dom:"<'&nbsp''row'<'col-sm-2 col-xs-6 text-left'l><'col-sm-4 col-xs-6 text-right'f><'col-sm-6 col-xs-12 text-right'B>><'row'<'col-sm-12't>><'&nbsp''row'<'col-sm-5 col-xs-12'i><'col-xs-12'p>>",
+      dom:
+        "<'&nbsp''row'<'col-sm-2 col-xs-6 text-left'l><'col-sm-4 col-xs-6 text-right'f><'col-sm-6 col-xs-12 text-right'B>><'row margin0'<'col-sm-12't>><'&nbsp''row'<'col-sm-5 col-xs-12'i><'col-xs-12'p>>",
       order: [],
       // responsive: true,
       select: true,
@@ -247,10 +248,12 @@ class ShowField extends Component {
       // "sDom": "<'&nbsp''row'<'H'CTrf>t<'F'lip<'row'<'col-sm-12'tr>><'&nbsp''row'<'col-sm-5'i><'col-sm-7'p>>>",
       aLengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
       sScrollX: "100%",
+      scrollX: true,
       // "sScrollXInner": "110%",
       bScrollCollapse: true,
       fnInitComplete: function() {
         this.css("visibility", "visible");
+
         $(".dataTables_scrollBody thead tr").css({ visibility: "collapse" });
       },
       renderer: "bootstrap",
@@ -394,7 +397,7 @@ class ShowField extends Component {
         reportResult.reportHeader[i] &&
         reportResult.reportHeader[i].type == "currency"
       ) {
-        return this.addCommas(val % 1 === 0 ? val : Number(val).toFixed(2));
+        return this.addCommas(Number(val) % 1 === 0 ? val : Number(val).toFixed(2));
       } else {
         return val;
       }
@@ -434,7 +437,8 @@ class ShowField extends Component {
       <thead>
         <tr className="report-table-header">
           <th key={"S. No."} className="report-header-cell">
-            {"S. No."}
+            {/* <Label className="report-header-row-label" labelStyle={{ wordWrap: "unset", wordBreak: "unset", fontWeight: "bold" }} label={"S. No"} /> */}
+            S. No
           </th>
           {metaData && metaData.reportDetails && metaData.reportDetails.selectiveDownload && (
             <th key={"testKey"}>
@@ -448,7 +452,7 @@ class ShowField extends Component {
                   <th key={i} className="report-header-cell">
                     <Label
                       className="report-header-row-label"
-                      labelStyle={{ width: "60%", wordWrap: "unset", wordBreak: "unset", fontWeight: "bold" }}
+                      labelStyle={{ wordWrap: "unset", wordBreak: "unset", fontWeight: "bold" }}
                       label={item.label}
                     />
                   </th>
@@ -458,7 +462,7 @@ class ShowField extends Component {
                   <th style={{ display: "none" }} key={i}>
                     <Label
                       className="report-header-row-label"
-                      labelStyle={{ width: "60%", wordWrap: "unset", wordBreak: "unset", fontWeight: "bold" }}
+                      labelStyle={{ wordWrap: "unset", wordBreak: "unset", fontWeight: "bold" }}
                       label={item.label}
                     />
                   </th>
@@ -703,7 +707,7 @@ class ShowField extends Component {
                 <th style={index !== 0 ? { textAlign: "right" } : {}} key={index}>
                   {index === 0
                     ? "Total"
-                    : this.addCommas(Number(total[index - 1] % 0 === 0 ? total[index - 1] : Number(total[index - 1]).toFixed(2)))}
+                    : this.addCommas(Number(total[index - 1]) % 1 === 0 ? total[index - 1] : Number(total[index - 1]).toFixed(2))}
                 </th>
               );
             })}
@@ -752,10 +756,6 @@ class ShowField extends Component {
 
       return (
         <div>
-          {/* <Card> */}
-          {/* <CardHeader title={self.state.reportSubTitle} /> */}
-          {/* <CardText> */}
-
           <table
             id="reportTable"
             style={{
