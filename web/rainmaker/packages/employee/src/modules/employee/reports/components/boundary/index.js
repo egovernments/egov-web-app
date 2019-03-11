1ee613f7a886e2c938c5801f259305fd41780ac2
Gyan <gyanprakash92@gmail.com>
2019-03-11 12:25:33 +0530

    boundary code cleaned up and made search fields reset

--- web/rainmaker/packages/employee/src/modules/employee/reports/components/boundary/index.js
+++ web/rainmaker/packages/employee/src/modules/employee/reports/components/boundary/index.js
@@ -1,16 +1,10 @@
 import { httpRequest } from "egov-ui-kit/utils/api";
+import { connect } from "react-redux";
 import Label from "egov-ui-kit/utils/translationNode";
 import jp from "jsonpath";
-// import UiSelectField from './UiSelectField';
-// import { translate, validate_fileupload } from '../../common/common';
-// import RaisedButton from 'material-ui/RaisedButton';
-// import { DropDown} from "components";
-import { AutoComplete } from "material-ui";
 import React, { Component } from "react";
 import { Col } from "react-bootstrap";
-import { connect } from "react-redux";
-import { withRouter } from "react-router";
-// import AutoComplete from "material-ui/AutoComplete";
+import AutoComplete from "material-ui/AutoComplete";
 
 class UiBoundary extends Component {
   constructor(props) {
@@ -22,56 +16,13 @@ class UiBoundary extends Component {
       labelArr: [],
       viewLabels: [],
       localityArray: [],
-      levelSearchString:{
-
-      }
+      levelSearchString: {},
     };
   }
 
   componentDidMount() {
     this.fetchLocations(this.props.item);
   }
-
-  // componentWillReceiveProps(nextProps) {
-  //   if (_.get(nextProps.formData, nextProps.item.jsonPath) === "" || _.get(nextProps.formData, nextProps.item.jsonPath) === undefined) {
-  //     this.setState({
-  //       dropDownDataVal: {},
-  //     });
-  //   }
-  //   if (window.location.hash.split("/")[1] != "create" || nextProps.item.setResponseData == true) {
-  //     if (_.get(this.props.formData, this.props.item.jsonPath) != _.get(nextProps.formData, nextProps.item.jsonPath)) {
-  //       this.fetchLocations(nextProps.item);
-  //     }
-  //   }
-  // }
-
-  // renderView = (labelData) => {
-  //   return (
-  //     <div>
-  //       <Row>
-  //         {!_.isEmpty(labelData)
-  //           ? Object.keys(labelData).map((key, index) => {
-  //               return (
-  //                 <Col
-  //                   key={index}
-  //                   id={key + index}
-  //                   // style={}
-  //                   xs={3}
-  //                   md={3}
-  //                 >
-  //                   <label>
-  //                     {" "}
-  //                     <span style={{ fontSize: "13px", fontWeight: 600 }}> {key} </span>
-  //                     <br /> {labelData[key]}
-  //                   </label>
-  //                 </Col>
-  //               );
-  //             })
-  //           : ""}
-  //       </Row>
-  //     </div>
-  //   );
-  // };
 
   initDropdownValues = (boundaryData, bdryCode) => {
     var ddArr = [];
@@ -126,33 +77,21 @@ class UiBoundary extends Component {
   };
 
   fetchLocations = async (item) => {
-    let { resTransfer } = this;
-    let self = this;
     const boundaryData = await this.fetchBoundaryData(item);
-    var jpath = "";
     let cityBdry = jp.query(boundaryData, `$.TenantBoundary[?(@.hierarchyType.name=="${item.hierarchyType}")].boundary[?(@.label=='City')]`);
-    var labelArr = self.fetchLabels(cityBdry[0]);
-    self.setState({
+    var labelArr = this.fetchLabels(cityBdry[0]);
+    let localityArray = jp.query(cityBdry, `$..children[?(@.label=="Locality")].code`);
+    this.setState({
       boundaryData: cityBdry,
       labelArr: labelArr,
+      localityArray: localityArray,
     });
 
-    self.setFirstDropDownData(cityBdry);
+    this.setFirstDropDownData(cityBdry);
 
     /**Code to set the localities for the entire tenant (first load)*/
-    this.state.localityArray = jp.query(cityBdry, `$..children[?(@.label=="Locality")].code`);
-    let e = { target: { value: this.state.localityArray } };
-    this.props.handleFieldChange(e, "localityArray", true, "");
+    this.props.handleFieldChange({ target: { value: localityArray } }, "localityArray", true, "");
     /**End of locality array code*/
-
-    // THIS IS FOR THE UPDATE PART
-    // if (window.location.hash.split("/")[1] != "create" || item.setResponseData) {
-    //   if (!_.isEmpty(self.props.formData)) {
-    //     if (typeof _.get(self.props.formData, self.props.item.jsonPath) != "undefined") {
-    //       self.initDropdownValues(cityBdry, _.get(self.props.formData, self.props.item.jsonPath));
-    //     }
-    //   }
-    // }
   };
 
   getDepth = (obj) => {
@@ -195,66 +134,33 @@ class UiBoundary extends Component {
       function onlyUnique(value, index, self) {
         return self.indexOf(value) === index;
       }
-
       labelArr = labelArr.filter(onlyUnique);
     }
     return labelArr;
   };
-  handler = (key, property) => {
-    let { dropDownDataVal, dropDownData,labelArr } = this.state;
 
-    let newDropDownDataVal = { dropDownDataVal: {} };
-    for (let i = 0; i < this.state.labelArr.length; i++) {
-      if (this.state.labelArr[i] == property) {
-        newDropDownDataVal.dropDownDataVal[property] = key;
+  handler = (key, property, text) => {
+    let { dropDownDataVal, dropDownData, labelArr } = this.state;
+    const { boundaryFieldsText, handleFieldChange } = this.props;
+    let newDropDownDataVal = {};
+
+    handleFieldChange({ target: { value: { ...boundaryFieldsText, [property]: text } } }, "boundaryFieldsText", true, "");
+    for (let i = 0; i < labelArr.length; i++) {
+      if (labelArr[i] == property) {
+        newDropDownDataVal[property] = key;
         break;
       } else {
-        newDropDownDataVal.dropDownDataVal[this.state.labelArr[i]] = dropDownDataVal[this.state.labelArr[i]];
+        newDropDownDataVal[labelArr[i]] = dropDownDataVal[labelArr[i]];
       }
     }
-    this.setState(newDropDownDataVal);
-
+    this.setState({ dropDownDataVal: newDropDownDataVal });
     //below runs for create & update only
     this.populateNextDropDown(key, property);
 
-    // if (labelArr.length>0) {
-    //   var arrayToDotTransformation=labelArr.join('.');
-    //   var splittedArray=arrayToDotTransformation.split(`${property}.`);
-    //   if (splittedArray[1]) {
-    //     var restDropDowns=splittedArray[1].split(".");
-    //     for (var i = 0; i < restDropDowns.length; i++) {
-    //       var documentObject=document.getElementById(`boundary-${property}`);
-    //       documentObject.value=null;
-    //     }
-    //   }
-    // }
-
-    // if (property == this.state.labelArr[this.state.labelArr.length - 1]) {
-    //   this.props.handler(
-    //     {
-    //       target: {
-    //         value: key,
-    //       },
-    //     },
-    //     this.props.item.jsonPath,
-    //     this.props.item.isRequired ? true : false,
-    //     "",
-    //     this.props.item.requiredErrMsg,
-    //     this.props.item.patternErrMsg
-    //   );
-    // }
-
-    /** Old search parameters */
-    // let e = { target: { value: key } };
-    // this.props.handleFieldChange(e, property, true, "");
-    /** END Old search params */
-
     /** Add locality array to search parameters */
-    this.props.handleFieldChange({ target: { value: this.state.localityArray } }, "localityArray", true, "");
-    // this.props.handleFieldChange({ target: { value: dropDownData[property].find(o => o.key == key).value } }, property, true, "");
-    this.props.handleFieldChange({ target: { value: newDropDownDataVal.dropDownDataVal } }, "ZonalSelection", true, "");
+    handleFieldChange({ target: { value: this.state.localityArray } }, "localityArray", true, "");
+    handleFieldChange({ target: { value: newDropDownDataVal } }, "ZonalSelection", true, "");
     /** END Add local... */
-
   };
 
   setFirstDropDownData = (cityBdry) => {
@@ -282,6 +188,7 @@ class UiBoundary extends Component {
     if (index > -1) {
       var objArr,
         ddData = [];
+      let localityArray = [];
       var str = "";
       for (var i = 0; i < index; i++) {
         str = str + ".*.children";
@@ -291,12 +198,15 @@ class UiBoundary extends Component {
 
       /**Code to set the locality array locality wise reports data*/
       if (this.state.labelArr.length - index == 2) {
-        this.state.localityArray = jp.query(objArr, `$..code`);
+        localityArray = jp.query(objArr, `$..code`);
       } else if (this.state.labelArr.length - index == 1) {
-        this.state.localityArray = [key];
+        localityArray = [key];
       } else {
-        this.state.localityArray = jp.query(objArr, `$..children[?(@.label=="Locality")].code`);
+        localityArray = jp.query(objArr, `$..children[?(@.label=="Locality")].code`);
       }
+      this.setState({
+        localityArray: localityArray,
+      });
       /**End of locality array code*/
 
       if (objArr.length > 0) {
@@ -319,13 +229,14 @@ class UiBoundary extends Component {
   };
 
   renderFields = (level) => {
-    // const dataSourceConfig = { text: "label", value: "value" };
-    let { dropDownData, dropDownDataVal, levelSearchString={} } = this.state;
-    let data = dropDownData[level]
-      ? dropDownData[level].map((dd, index) => {
-          return { value: dd.key, text: dd.value };
-        })
-      : [];
+    let { dropDownData, dropDownDataVal, levelSearchString = {} } = this.state;
+    const { boundaryFieldsText, handleFieldChange } = this.props;
+    let data =
+      dropDownData && dropDownData[level]
+        ? dropDownData[level].map((dd, index) => {
+            return { value: dd.key, text: dd.value };
+          })
+        : [];
     const dataSourceConfig = { text: "text", value: "value" };
     return (
       <div>
@@ -333,7 +244,7 @@ class UiBoundary extends Component {
           id={`boundary-${level}`}
           floatingLabelText={
             <div className="rainmaker-displayInline">
-              <Label className="show-field-label" label={level} containerStyle={{ marginRight: "5px"}}  style={{fontSize:"16px !important"}}/>
+              <Label className="show-field-label" label={level} containerStyle={{ marginRight: "5px" }} style={{ fontSize: "16px !important" }} />
             </div>
           }
           floatingLabelFixed={true}
@@ -342,12 +253,12 @@ class UiBoundary extends Component {
           filter={(searchText, key) => {
             return key.toLowerCase().indexOf(searchText.toLowerCase()) !== -1;
           }}
-          // searchText={levelSearchString[level]}
+          searchText={(boundaryFieldsText && boundaryFieldsText[level]) || ""}
           onNewRequest={(data, index) => {
-            this.handler(data.value, level);
+            this.handler(data.value, level, data.text);
           }}
-          onUpdateInput={(searchText,dataSource,params)=>{
-            this.handler(searchText, level);
+          onUpdateInput={(searchText, dataSource, params) => {
+            handleFieldChange({ target: { value: { ...boundaryFieldsText, [level]: searchText } } }, "boundaryFieldsText", true, "");
           }}
           dataSource={data}
           dataSourceConfig={dataSourceConfig}
@@ -358,105 +269,23 @@ class UiBoundary extends Component {
     );
   };
 
-  renderBoundary = (item) => {
-    if (window.location.hash.split("/")[1] != "view") {
-      // switch (this.props.ui) {
-      //   case "google":
-      return this.state.labelArr.map((v, i) => {
-        return (
-          <Col xs={12} sm={4} md={4} lg={4} key={i}>
-            {this.renderFields(v)}
-          </Col>
-        );
-      });
-      // (
-      //   <div>
-      //     {/*<Row>*/}
-      //       {this.state.labelArr.map((v, i) => {
-      //         return (
-      //           <Col xs={12} sm={4} md={3} lg={3} key={i}>
-      //             {this.renderFields(v)}
-      //           </Col>
-      //         );
-      //       })}
-      //     {/*</Row>*/}
-      //     <br />
-      //   </div>
-      // );
-      // }
-    } else {
-    }
-  };
-
   render() {
-    return this.renderBoundary(this.props.item);
-    // (
-    //   {/*<div style={this.props.item.style && this.props.item.style}>*/}
-    //     {/* // The commented part is for view purpose (Populate, upadte, view) */}
-    //     {/* {this.props.match.url.split("/")[1] == "view" && typeof _.get(this.props.formData, this.props.item.jsonPath) != "undefined"
-    //       ? this.renderView(this.state.viewLabels)
-    //       : this.renderBoundary(this.props.item)} */}
-    //     {this.renderBoundary(this.props.item)}
-    //     {/* {this.props.item.type == 'boundary' ? null : this.props.callbackFromCollectionRoute(this.state.dropDownDataVal, this.state.labelArr)} */}
-    //   {/*</div>*/}
-    // );
+    return this.state.labelArr.map((v, i) => {
+      return (
+        <Col xs={12} sm={4} md={4} lg={4} key={i}>
+          {this.renderFields(v)}
+        </Col>
+      );
+    });
   }
 }
+const mapStateToProps = (state) => {
+  const { form } = state.formtemp || {};
+  const { boundaryFieldsText } = (form && form) || {};
+  return { boundaryFieldsText };
+};
 
-const mapStateToProps = (state) => ({
-  formData: state.formtemp.form,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  setFormData: (data) => {
-    dispatch({ type: "SET_FORM_DATA", data });
-  },
-  toggleSnackbarAndSetText: (snackbarState, toastMsg, isSuccess, isError) => {
-    dispatch({
-      type: "TOGGLE_SNACKBAR_AND_SET_TEXT",
-      snackbarState,
-      toastMsg,
-      isSuccess,
-      isError,
-    });
-  },
-  setLoadingStatus: (loadingStatus) => {
-    dispatch({ type: "SET_LOADING_STATUS", loadingStatus });
-  },
-});
-
-export default withRouter(
-  connect(
-    null,
-    null
-  )(UiBoundary)
-);
-
-// <AutoComplete
-//   // className="custom-form-control-for-textfield"
-//   // floatingLabelStyle={{ fontSize: "20px"}}
-//   fullWidth={true}
-//   // style={{ display: "inline-block" }}
-//   filter={(searchText, key) => {
-//     return key.toLowerCase().indexOf(searchText.toLowerCase()) !== -1;
-//   }}
-//   // listStyle={{ maxHeight: 100, overflow: "auto" }}
-//   onNewRequest={(value) => {
-//     this.props.handler(value,level);
-//   }}
-//   onUpdateInput={(searchText,dataSource,params)=>{
-//      this.handler(searchText, level);
-//   }}
-//   dataSource={dropDownData[level]}
-//   dataSourceConfig={dataSourceConfig}
-//   openOnFocus={true}
-//   {...labelProperty}
-//   disabled={this.props.item.isDisabled}
-//     maxHeight={200}
-//     style={{ display: "inline-block" }}
-//     errorStyle={{ float: "left" }}
-//     id={this.props.item.jsonPath.split(".").join("-") + "-" + level}
-//     dropDownMenuProps={{
-//       targetOrigin: { horizontal: "left", vertical: "bottom" },
-//     }}
-// />
+export default connect(
+  mapStateToProps,
+  null
+)(UiBoundary);
