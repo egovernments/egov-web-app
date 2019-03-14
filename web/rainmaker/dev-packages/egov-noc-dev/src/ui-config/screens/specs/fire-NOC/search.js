25cd9b0cd410eaaa893fcc18a5f9729ed2c9e13a
sharath-golluri97 <sharathgolluri@gmail.com>
2019-03-14 15:57:54 +0530

    Added NOC search screen

---
+++ web/rainmaker/dev-packages/egov-noc-dev/src/ui-config/screens/specs/fire-NOC/search.js
@@ -0,0 +1,104 @@
+import {
+  getCommonHeader,
+  getLabel,
+  getBreak
+} from "egov-ui-framework/ui-config/screens/specs/utils";
+import { NOCApplication } from "./searchResource/fireNOCApplication";
+
+import { getQueryArg } from "egov-ui-framework/ui-utils/commons";
+import { pendingApprovals } from "./searchResource/pendingApprovals";
+// import { progressStatus } from "./searchResource/progressStatus";
+import { searchResults } from "./searchResource/searchResults";
+
+const hasButton = getQueryArg(window.location.href, "hasButton");
+//const hasApproval = getQueryArg(window.location.href, "hasApproval");
+let enableButton = true;
+//enableInbox = hasApproval && hasApproval === "false" ? false : true;
+enableButton = hasButton && hasButton === "false" ? false : true;
+
+const header = getCommonHeader({
+  labelName: "Fire NOC",
+  labelKey: "NOC_COMMON_NOC"
+});
+const NOCSearchAndResult = {
+  uiFramework: "material-ui",
+  name: "search",
+  components: {
+    div: {
+      uiFramework: "custom-atoms",
+      componentPath: "Form",
+      props: {
+        className: "common-div-css",
+        id: "search"
+      },
+      children: {
+        headerDiv: {
+          uiFramework: "custom-atoms",
+          componentPath: "Container",
+
+          children: {
+            header: {
+              gridDefination: {
+                xs: 12,
+                sm: 6
+              },
+              ...header
+            },
+            newApplicationButton: {
+              componentPath: "Button",
+              gridDefination: {
+                xs: 12,
+                sm: 6,
+                align: "right"
+              },
+              visible: enableButton,
+              props: {
+                variant: "contained",
+                color: "primary",
+                style: {
+                  color: "white",
+                  borderRadius: "2px",
+                  width: "250px",
+                  height: "48px"
+                }
+              },
+
+              children: {
+                plusIconInsideButton: {
+                  uiFramework: "custom-atoms",
+                  componentPath: "Icon",
+                  props: {
+                    iconName: "add",
+                    style: {
+                      fontSize: "24px"
+                    }
+                  }
+                },
+
+                buttonLabel: getLabel({
+                  labelName: "NEW APPLICATION",
+                  labelKey: "NOC_HOME_SEARCH_RESULTS_NEW_APP_BUTTON"
+                })
+              },
+              onClickDefination: {
+                action: "page_change",
+                path: "/fire-noc/apply"
+              },
+              roleDefination: {
+                rolePath: "user-info.roles",
+                roles: ["TL_CEMP"]
+              }
+            }
+          }
+        },
+        pendingApprovals,
+        NOCApplication,
+        breakAfterSearch: getBreak(),
+        // progressStatus,
+        searchResults
+      }
+    }
+  }
+};
+
+export default NOCSearchAndResult;
---
+++ web/rainmaker/dev-packages/egov-noc-dev/src/ui-config/screens/specs/utils/index.js
@@ -0,0 +1,111 @@
+import { validate } from "egov-ui-framework/ui-redux/screen-configuration/utils";
+import get from "lodash/get";
+
+export const transformById = (payload, id) => {
+  return (
+    payload &&
+    payload.reduce((result, item) => {
+      result[item[id]] = {
+        ...item
+      };
+
+      return result;
+    }, {})
+  );
+};
+
+export const getTranslatedLabel = (labelKey, localizationLabels) => {
+  let translatedLabel = null;
+  if (localizationLabels && localizationLabels.hasOwnProperty(labelKey)) {
+    translatedLabel = localizationLabels[labelKey];
+    if (
+      translatedLabel &&
+      typeof translatedLabel === "object" &&
+      translatedLabel.hasOwnProperty("message")
+    )
+      translatedLabel = translatedLabel.message;
+  }
+  return translatedLabel || labelKey;
+};
+
+export const validateFields = (
+  objectJsonPath,
+  state,
+  dispatch,
+  screen = "apply"
+) => {
+  const fields = get(
+    state.screenConfiguration.screenConfig[screen],
+    objectJsonPath,
+    {}
+  );
+  let isFormValid = true;
+  for (var variable in fields) {
+    if (fields.hasOwnProperty(variable)) {
+      if (
+        fields[variable] &&
+        fields[variable].props &&
+        (fields[variable].props.disabled === undefined ||
+          !fields[variable].props.disabled) &&
+        !validate(
+          screen,
+          {
+            ...fields[variable],
+            value: get(
+              state.screenConfiguration.preparedFinalObject,
+              fields[variable].jsonPath
+            )
+          },
+          dispatch,
+          true
+        )
+      ) {
+        isFormValid = false;
+      }
+    }
+  }
+  return isFormValid;
+};
+
+export const convertDateToEpoch = (dateString, dayStartOrEnd = "dayend") => {
+  //example input format : "2018-10-02"
+  try {
+    const parts = dateString.match(/(\d{4})-(\d{1,2})-(\d{1,2})/);
+    const DateObj = new Date(Date.UTC(parts[1], parts[2] - 1, parts[3]));
+    DateObj.setMinutes(DateObj.getMinutes() + DateObj.getTimezoneOffset());
+    if (dayStartOrEnd === "dayend") {
+      DateObj.setHours(DateObj.getHours() + 24);
+      DateObj.setSeconds(DateObj.getSeconds() - 1);
+    }
+    return DateObj.getTime();
+  } catch (e) {
+    return dateString;
+  }
+};
+
+export const getEpochForDate = date => {
+  const dateSplit = date.split("/");
+  return new Date(dateSplit[2], dateSplit[1] - 1, dateSplit[0]).getTime();
+};
+
+export const sortByEpoch = (data, order) => {
+  if (order) {
+    return data.sort((a, b) => {
+      return a[a.length - 1] - b[b.length - 1];
+    });
+  } else {
+    return data.sort((a, b) => {
+      return b[b.length - 1] - a[a.length - 1];
+    });
+  }
+};
+
+export const convertEpochToDate = dateEpoch => {
+  const dateFromApi = new Date(dateEpoch);
+  let month = dateFromApi.getMonth() + 1;
+  let day = dateFromApi.getDate();
+  let year = dateFromApi.getFullYear();
+  month = (month > 9 ? "" : "0") + month;
+  day = (day > 9 ? "" : "0") + day;
+  return `${day}/${month}/${year}`;
+};
