b6065a77ebbc5fed5786f4e7ffa3a2e4d3791175
avijeet7 <avijeet7@gmail.com>
2019-04-09 17:30:10 +0530

    TL: minor css changes

--- web/rainmaker/dev-packages/egov-workflow-dev/src/ui-molecules-local/ActionDialog/index.js
+++ web/rainmaker/dev-packages/egov-workflow-dev/src/ui-molecules-local/ActionDialog/index.js
@@ -11,6 +11,7 @@ import CloseIcon from "@material-ui/icons/Close";
 import { withStyles } from "@material-ui/core/styles";
 import { UploadMultipleFiles } from "egov-ui-framework/ui-molecules";
 import { toggleSnackbar } from "egov-ui-framework/ui-redux/screen-configuration/actions";
+import "./index.css";
 
 const styles = theme => ({
   root: {
@@ -101,13 +102,22 @@ class ActionDialog extends React.Component {
       isDocRequired
     } = dialogData;
     const { getButtonLabelName } = this;
+    let fullscreen = false;
+    if (window.innerWidth <= 768) {
+      fullscreen = true;
+    }
     return (
-      <Dialog open={open} onClose={onClose} maxWidth="lg">
+      <Dialog
+        fullScreen={fullscreen}
+        open={open}
+        onClose={onClose}
+        maxWidth={false}
+      >
         <DialogContent
           children={
             <Container
               children={
-                <Grid container="true" sm="12" marginTop={16}>
+                <Grid container="true" spacing={12} marginTop={16} className="action-container">
                   <Grid
                     style={{
                       alignItems: "center",
@@ -123,7 +133,13 @@ class ActionDialog extends React.Component {
                   <Grid
                     item
                     sm={2}
-                    style={{ textAlign: "right", cursor: "pointer" }}
+                    style={{
+                      textAlign: "right",
+                      cursor: "pointer",
+                      position: "absolute",
+                      right: "16px",
+                      top: "16px"
+                    }}
                     onClick={onClose}
                   >
                     <CloseIcon />
