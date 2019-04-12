fd41fd03c726a80e33c23349dc7dfa945d8bf667
Shreya <shreya.b@egovernments.org>
2019-04-12 18:04:31 +0530

    TL search Headers Localised

--- web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-config/screens/specs/tradelicence/searchResource/searchResults.js
+++ web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-config/screens/specs/tradelicence/searchResource/searchResults.js
@@ -2,77 +2,74 @@ import React from "react";
 import { Link } from "react-router-dom";
 import get from "lodash/get";
 import { sortByEpoch, getEpochForDate } from "../../utils";
+import { getLocaleLabels } from "egov-ui-framework/ui-utils/commons";
 import {
   getLocalization,
   getLocale
 } from "egov-ui-kit/utils/localStorageUtils";
 
-const getLocalTextFromCode = localCode => {
-  return JSON.parse(getLocalization(`localization_${getLocale()}`)).find(
-    item => item.code == localCode
-  );
-};
+const localeLabels = JSON.parse(getLocalization(`localization_${getLocale()}`));
 
 export const textToLocalMapping = {
-  "Application No": get(
-    getLocalTextFromCode("TL_COMMON_TABLE_COL_APP_NO"),
-    "message"
-    // "Application No"
+  "Application No": getLocaleLabels(
+    "Application No",
+    "TL_COMMON_TABLE_COL_APP_NO",
+    localeLabels
   ),
-  "License No": get(
-    getLocalTextFromCode("TL_COMMON_TABLE_COL_LIC_NO"),
-    "message"
-    // "License No"
+
+  "License No": getLocaleLabels(
+    "License No",
+    "TL_COMMON_TABLE_COL_LIC_NO",
+    localeLabels
   ),
-  "Trade Name": get(
-    getLocalTextFromCode("TL_COMMON_TABLE_COL_TRD_NAME"),
-    "message"
-    // "Trade Name"
+
+  "Trade Name": getLocaleLabels(
+    "Trade Name",
+    "TL_COMMON_TABLE_COL_TRD_NAME",
+    localeLabels
   ),
-  "Owner Name": get(
-    getLocalTextFromCode("TL_COMMON_TABLE_COL_OWN_NAME"),
-    "message"
-    // "Owner Name"
+  "Owner Name": getLocaleLabels(
+    "Owner Name",
+    "TL_COMMON_TABLE_COL_OWN_NAME",
+    localeLabels
   ),
-  "Application Date": get(
-    getLocalTextFromCode("TL_COMMON_TABLE_COL_APP_DATE"),
-    "message"
-    // "Application Date"
+
+  "Application Date": getLocaleLabels(
+    "Application Date",
+    "TL_COMMON_TABLE_COL_APP_DATE",
+    localeLabels
   ),
-  Status: get(
-    getLocalTextFromCode("TL_COMMON_TABLE_COL_STATUS"),
-    "message"
-    // "Status"
+
+  Status: getLocaleLabels("Status", "TL_COMMON_TABLE_COL_STATUS", localeLabels),
+
+  INITIATED: getLocaleLabels("Initiated,", "TL_INITIATED", localeLabels),
+  APPLIED: getLocaleLabels("Applied", "TL_APPLIED", localeLabels),
+  PAID: getLocaleLabels("Paid", "WF_NEWTL_PENDINGAPPROVAL", localeLabels),
+
+  APPROVED: getLocaleLabels("Approved", "TL_APPROVED", localeLabels),
+  REJECTED: getLocaleLabels("Rejected", "TL_REJECTED", localeLabels),
+  CANCELLED: getLocaleLabels("Cancelled", "TL_CANCELLED", localeLabels),
+  PENDINGAPPROVAL: getLocaleLabels(
+    "Pending for Approval",
+    "WF_NEWTL_PENDINGAPPROVAL",
+    localeLabels
   ),
-  INITIATED: get(getLocalTextFromCode("TL_INITIATED"), "message"),
-  APPLIED: get(getLocalTextFromCode("TL_APPLIED"), "message"),
-  PAID: get(
-    getLocalTextFromCode("WF_NEWTL_PENDINGAPPROVAL"),
-    "message",
-    "PAID"
+  PENDINGPAYMENT: getLocaleLabels(
+    "Pending payment",
+    "WF_NEWTL_PENDINGPAYMENT",
+    localeLabels
   ),
-  APPROVED: get(getLocalTextFromCode("TL_APPROVED"), "message"),
-  REJECTED: get(getLocalTextFromCode("TL_REJECTED"), "message"),
-  CANCELLED: get(getLocalTextFromCode("TL_CANCELLED"), "message"),
-  PENDINGAPPROVAL: get(
-    getLocalTextFromCode("WF_NEWTL_PENDINGAPPROVAL"),
-    "message"
-    // "Pending for Approval"
+
+  FIELDINSPECTION: getLocaleLabels(
+    "Pending for Field Inspection",
+    "WF_NEWTL_FIELDINSPECTION",
+    localeLabels
   ),
-  PENDINGPAYMENT: get(
-    getLocalTextFromCode("WF_NEWTL_PENDINGPAYMENT"),
-    "message"
-    // "Pending payment"
-  ),
-  FIELDINSPECTION: get(
-    getLocalTextFromCode("WF_NEWTL_FIELDINSPECTION"),
-    "message"
-    // "Pending for Field Inspection"
-  ),
-  "Search Results for Trade License Applications": get(
-    getLocalTextFromCode("TL_HOME_SEARCH_RESULTS_TABLE_HEADING"),
-    "message"
-    // "Search Results for Trade License Applications"
+
+  "Search Results for Trade License Applications": getLocaleLabels(
+    "",
+    "TL_HOME_SEARCH_RESULTS_TABLE_HEADING",
+    localeLabels
   )
 };
 
