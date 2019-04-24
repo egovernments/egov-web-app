3e216b9f75244e10660d48d1c61911e582f08d33
Shreya <shreya.b@egovernments.org>
2019-04-24 13:07:37 +0530

    Fixed Citizen TL financial year issue

--- web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-config/screens/specs/utils/index.js
+++ web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-config/screens/specs/utils/index.js
@@ -1162,7 +1162,7 @@ export const getCurrentFinancialYear = () => {
   var today = new Date();
   var curMonth = today.getMonth();
   var fiscalYr = "";
-  if (curMonth > 3) {
+  if (curMonth >= 3) {
     var nextYr1 = (today.getFullYear() + 1).toString();
     fiscalYr = today.getFullYear().toString() + "-" + nextYr1;
   } else {
