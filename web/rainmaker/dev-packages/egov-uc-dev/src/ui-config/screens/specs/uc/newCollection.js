ef1afc8c8209224578849272b2c7eb5ff76a24a8
sonalikasolanki <sonalika.n@egovernments.org>
2019-05-03 17:52:42 +0530

    new collection

---
+++ web/rainmaker/dev-packages/egov-uc-dev/src/ui-config/screens/specs/uc/newCollection.js
@@ -0,0 +1,46 @@
+import {
+    getCommonHeader,
+    getBreak
+} from "egov-ui-framework/ui-config/screens/specs/utils";
+import { newCollectionDetailsCard } from "./newCollectionResource/newCollectionDetails";
+import { newCollectionFooter } from "./newCollectionResource/newCollectionFooter"
+
+const header = getCommonHeader({
+    labelName: "New Collection",
+    labelKey: "UC_COMMON_HEADER"
+});
+
+const newCollection = {
+    uiFramework: "material-ui",
+    name: "newCollection",
+    components: {
+        div: {
+            uiFramework: "custom-atoms",
+            componentPath: "Form",
+            props: {
+                className: "common-div-css",
+                id: "newCollection"
+            },
+            children: {
+                headerDiv: {
+                    uiFramework: "custom-atoms",
+                    componentPath: "Container",
+
+                    children: {
+                        header: {
+                            gridDefination: {
+                                xs: 12,
+                                sm: 6
+                            },
+                            ...header
+                        }
+                    }
+                },
+                newCollectionDetailsCard,
+                newCollectionFooter,
+            }
+        },
+    }
+};
+
+export default newCollection;
---
+++ web/rainmaker/dev-packages/egov-uc-dev/src/ui-config/screens/specs/uc/newCollectionResource/newCollectionFooter.js
@@ -0,0 +1,44 @@
+import { getLabel } from "egov-ui-framework/ui-config/screens/specs/utils";
+
+const getCommonApplyFooter = children => {
+  return {
+    uiFramework: "custom-atoms",
+    componentPath: "Div",
+    props: {
+      className: "apply-wizard-footer"
+    },
+    children
+  };
+};
+
+export const newCollectionFooter = getCommonApplyFooter({
+  nextButton: {
+    componentPath: "Button",
+    props: {
+      variant: "contained",
+      color: "primary",
+      style: {
+        minWidth: "200px",
+        height: "48px",
+        marginRight: "16px"
+      }
+    },
+    children: {
+      downloadReceiptButtonLabel: getLabel({
+        labelName: "NEXT",
+        labelKey: "UC_BUTTON_NEXT"
+      }),
+      nextButtonIcon: {
+        uiFramework: "custom-atoms",
+        componentPath: "Icon",
+        props: {
+          iconName: "keyboard_arrow_right"
+        }
+      }
+    }
+    // onClickDefination: {
+    //     action: "page_change",
+    //     path: redirectionURL
+    // }
+  }
+});
