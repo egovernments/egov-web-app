880c21c40106ee2441aea0cc9dd601c17501180e
avijeet7 <avijeet7@gmail.com>
2019-05-22 02:01:31 +0530

    NOC: Documents upload WIP, contract and UI done

--- web/rainmaker/dev-packages/egov-noc-dev/src/ui-molecules-local/DocumentList/index.js
+++ web/rainmaker/dev-packages/egov-noc-dev/src/ui-molecules-local/DocumentList/index.js
@@ -17,11 +17,27 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { UploadSingleFile } from "../../ui-molecules-local";
 
-const styles = theme => ({
+const themeStyles = theme => ({
   documentContainer: {
     backgroundColor: "#F2F2F2",
     padding: "16px",
+    marginTop: "10px",
     marginBottom: "16px"
+  },
+  documentCard: {
+    backgroundColor: "#F2F2F2",
+    padding: "16px",
+    marginTop: "10px",
+    marginBottom: "16px"
+  },
+  documentSubCard: {
+    backgroundColor: "#F2F2F2",
+    padding: "16px",
+    marginTop: "10px",
+    marginBottom: "10px",
+    border: "#d6d6d6",
+    borderStyle: "solid",
+    borderWidth: "1px"
   },
   documentIcon: {
     backgroundColor: "#FFFFFF",
@@ -73,13 +89,24 @@ const styles = theme => ({
   }
 });
 
-const documentTitle = {
-  color: "rgba(0, 0, 0, 0.87)",
-  fontFamily: "Roboto",
-  fontSize: "16px",
-  fontWeight: 400,
-  letterSpacing: "0.67px",
-  lineHeight: "19px"
+const styles = {
+  documentTitle: {
+    color: "rgba(0, 0, 0, 0.87)",
+    fontFamily: "Roboto",
+    fontSize: "16px",
+    fontWeight: 500,
+    letterSpacing: "0.67px",
+    lineHeight: "19px",
+    paddingBottom: "5px"
+  },
+  documentName: {
+    color: "rgba(0, 0, 0, 0.87)",
+    fontFamily: "Roboto",
+    fontSize: "16px",
+    fontWeight: 400,
+    letterSpacing: "0.67px",
+    lineHeight: "19px"
+  }
 };
 
 const requiredIcon = (
@@ -190,101 +217,121 @@ class DocumentList extends Component {
     );
   };
 
+  getUploadCard = (card, key) => {
+    const { classes } = this.props;
+    const { uploadedIndex } = this.state;
+    return (
+      <Grid container={true}>
+        <Grid item={true} xs={2} sm={1} className={classes.iconDiv}>
+          {uploadedIndex.indexOf(key) > -1 ? (
+            <div className={classes.documentSuccess}>
+              <Icon>
+                <i class="material-icons">done</i>
+              </Icon>
+            </div>
+          ) : (
+            <div className={classes.documentIcon}>
+              <span>{key + 1}</span>
+            </div>
+          )}
+        </Grid>
+        <Grid
+          item={true}
+          xs={10}
+          sm={5}
+          md={4}
+          align="left"
+          className={classes.descriptionDiv}
+        >
+          <LabelContainer labelKey={card.name} style={styles.documentName} />
+          {card.required && requiredIcon}
+          {/* <Typography variant="caption">{document.statement}</Typography>
+          <Typography variant="caption">{description}</Typography> */}
+        </Grid>
+        <Grid item={true} xs={12} sm={6} md={4}>
+          {/* {document.selector && (
+            <FormControl required className={classes.formControl}>
+              <InputLabel shrink htmlFor="age-label-placeholder">
+                {document.selector.inputLabel}
+              </InputLabel>
+              <Select
+                value={
+                  documents[key].selector.value ||
+                  documents[key].selector.initialValue
+                }
+                onChange={event => this.handleChange(key, event)}
+                name="selected-document"
+              >
+                {document.selector.menuItems &&
+                  document.selector.menuItems.map(item => {
+                    return <MenuItem value={item.code}>{item.code}</MenuItem>;
+                  })}
+              </Select>
+            </FormControl>
+          )} */}
+        </Grid>
+        <Grid
+          item={true}
+          xs={12}
+          sm={12}
+          md={3}
+          className={classes.fileUploadDiv}
+        >
+          <UploadSingleFile
+            classes={this.props.classes}
+            handleFileUpload={e =>
+              handleFileUpload(e, this.handleDocument, this.props)
+            }
+            uploaded={uploadedIndex.indexOf(key) > -1}
+            removeDocument={() => this.removeDocument(key)}
+            documents={this.state.uploadedDocuments[key]}
+            onButtonClick={() => this.onUploadClick(key)}
+            inputProps={this.props.inputProps}
+            buttonLabel={this.props.buttonLabel}
+          />
+        </Grid>
+      </Grid>
+    );
+  };
+
   render() {
     const { classes, documents, documentsList, description } = this.props;
     // console.log("_____", this.props);
-    const { uploadedIndex } = this.state;
-    const finalDocuments = documentsList;
+    // const finalDocuments = documentsList;
+    let index = 0;
     return (
-      <div style={{ paddingTop: 10 }}>
-        {finalDocuments &&
-          finalDocuments.map((document, key) => {
+      <div>
+        {documentsList &&
+          documentsList.map(container => {
             return (
-              <div
-                key={key}
-                id={`document-upload-${key}`}
-                className={classes.documentContainer}
-              >
-                <Grid container={true}>
-                  <Grid item={true} xs={2} sm={1} className={classes.iconDiv}>
-                    {uploadedIndex.indexOf(key) > -1 ? (
-                      <div className={classes.documentSuccess}>
-                        <Icon>
-                          <i class="material-icons">done</i>
-                        </Icon>
-                      </div>
-                    ) : (
-                      <div className={classes.documentIcon}>
-                        <span>{key + 1}</span>
-                      </div>
-                    )}
-                  </Grid>
-                  <Grid
-                    item={true}
-                    xs={10}
-                    sm={5}
-                    md={4}
-                    align="left"
-                    className={classes.descriptionDiv}
-                  >
-                    <LabelContainer
-                      labelName={document.name}
-                      labelKey={document.name}
-                      style={documentTitle}
-                    />
-                    {document.required && requiredIcon}
-                    <Typography variant="caption">
-                      {document.statement}
-                    </Typography>
-                    <Typography variant="caption">{description}</Typography>
-                  </Grid>
-                  <Grid item={true} xs={12} sm={6} md={4}>
-                    {document.selector && (
-                      <FormControl required className={classes.formControl}>
-                        <InputLabel shrink htmlFor="age-label-placeholder">
-                          {document.selector.inputLabel}
-                        </InputLabel>
-                        <Select
-                          value={
-                            documents[key].selector.value ||
-                            documents[key].selector.initialValue
-                          }
-                          onChange={event => this.handleChange(key, event)}
-                          name="selected-document"
-                        >
-                          {document.selector.menuItems &&
-                            document.selector.menuItems.map(item => {
-                              return (
-                                <MenuItem value={item.code}>
-                                  {item.code}
-                                </MenuItem>
-                              );
-                            })}
-                        </Select>
-                      </FormControl>
-                    )}
-                  </Grid>
-                  <Grid
-                    item={true}
-                    xs={12}
-                    sm={12}
-                    md={3}
-                    className={classes.fileUploadDiv}
-                  >
-                    <UploadSingleFile
-                      classes={this.props.classes}
-                      handleFileUpload={e =>
-                        handleFileUpload(e, this.handleDocument, this.props)
-                      }
-                      uploaded={uploadedIndex.indexOf(key) > -1}
-                      removeDocument={() => this.removeDocument(key)}
-                      documents={this.state.uploadedDocuments[key]}
-                      onButtonClick={() => this.onUploadClick(key)}
-                      inputProps={this.props.inputProps}
-                      buttonLabel={this.props.buttonLabel}
-                    />
-                  </Grid>
-                </Grid>
+              <div>
+                <LabelContainer
+                  labelKey={container.title}
+                  style={styles.documentTitle}
+                />
+                {container.cards.map(card => {
+                  return (
+                    <div className={classes.documentContainer}>
+                      {card.hasSubCards && (
+                        <LabelContainer
+                          labelKey={card.name}
+                          style={styles.documentTitle}
+                        />
+                      )}
+                      {card.hasSubCards &&
+                        card.subCards.map(subCard => {
+                          return (
+                            <div className={classes.documentSubCard}>
+                              {this.getUploadCard(subCard, index++)}
+                            </div>
+                          );
+                        })}
+                      {!card.hasSubCards && (
+                        <div>{this.getUploadCard(card, index++)}</div>
+                      )}
+                    </div>
+                  );
+                })}
               </div>
             );
           })}
@@ -319,7 +366,7 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default withStyles(styles)(
+export default withStyles(themeStyles)(
   connect(
     null,
     mapDispatchToProps
--- web/rainmaker/dev-packages/egov-noc-dev/src/ui-utils/commons.js
+++ web/rainmaker/dev-packages/egov-noc-dev/src/ui-utils/commons.js
@@ -84,28 +84,106 @@ export const prepareDocumentsUploadData = (state, dispatch) => {
     return item.active;
   });
   let documentsContract = [];
-  documents.forEach(item => {
-    if (item.hasMultipleRows && item.options) {
-      item.options.forEach(option => {
-        let doc = {};
-        doc["name"] = `${item.code}_${option.code}`;
-        doc["required"] = option.required ? true : false;
-        doc["jsonPath"] = `FireNOCs[0].documents.${item.code}.${option.code}`;
-        documentsContract.push(doc);
+  let tempDoc = {};
+  documents.forEach(doc => {
+    let card = {};
+    card["code"] = doc.documentType;
+    card["title"] = doc.documentType;
+    card["cards"] = [];
+    tempDoc[doc.documentType] = card;
+  });
+
+  documents.forEach(doc => {
+    let card = {};
+    card["name"] = doc.code;
+
+    // Handle the case for multiple muildings
+    if (doc.code === "BUILDING_PLAN" && doc.hasMultipleRows && doc.options) {
+      card["hasSubCards"] = true;
+      card["subCards"] = [];
+      doc.options.forEach(subDoc => {
+        let subCard = {};
+        subCard["name"] = subDoc.code;
+        subCard["required"] = subDoc.required ? true : false;
+        card.subCards.push(subCard);
       });
     } else {
-      let doc = {};
-      doc["name"] = item.code;
-      doc["required"] = item.required ? true : false;
-      doc["jsonPath"] = `FireNOCs[0].documents.${item.code}`;
-      if (item.hasDropdown && item.dropdownData) {
-        doc["selector"] = {
-          inputLabel: "Select Document",
-          menuItems: get(item, "dropdownData", [])
-        };
-      }
-      documentsContract.push(doc);
+      card["required"] = doc.required ? true : false;
     }
+    tempDoc[doc.documentType].cards.push(card);
   });
+
+  Object.keys(tempDoc).forEach(key => {
+    documentsContract.push(tempDoc[key]);
+  });
+
+  console.log("+++++", documentsContract);
+  console.log("+++++", tempDoc);
+
+  // let documentsContract = [];
+  // let documentsCards = {};
+  // documents.forEach(item => {
+  //   // Initialize a map with keys as documentType (eg. Owner, Building)
+  //   documentsCards[item.documentType] = get(
+  //     documentsCards,
+  //     item.documentType,
+  //     []
+  //   );
+
+  //   // Handling multiple buildings by calculating cards required
+  //   if (item.hasMultipleRows && item.options && item.code === "BUILDING_PLAN") {
+  //     // Get the buildings added in property details step
+  //     let buildings = get(
+  //       state,
+  //       "screenConfiguration.preparedFinalObject.FireNOCs[0].fireNOCDetails.buildings",
+  //       [{ name: "ASd" }]
+  //     );
+  //     // For each building, be it 0 or >0, add the cards in contract
+  //     buildings.forEach((building, key) => {
+  //       item.options.forEach(option => {
+  //         let doc = {};
+  //         doc["name"] = `${item.code}_${option.code}`;
+  //         doc["required"] = option.required ? true : false;
+  //         doc["jsonPath"] = `uploadedDocuments.${item.documentType}[${key}].${
+  //           item.code
+  //         }.${option.code}`;
+  //         doc["buildingName"] = building.name;
+  //         documentsCards[item.documentType].push(doc);
+  //       });
+  //     });
+  //   }
+  //   // In case of multiple rows with options (Eg. Building plans, FireStations plans)
+  //   else if (item.hasMultipleRows && item.options) {
+  //     item.options.forEach(option => {
+  //       let doc = {};
+  //       doc["name"] = `${item.code}_${option.code}`;
+  //       doc["required"] = option.required ? true : false;
+  //       doc["jsonPath"] = `FireNOCs[0].documents.${item.code}.${option.code}`;
+  //       documentsCards[item.documentType].push(doc);
+  //     });
+  //   } else {
+  //     let doc = {};
+  //     doc["name"] = item.code;
+  //     doc["required"] = item.required ? true : false;
+  //     doc["jsonPath"] = `FireNOCs[0].documents.${item.code}`;
+  //     if (item.hasDropdown && item.dropdownData) {
+  //       doc["selector"] = {
+  //         inputLabel: "Select Document",
+  //         menuItems: get(item, "dropdownData", [])
+  //       };
+  //     }
+  //     // documentsContract.push(doc);
+  //     documentsCards[item.documentType].push(doc);
+  //   }
+  // });
+  // documentsContract = Object.keys(documentsCards).map(key => {
+  //   return {
+  //     name: `NOC_${key}_CARD_HEADER`,
+  //     code: key,
+  //     children: documentsCards[key]
+  //   };
+  // });
+  // console.log("+++++=========", documentsContract);
+  // console.log("+++++=========", documentsCards);
   dispatch(prepareFinalObject("documentsContract", documentsContract));
 };
