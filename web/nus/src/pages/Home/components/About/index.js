76d3cb6d2a849c70a9e90233e152b2fe6f6031d5
NandaniMalviya <nandani@egovernments.org>
2019-05-27 19:26:17 +0530

    menu and about and implementation1.0

--- web/nus/src/pages/Home/components/About/index.js
+++ web/nus/src/pages/Home/components/About/index.js
@@ -1,14 +1,133 @@
-import React from "react";
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { withStyles } from '@material-ui/core/styles';
+import CardMedia from '@material-ui/core/CardMedia';
+import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Star from '@material-ui/icons/Star';
+import AboutImg from '../../../../../src/img/about.jpg';
+import UpperImg from '../../../../../src/img/upper.jpg';
+const styles = {
+	card: {
+		maxWidth: 345
+	},
+	media: {
+		height: 150
+	},
+	displayInline: {
+		display: 'inline'
+	}
+};
 
 class About extends React.Component {
-  render()
-  {
-    return(
-      <div>
-        About
-      </div>
-    );
-  }
-}
+	render() {
+		const { classes } = this.props;
+		return (
+			<Grid container className="aboutSection">
+				<Grid item xs={6}>
+					<div className="leftSection">
+						<div className="imgWrapper">
+							{
+								<div className="innerImg">
+									<img src={AboutImg} />
+								</div>
+							}
+							{
+								<div className="upperImg">
+									<img src={UpperImg} />
+								</div>
+							}
+						</div>
+					</div>
+				</Grid>
+				<Grid item xs={6}>
+					<div className="rightSection">
+						<Card className={classes.card}>
+							<CardContent>
+								<Typography gutterBottom color="initial" variant="caption" align="left">
+									About
+								</Typography>
+								<Typography
+									className={classes.displayInline}
+									gutterBottom
+									component="h1"
+									color="primary"
+									variant="h5"
+									align="left"
+								>
+									|National Urban Stack
+								</Typography>
+								<Typography
+									gutterBottom
+									variant="body1"
+									color="initial"
+									display="inline"
+									component="h4"
+									align="left"
+								>
+									This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor
+									aliquet. Aenean sollicitudin
+								</Typography>
+								<Typography
+									gutterBottom
+									variant="caption"
+									color="initial"
+									//	display="inline"
+									component="h4"
+									align="left"
+									component="p"
+								>
+									This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor
+									aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum,
+									nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit
+									amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a
+									ornare odio.
+								</Typography>
+								<List component="nav">
+									<ListItem button>
+										<ListItemIcon>
+											<Star />
+										</ListItemIcon>
+										<ListItemText primary="Open, shared, extensible national digital infrastructure" />
+									</ListItem>
+									<ListItem button>
+										<ListItemIcon>
+											<Star />
+										</ListItemIcon>
+										<ListItemText primary="Acts as a Public Good with a federated architecture" />
+									</ListItem>
+									<ListItem button>
+										<ListItemIcon>
+											<Star />
+										</ListItemIcon>
+										<ListItemText primary="State Government(s) have strategic control" />
+									</ListItem>
+									<ListItem button>
+										<ListItemIcon>
+											<Star />
+										</ListItemIcon>
+										<ListItemText primary="Free of cost Technology Platform" />
+									</ListItem>
+									<ListItem button>
+										<ListItemIcon>
+											<Star />
+										</ListItemIcon>
+										<ListItemText primary="Provide locally relevant solutions for cities" />
+									</ListItem>
+								</List>
 
-export default About;
+								<List component="nav" />
+							</CardContent>
+						</Card>
+					</div>
+				</Grid>
+			</Grid>
+		);
+	}
+}
+export default withStyles(styles)(About);
--- web/nus/src/pages/Home/components/Menu/index.js
+++ web/nus/src/pages/Home/components/Menu/index.js
@@ -1,14 +1,71 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
 
-class Menu extends React.Component {
-  render()
-  {
-    return(
-      <div>
-        Menu
-      </div>
-    );
-  }
+function TabContainer(props) {
+	return (
+		<Typography component="div" style={{ padding: 8 * 3 }}>
+			{props.children}
+		</Typography>
+	);
 }
 
-export default Menu;
+TabContainer.propTypes = {
+	children: PropTypes.node.isRequired
+};
+
+const styles = (theme) => ({
+	root: {
+		flexGrow: 1,
+		backgroundColor: '#4A90E2',
+		color: 'white'
+	}
+});
+
+class Menu extends React.Component {
+	state = {
+		value: 0
+	};
+
+	handleChange = (event, value) => {
+		this.setState({ value });
+	};
+
+	render() {
+		const { classes, menuItems } = this.props;
+		const { value } = this.state;
+
+		return (
+			<div>
+				<AppBar className={classes.root} color="#4A90E2" Width="1280px" Height="56px" position="static">
+					<Tabs
+						value={value}
+						color="#FFFFFF"
+						indicatorColor="primary"
+						margin="11.64px"
+						font-size="14px"
+						Line
+						Spacing="17px"
+						onChange={this.handleChange}
+					>
+						{menuItems.map((item) => {
+							//console.log(item.label);
+
+							return <Tab label={item.label} />;
+						})}
+					</Tabs>
+				</AppBar>
+			</div>
+		);
+	}
+}
+
+Menu.propTypes = {
+	classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Menu);
