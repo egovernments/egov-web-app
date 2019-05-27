838f56706902b7be1ab51d679b599d090f4bb36c
sharath-golluri97 <sharathgolluri@gmail.com>
2019-05-27 10:24:53 +0530

    ABG: BillSearch static screen v1.0

---
+++ web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/abg/billSearch.js
@@ -0,0 +1,110 @@
+import {
+  getCommonHeader,
+  getLabel,
+  getBreak
+} from "egov-ui-framework/ui-config/screens/specs/utils";
+import { billSearchCard } from "./billSearchResources/billSearchCard";
+//   import { getQueryArg } from "egov-ui-framework/ui-utils/commons";
+import { searchResults } from "./billSearchResources/searchResults";
+import { prepareFinalObject } from "egov-ui-framework/ui-redux/screen-configuration/actions";
+import { httpRequest } from "../../../../ui-utils";
+import { getQueryArg } from "egov-ui-framework/ui-utils/commons";
+
+const header = getCommonHeader({
+  labelName: "Universal Bill",
+  labelKey: "ABG_UNIVERSAL_BILL_COMMON_HEADER"
+});
+
+const hasButton = getQueryArg(window.location.href, "hasButton");
+let enableButton = true;
+enableButton = hasButton && hasButton === "false" ? false : true;
+
+const getData = async (action, state, dispatch) => {
+  await getMDMSData(action, state, dispatch);
+};
+
+const getMDMSData = async (action, state, dispatch) => {
+  const tenantId = "pb.amritsar";
+  let mdmsBody = {
+    MdmsCriteria: {
+      tenantId: tenantId,
+      moduleDetails: [
+        {
+          moduleName: "BillingService",
+          masterDetails: [{ name: "BusinessService" }]
+        }
+      ]
+    }
+  };
+  try {
+    const payload = await httpRequest(
+      "post",
+      "/egov-mdms-service/v1/_search",
+      "_search",
+      [],
+      mdmsBody
+    );
+    dispatch(prepareFinalObject("searchScreenMdmsData", payload.MdmsRes));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const ucSearchAndResult = {
+  uiFramework: "material-ui",
+  name: "search",
+  beforeInitScreen: (action, state, dispatch) => {
+    getData(action, state, dispatch);
+    return action;
+  },
+  components: {
+    div: {
+      uiFramework: "custom-atoms",
+      componentPath: "Form",
+      props: {
+        className: "common-div-css",
+        id: "universalCollection"
+      },
+      children: {
+        headerDiv: {
+          uiFramework: "custom-atoms",
+          componentPath: "Container",
+
+          children: {
+            header: {
+              gridDefination: {
+                xs: 12,
+                sm: 6
+              },
+              ...header
+            },
+            newApplicationButton: {
+              componentPath: "Button",
+              gridDefination: {
+                xs: 12,
+                sm: 6,
+                align: "right"
+              },
+              visible: enableButton,
+              props: {
+                variant: "contained",
+                color: "primary",
+                style: {
+                  color: "white",
+                  borderRadius: "2px",
+                  width: "250px",
+                  height: "48px"
+                }
+              },
+            }
+          }
+        },
+        billSearchCard,
+        breakAfterSearch: getBreak(),
+        searchResults
+      }
+    }
+  }
+};
+
+export default ucSearchAndResult;
