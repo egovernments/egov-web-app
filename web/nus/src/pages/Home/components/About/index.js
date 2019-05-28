f6ff2abdad48e8f7810e7366e156e8ae4bd6adbb
NandaniMalviya <nandani@egovernments.org>
2019-05-28 19:29:29 +0530

    how and new gallery1.0

--- web/nus/src/pages/Home/components/About/index.js
+++ web/nus/src/pages/Home/components/About/index.js
@@ -3,7 +3,6 @@ import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { withStyles } from '@material-ui/core/styles';
-import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
