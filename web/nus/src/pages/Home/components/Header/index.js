b4004560fc71a9e7e527b6cb8826d25a00e0a86d
muralimgwl <murali.m@goodworklabs.com>
2019-05-31 13:43:35 +0530

    header

--- web/nus/src/pages/Home/components/Header/index.js
+++ web/nus/src/pages/Home/components/Header/index.js
@@ -2,33 +2,34 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
+import Grid from "@material-ui/core/Grid";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import logo1 from "../../../../assets/logo1.png";
 import urban from "../../../../assets/urban.png";
 import SearchIcon from "@material-ui/icons/Search";
 const styles = {
-	root: {
-		flexGrow: 1
-	},
-	grow: {
-		flexGrow: 1
-	},
-	app: {
-		paddingLeft: '20px',
-		paddingRight: '10px',
-		display: 'flex',
-		flexDirection: 'row'
-	},
-	appBarRoot: {
-		backgroundColor: '#FFFFFF',
-		color: 'black'
-	},
-	appBarRoot1: {
-		backgroundColor: '#F0F0F0',
-		color: 'black',
-		flexDirection: 'row'
-	}
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  app: {
+    paddingLeft: "20px",
+    paddingRight: "10px",
+    display: "flex",
+    flexDirection: "row"
+  },
+  appBarRoot: {
+    backgroundColor: "#FFFFFF",
+    color: "black"
+  },
+  appBarRoot1: {
+    backgroundColor: "#F0F0F0",
+    color: "black",
+    flexDirection: "row"
+  }
 };
 
 class Header extends React.Component {
@@ -37,7 +38,7 @@ class Header extends React.Component {
     return (
       <div className={classes.root}>
         <AppBar classes={{ root: classes.appBarRoot1 }} position="static">
-          <div
+      <div
             className="leftdiv"
             style={{
               display: "flex",
@@ -86,28 +87,37 @@ class Header extends React.Component {
             <Toolbar>
               <div className={classes.SearchIcon} style={{ display: "flex" }}>
                 <SearchIcon />
+                <div
+                  className={classes.app}
+                  style={{ minWidth: "120px", justifyContent: "space-between" }}
+                >
+                  <span>-A</span>
+                  <span>A</span>
 
-									<span> A</span>
+                  <span>+A</span>
 
-									<span>+A</span>
-
-									<span>हिंदी </span>
-								</div>
-							</div>
-						</Toolbar>
-					</div>
-				</AppBar>
-				<AppBar classes={{ root: classes.appBarRoot }} position="static">
-					<Toolbar>
-						<img alt="logo1" src={logo1} width="162px" height="74px" />
-						<Typography variant="h6" align="right" color="inherit" className={classes.grow}>
-							<img alt="urban" src={urban} width="164px" height="74px" />
-						</Typography>
-					</Toolbar>
-				</AppBar>
-			</div>
-		);
-	}
+                  <span>हिंदी </span>
+                </div>
+              </div>
+            </Toolbar>
+          </div>
+        </AppBar>
+        <AppBar classes={{ root: classes.appBarRoot }} position="static">
+          <Toolbar>
+            <img alt="logo1" src={logo1} width="162px" height="74px" />
+            <Typography
+              variant="h6"
+              align="right"
+              color="inherit"
+              className={classes.grow}
+            >
+              <img alt="urban" src={urban} width="164px" height="74px" />
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
 export default withStyles(styles)(Header);
