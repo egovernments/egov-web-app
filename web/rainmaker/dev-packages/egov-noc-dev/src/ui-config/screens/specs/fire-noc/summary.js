7d29629238652edebe009da6125fc591df124e40
avijeet7 <avijeet7@gmail.com>
2019-06-10 19:49:32 +0530

    NOC: summary page documents

--- web/rainmaker/dev-packages/egov-noc-dev/src/ui-config/screens/specs/fire-noc/summary.js
+++ web/rainmaker/dev-packages/egov-noc-dev/src/ui-config/screens/specs/fire-noc/summary.js
@@ -1,17 +1,14 @@
-import {
-  getCommonCard,
-  getCommonHeader,
-  getCommonContainer,
-  getCommonTitle
-} from "egov-ui-framework/ui-config/screens/specs/utils";
-import { estimateSummary } from "./summaryResource/estimateSummary";
-import { nocSummary } from "./summaryResource/nocSummary";
-import { propertySummary } from "./summaryResource/propertySummary";
+import { getCommonCard, getCommonContainer, getCommonHeader } from "egov-ui-framework/ui-config/screens/specs/utils";
+import { prepareFinalObject } from "egov-ui-framework/ui-redux/screen-configuration/actions";
+import { getFileUrlFromAPI, getQueryArg } from "egov-ui-framework/ui-utils/commons";
+import jp from "jsonpath";
+import get from "lodash/get";
 import { applicantSummary } from "./summaryResource/applicantSummary";
 import { documentsSummary } from "./summaryResource/documentsSummary";
+import { estimateSummary } from "./summaryResource/estimateSummary";
 import { footer } from "./summaryResource/footer";
-import { prepareFinalObject } from "egov-ui-framework/ui-redux/screen-configuration/actions";
-import { getQueryArg } from "egov-ui-framework/ui-utils/commons";
+import { nocSummary } from "./summaryResource/nocSummary";
+import { propertySummary } from "./summaryResource/propertySummary";
 
 const header = getCommonContainer({
   header: getCommonHeader({
@@ -20,15 +17,34 @@ const header = getCommonContainer({
   })
 });
 
+const prepareDocumentsView = async (state, dispatch) => {
+  let documentsPreview = [];
+  let reduxDocuments = get(state, "screenConfiguration.preparedFinalObject.documentsUploadRedux", {});
+  jp.query(reduxDocuments, "$.*").forEach(doc => {
+    if (doc.documents && doc.documents.length > 0) {
+      documentsPreview.push({
+        title: doc.documentCode,
+        name: doc.documents[0].fileName,
+        fileStoreId: doc.documents[0].fileStoreId,
+        linkText: "View"
+      });
+    }
+  });
+  let fileStoreIds = jp.query(documentsPreview, "$.*.fileStoreId");
+  let fileUrls = fileStoreIds.length > 0 ? await getFileUrlFromAPI(fileStoreIds) : [];
+  documentsPreview = documentsPreview.map(doc => {
+    doc["link"] = fileUrls[doc.fileStoreId];
+    return doc;
+  });
+  dispatch(prepareFinalObject("documentsPreview", documentsPreview));
+};
+
 const screenConfig = {
   uiFramework: "material-ui",
   name: "summary",
   beforeInitScreen: (action, state, dispatch) => {
-    const applicationNumber = getQueryArg(
-      window.location.href,
-      "applicationNumber"
-    );
-    // if (applicationNumber) 
+    // const applicationNumber = getQueryArg(window.location.href, "applicationNumber");
+    prepareDocumentsView(state, dispatch);
     return action;
   },
   components: {
