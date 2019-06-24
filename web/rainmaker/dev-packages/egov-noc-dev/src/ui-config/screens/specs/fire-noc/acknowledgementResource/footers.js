f17fa55e51de9c98874f87205a59668174106f4f
avijeet7 <avijeet7@gmail.com>
2019-06-24 17:40:43 +0530

    NOC:  goto home footer

--- web/rainmaker/dev-packages/egov-noc-dev/src/ui-config/screens/specs/fire-noc/acknowledgementResource/footers.js
+++ web/rainmaker/dev-packages/egov-noc-dev/src/ui-config/screens/specs/fire-noc/acknowledgementResource/footers.js
@@ -88,32 +88,32 @@ const getCommonApplyFooter = children => {
 };
 
 //Function for go to home button
-// export const gotoHomeFooter = getCommonApplyFooter({
-//   gotoHome: {
-//     componentPath: "Button",
-//     props: {
-//       variant: "outlined",
-//       color: "primary",
-//       style: {
-//         minWidth: "200px",
-//         height: "48px",
-//         marginRight: "16px"
-//       }
-//     },
-//     children: {
-//       //downloadReceiptButtonLabel: getLabel
-//       goToHomeButtonLabel: getLabel({
-//         labelName: "GO TO HOME",
-//         labelKey: "NOC_COMMON_BUTTON_HOME"
-//       })
-//     },
-//     // Check this onClickDefinition later again
-//     onClickDefination: {
-//       action: "page_change",
-//       path: `${getRedirectionURL()}`
-//     }
-//   }
-// });
+export const gotoHomeFooter = getCommonApplyFooter({
+  gotoHome: {
+    componentPath: "Button",
+    props: {
+      variant: "outlined",
+      color: "primary",
+      style: {
+        minWidth: "200px",
+        height: "48px",
+        marginRight: "16px"
+      }
+    },
+    children: {
+      //downloadReceiptButtonLabel: getLabel
+      goToHomeButtonLabel: getLabel({
+        labelName: "GO TO HOME",
+        labelKey: "NOC_COMMON_BUTTON_HOME"
+      })
+    },
+    // Check this onClickDefinition later again
+    onClickDefination: {
+      action: "page_change",
+      path: `${getRedirectionURL()}`
+    }
+  }
+});
 
 //Function for application success(show those 3 buttons )
 export const applicationSuccessFooter = (
