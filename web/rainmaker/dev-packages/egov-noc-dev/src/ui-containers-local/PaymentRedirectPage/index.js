fc90ccdfc51c5f3f49f20676aaeb53deb75235c2
avijeet7 <avijeet7@gmail.com>
2019-07-01 20:39:09 +0530

    NOC: Citizen update action after pay

--- web/rainmaker/dev-packages/egov-noc-dev/src/ui-containers-local/PaymentRedirectPage/index.js
+++ web/rainmaker/dev-packages/egov-noc-dev/src/ui-containers-local/PaymentRedirectPage/index.js
@@ -1,7 +1,9 @@
-import React, { Component } from "react";
+import { httpRequest } from "egov-ui-framework/ui-utils/api";
 import get from "lodash/get";
-import { httpRequest } from "../../ui-utils/api";
+import set from "lodash/set";
+import React, { Component } from "react";
 import { withRouter } from "react-router";
+import { getSearchResults } from "../../ui-utils/commons";
 
 class PaymentRedirect extends Component {
   componentDidMount = async () => {
@@ -20,6 +22,24 @@ class PaymentRedirect extends Component {
       if (get(pgUpdateResponse, "Transaction[0].txnStatus") === "FAILURE") {
         window.location.href = `/fire-noc/acknowledgement?purpose=${"pay"}&status=${"failure"}&applicationNumber=${consumerCode}&tenantId=${tenantId}`;
       } else {
+        let response = await getSearchResults([
+          {
+            key: "tenantId",
+            value: tenantId
+          },
+          { key: "applicationNumber", value: consumerCode }
+        ]);
+        set(response, "FireNOCs[0].fireNOCDetails.action", "PAY");
+        response = await httpRequest(
+          "post",
+          "/firenoc-services/v1/_update",
+          "",
+          [],
+          {
+            FireNOCs: get(response, "FireNOCs", [])
+          }
+        );
+
         let transactionId = get(pgUpdateResponse, "Transaction[0].txnId");
         window.location.href = `/fire-noc/acknowledgement?purpose=${"pay"}&status=${"success"}&applicationNumber=${consumerCode}&tenantId=${tenantId}&secondNumber=${transactionId}`;
       }
