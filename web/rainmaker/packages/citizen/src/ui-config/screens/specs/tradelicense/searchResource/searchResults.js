5fedaa02c904b42473605f75e69773b1f7b0dcfd
sudhanshudeshmukh <sudhanshu.s.deshmukh@gmail.com>
2019-08-06 17:24:03 +0530

    search screen without auth

---
+++ web/rainmaker/packages/citizen/src/ui-config/screens/specs/tradelicense/searchResource/searchResults.js
@@ -0,0 +1,71 @@
+import React from "react";
+import get from "lodash/get";
+import { getLocaleLabels, getTransformedLocalStorgaeLabels } from "egov-ui-framework/ui-utils/commons";
+
+export const textToLocalMapping = {
+  "Application No": getLocaleLabels("Application No", "TL_COMMON_TABLE_COL_APP_NO", getTransformedLocalStorgaeLabels()),
+
+  "License No": getLocaleLabels("License No", "TL_COMMON_TABLE_COL_LIC_NO", getTransformedLocalStorgaeLabels()),
+
+  "Trade Name": getLocaleLabels("Trade Name", "TL_COMMON_TABLE_COL_TRD_NAME", getTransformedLocalStorgaeLabels()),
+  "Owner Name": getLocaleLabels("Owner Name", "TL_COMMON_TABLE_COL_OWN_NAME", getTransformedLocalStorgaeLabels()),
+
+  "Application Date": getLocaleLabels("Application Date", "TL_COMMON_TABLE_COL_APP_DATE", getTransformedLocalStorgaeLabels()),
+
+  Tenant: getLocaleLabels("Tenant", "TL_COMMON_TABLE_COL_TENANT", getTransformedLocalStorgaeLabels()),
+
+  Status: getLocaleLabels("Status", "TL_COMMON_TABLE_COL_STATUS", getTransformedLocalStorgaeLabels()),
+
+  INITIATED: getLocaleLabels("Initiated,", "TL_INITIATED", getTransformedLocalStorgaeLabels()),
+  APPLIED: getLocaleLabels("Applied", "TL_APPLIED", getTransformedLocalStorgaeLabels()),
+  PAID: getLocaleLabels("Paid", "WF_NEWTL_PENDINGAPPROVAL", getTransformedLocalStorgaeLabels()),
+
+  APPROVED: getLocaleLabels("Approved", "TL_APPROVED", getTransformedLocalStorgaeLabels()),
+  REJECTED: getLocaleLabels("Rejected", "TL_REJECTED", getTransformedLocalStorgaeLabels()),
+  CANCELLED: getLocaleLabels("Cancelled", "TL_CANCELLED", getTransformedLocalStorgaeLabels()),
+  PENDINGAPPROVAL: getLocaleLabels("Pending for Approval", "WF_NEWTL_PENDINGAPPROVAL", getTransformedLocalStorgaeLabels()),
+  PENDINGPAYMENT: getLocaleLabels("Pending payment", "WF_NEWTL_PENDINGPAYMENT", getTransformedLocalStorgaeLabels()),
+
+  FIELDINSPECTION: getLocaleLabels("Pending for Field Inspection", "WF_NEWTL_FIELDINSPECTION", getTransformedLocalStorgaeLabels()),
+
+  "Search Results for Trade License Applications": getLocaleLabels("", "TL_HOME_SEARCH_RESULTS_TABLE_HEADING", getTransformedLocalStorgaeLabels()),
+};
+
+export const searchResults = {
+  uiFramework: "custom-molecules",
+  // moduleName: "egov-tradelicence",
+  componentPath: "Table",
+  visible: false,
+  props: {
+    //data: [],
+    columns: {
+      [get(textToLocalMapping, "Application No")]: {
+        format: (rowData) => {
+          return (
+            <span
+              style={{
+                color: "#FE7A51",
+              }}
+            >
+              {rowData[get(textToLocalMapping, "Application No")]}
+            </span>
+          );
+        },
+      },
+      [get(textToLocalMapping, "License No")]: {},
+      [get(textToLocalMapping, "Trade Name")]: {},
+      [get(textToLocalMapping, "Application Date")]: {},
+      [get(textToLocalMapping, "Tenant")]: {},
+      [get(textToLocalMapping, "Status")]: {},
+    },
+    title: get(textToLocalMapping, "Search Results for Trade License Applications"),
+    options: {
+      filter: false,
+      download: false,
+      responsive: "stacked",
+      selectableRows: false,
+      hover: true,
+      rowsPerPageOptions: [10, 15, 20],
+    },
+  },
+};
---
+++ web/rainmaker/packages/citizen/src/ui-config/screens/specs/utils/index.js
@@ -0,0 +1,43 @@
+import get from "lodash/get";
+import { validate } from "egov-ui-framework/ui-redux/screen-configuration/utils";
+
+export const validateFields = (objectJsonPath, state, dispatch, screen = "apply") => {
+  const fields = get(state.screenConfiguration.screenConfig[screen], objectJsonPath, {});
+  let isFormValid = true;
+  for (var variable in fields) {
+    if (fields.hasOwnProperty(variable)) {
+      if (
+        fields[variable] &&
+        fields[variable].props &&
+        (fields[variable].props.disabled === undefined || !fields[variable].props.disabled) &&
+        !validate(
+          screen,
+          {
+            ...fields[variable],
+            value: get(state.screenConfiguration.preparedFinalObject, fields[variable].jsonPath),
+          },
+          dispatch,
+          true
+        )
+      ) {
+        isFormValid = false;
+      }
+    }
+  }
+  return isFormValid;
+};
+
+export const convertEpochToDate = (dateEpoch) => {
+  const dateFromApi = new Date(dateEpoch);
+  let month = dateFromApi.getMonth() + 1;
+  let day = dateFromApi.getDate();
+  let year = dateFromApi.getFullYear();
+  month = (month > 9 ? "" : "0") + month;
+  day = (day > 9 ? "" : "0") + day;
+  return `${day}/${month}/${year}`;
+};
+
+export const getEpochForDate = (date) => {
+  const dateSplit = date.split("/");
+  return new Date(dateSplit[2], dateSplit[1] - 1, dateSplit[0]).getTime();
+};
