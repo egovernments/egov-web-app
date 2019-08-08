a2a76dc3481b879b42130d2f680547cee2975032
avijeet7 <avijeet7@gmail.com>
2019-08-08 17:15:48 +0530

    MT: Dropdown implementation, fix user info tenant

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/Header/components/UserSettings/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/Header/components/UserSettings/index.js
@@ -1,17 +1,19 @@
-import React, { Component } from "react";
 import { DropDown, Icon, Image, List } from "components";
+import { getTransformedLocale, getLocaleLabels } from "egov-ui-framework/ui-utils/commons";
 import emptyFace from "egov-ui-kit/assets/images/download.png";
-import { CommonMenuItems } from "../NavigationDrawer/commonMenuItems";
-import { getLocale } from "egov-ui-kit/utils/localStorageUtils";
-import { connect } from "react-redux";
+import { getLocale, getTenantId, setTenantId } from "egov-ui-kit/utils/localStorageUtils";
 import get from "lodash/get";
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { CommonMenuItems } from "../NavigationDrawer/commonMenuItems";
+import { setRoute } from "egov-ui-framework/ui-redux/app/actions";
 import "./index.css";
 
 class UserSettings extends Component {
   state = {
     languageSelected: getLocale(),
     displayAccInfo: false,
+    tenantSelected: getTenantId(),
   };
   style = {
     baseStyle: {
@@ -41,11 +43,24 @@ class UserSettings extends Component {
       display: "flex",
       alignItems: "center",
     },
+    baseTenantStyle: {
+      background: "#ffffff",
+      height: "65px",
+      marginRight: "30px",
+      width: "120px",
+      marginBottom: "24px",
+    },
   };
 
   onChange = (event, index, value) => {
-    this.setState({ languageSelected: value });
+    this.setState({ ...this.state, languageSelected: value });
     this.props.fetchLocalizationLabel(value);
+  };
+
+  onTenantChange = (event, index, value) => {
+    this.setState({ ...this.state, tenantSelected: value });
+    setTenantId(value);
+    this.props.setRoute("/");
   };
 
   toggleAccInfo() {
@@ -55,11 +70,32 @@ class UserSettings extends Component {
   }
 
   render() {
-    const { languageSelected, displayAccInfo } = this.state;
+    const { languageSelected, displayAccInfo, tenantSelected } = this.state;
     const { style } = this;
-    const { onIconClick, userInfo, handleItemClick, hasLocalisation, languages } = this.props;
+    const { onIconClick, userInfo, handleItemClick, hasLocalisation, languages, fetchLocalizationLabel } = this.props;
+
+    /**
+     * Get All tenant id's from (user info -> roles) to populate dropdown
+     */
+    let tenantIdsList = get(userInfo, "roles", []).map((role) => {
+      return role.tenantId;
+    });
+    tenantIdsList = [...new Set(tenantIdsList)];
+    tenantIdsList = tenantIdsList.map((tenantId) => {
+      return { value: tenantId, label: getLocaleLabels(tenantId, "TENANT_TENANTS_" + getTransformedLocale(tenantId)) };
+    });
+
     return (
       <div className="userSettingsContainer">
+        <DropDown
+          onChange={this.onTenantChange}
+          listStyle={style.listStyle}
+          style={style.baseTenantStyle}
+          labelStyle={style.label}
+          dropDownData={tenantIdsList}
+          value={tenantSelected}
+          underlineStyle={{ borderBottom: "none" }}
+        />
         {hasLocalisation && (
           <DropDown
             onChange={this.onChange}
@@ -109,7 +145,13 @@ const mapStateToProps = ({ common }) => {
   return { languages };
 };
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setRoute: (route) => dispatch(setRoute(route)),
+  };
+};
+
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(UserSettings);
